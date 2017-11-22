805c7e769843bf9b7c0f13e278afd2f30d1e4b5d
LnT <lalit.tanwar07@gmail.com>
2017-11-22 12:05:12 +0530

    Url Config

--- server-ui/src/main/java/com/amx/ui/config/WebSecurityConfig.java
+++ server-ui/src/main/java/com/amx/ui/config/WebSecurityConfig.java
@@ -7,27 +7,30 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	CustomerAuthProvider customAuthProvider;
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-//		http.authorizeRequests().antMatchers("/", "/home").permitAll().anyRequest().authenticated().and().formLogin()
-//				.loginPage("/login").permitAll().and().logout().permitAll();
+		http.authorizeRequests().antMatchers("/register/**").permitAll().antMatchers("/app/**").authenticated().and()
+				.formLogin().loginPage("/login").failureUrl("/login?error").permitAll().and().logout()
+				.logoutSuccessUrl("/login?logout").permitAll().and().exceptionHandling().accessDeniedPage("/403");
 	}
 
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
+		auth.authenticationProvider(customAuthProvider).inMemoryAuthentication().withUser("user").password("password")
+				.roles("USER");
 	}
 
 	@Override
 	public void configure(WebSecurity web) throws Exception {
 		web.ignoring().antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**");
 	}
-	
+
 }
