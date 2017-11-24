7eb8b8e06e8f37845b154828324af5762d1a8082
LnT <lalit.tanwar07@gmail.com>
2017-11-24 16:17:34 +0530

    register fix

--- server-ui/src/main/java/com/amx/jax/ui/config/WebMvcConfig.java
+++ server-ui/src/main/java/com/amx/jax/ui/config/WebMvcConfig.java
@@ -9,7 +9,7 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
-		registry.addViewController("/register").setViewName("register");
+		registry.addViewController("/register/**").setViewName("register");
 		registry.addViewController("/home").setViewName("home");
 		registry.addViewController("/").setViewName("home");
 		registry.addViewController("/hello").setViewName("hello");
