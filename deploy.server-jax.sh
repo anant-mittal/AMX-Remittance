d3e39816ac1cfad02608faa337449761dfb01c5a
Prashant <prashant.thorat@almullagroup.com>
2017-11-24 19:47:06 +0530

    build server deployment for jax ser ice

---
+++ deploy.server-jax.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+bash setup.sh
+
+mvn clean
+
+mvn package -DskipTests
+
+scp ms-jax/target/ms-jax-0.0.1-SNAPSHOT.jar devenvironment@10.6.42.95:~/jax
+
+ssh devenvironment@10.6.42.95  '/usr/lib/jvm/java-8-oracle/bin/java -Djax.logging-dir=/home/devenvironment/jax/ -Dserver-port=8081 -jar /home/devenvironment/jax/ms-jax-0.0.1-SNAPSHOT.jar'
