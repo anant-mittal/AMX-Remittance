3c44fc2035fbd6552020f2c57ba984d692b1dfe8
Prashant <prashant.thorat@almullagroup.com>
2017-11-29 10:50:17 +0530

    misc

--- deploy.server-jax.sh
+++ deploy.server-jax.sh
@@ -6,7 +6,7 @@ mvn clean
 
 mvn package -DskipTests
 
-scp ms-jax/target/ms-jax-0.0.1-SNAPSHOT.jar devenvironment@10.6.42.95:~/jax
+sshpass -p "amx@123" scp ms-jax/target/ms-jax-0.0.1-SNAPSHOT.jar devenvironment@10.6.42.95:~/jax
 
 sshpass -p "amx@123" ssh -o StrictHostKeyChecking=no devenvironment@10.6.42.95 '/usr/lib/jvm/java-8-oracle/bin/java -Djax.logging-dir=/home/devenvironment/jax/ -Dserver-port=8081 -jar /home/devenvironment/jax/ms-jax-0.0.1-SNAPSHOT.jar'
 
