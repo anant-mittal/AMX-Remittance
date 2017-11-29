7e5fc6c98ebc5da650555450e9397fa48b8f294b
Lalit Tanwar <lalit.tanwar07@gmail.com>
2017-11-29 08:00:11 +0000

    Update sonar.scan.sh

--- sonar.scan.sh
+++ sonar.scan.sh
@@ -1,2 +1,10 @@
 bash setup.sh
+
+mvn clean verify sonar:sonar
+ 
+# In some situation you may want to run sonar:sonar goal as a dedicated step. Be sure to use install as first step for multi-module projects
+mvn clean install
 mvn sonar:sonar
+ 
+# Specify the version of sonar-maven-plugin instead of using the latest. See also 'How to Fix Version of Maven Plugin' below.
+mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.3.0.603:sonar
