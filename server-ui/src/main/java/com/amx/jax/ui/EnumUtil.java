5199378e58a0b6934529687a1363370ccb7bb88e
LnT <lalit.tanwar07@gmail.com>
2017-11-30 14:38:05 +0530

    Code Formatting

--- server-ui/src/main/java/com/amx/jax/ui/EnumUtil.java
+++ server-ui/src/main/java/com/amx/jax/ui/EnumUtil.java
@@ -2,13 +2,19 @@ package com.amx.jax.ui;
 
 public class EnumUtil {
 
+	/**
+	 * http://www.restapitutorial.com/httpstatuscodes.html
+	 * 
+	 * @author lalittanwar
+	 *
+	 */
 	public enum StatusCode {
 
 		// Registration
-		ALREADY_ACTIVE("400.1", "ALREADY_ACTIVE"), INVALID_ID("202.2", "INVALID_ID"),
+		ALREADY_ACTIVE("302", "ALREADY_ACTIVE"), INVALID_ID("202.2", "INVALID_ID"),
 
-		ALREADY_LOGGED_IN("202.3", "ALREADY_LOGGED_IN"), VERIFY_SUCCESS("202.4",
-				"VERIFY_SUCCESS"), VERIFY_FAILED("202.4", "VERIFY_FAILED"),
+		ALREADY_LOGGED_IN("302", "ALREADY_LOGGED_IN"), VERIFY_SUCCESS("201", "VERIFY_SUCCESS"), VERIFY_FAILED("401",
+				"VERIFY_FAILED"),
 
 		QA_UPDATED("200", "QA_UPDATED"),
 
