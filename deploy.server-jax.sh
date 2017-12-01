c45e90c2630f979832d5394cb57f543c25aa90e7
Prashant <prashant.thorat@almullagroup.com>
2017-12-01 12:47:24 +0530

    integration

--- deploy.server-jax.sh
+++ deploy.server-jax.sh
@@ -10,10 +10,10 @@ mvn clean
 
 mvn package -DskipTests
 
-sshpass -p "$JAX_PASS" scp ms-jax/target/ms-jax-0.0.1-SNAPSHOT.jar $JAX_USER@$JAX_HOST :~/jax
+sshpass -p $JAX_PASS scp ms-jax/target/ms-jax-0.0.1-SNAPSHOT.jar $JAX_USER@$JAX_HOST :~/jax
 
 # sshpass -p "$JAX_PASS" ssh -o StrictHostKeyChecking=no $JAX_USER@$JAX_HOST 'nohup /usr/lib/jvm/java-8-oracle/bin/java -Djax.logging-dir=/home/devenvironment/jax/ -Dserver-port=$JAX_PORT -jar /home/devenvironment/jax/ms-jax-0.0.1-SNAPSHOT.jar &'
-sshpass -p "$JAX_PASS" ssh -o StrictHostKeyChecking=no $JAX_USER@$JAX_HOST '/etc/init.d/jaxapp restart &'
+sshpass -p $JAX_PASS ssh -o StrictHostKeyChecking=no $JAX_USER@$JAX_HOST '/etc/init.d/jaxapp restart &'
 
 
 # use this command
