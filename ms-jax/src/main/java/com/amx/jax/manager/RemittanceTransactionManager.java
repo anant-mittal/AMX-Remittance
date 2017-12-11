df7425ba6277573a51748e780c3aec457963ee53
Prashant <prashant.thorat@almullagroup.com>
2017-12-11 19:45:24 +0530

    exchange service

--- ms-jax/src/main/java/com/amx/jax/manager/RemittanceTransactionManager.java
+++ ms-jax/src/main/java/com/amx/jax/manager/RemittanceTransactionManager.java
@@ -1,11 +1,17 @@
 package com.amx.jax.manager;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.amx.amxlib.error.JaxError;
 import com.amx.amxlib.model.request.RemittanceTransactionRequestModel;
+import com.amx.jax.dao.BlackListDao;
+import com.amx.jax.dbmodel.BenificiaryListView;
+import com.amx.jax.dbmodel.BlackListModel;
+import com.amx.jax.exception.GlobalException;
 import com.amx.jax.repository.IBeneficiaryOnlineDao;
 
 @Component
@@ -13,15 +19,30 @@ public class RemittanceTransactionManager {
 
 	@Autowired
 	IBeneficiaryOnlineDao beneficiaryOnlineDao;
-	
+
+	@Autowired
+	private BlackListDao blistDao;
+
 	public Object validateTransactionData(RemittanceTransactionRequestModel model) {
-		
+
 		BigDecimal beneId = model.getBeneId();
-		beneficiaryOnlineDao.findOne(beneId);
+		BenificiaryListView beneficiary = beneficiaryOnlineDao.findOne(beneId);
+		validateBlackListedBene(beneficiary);
 		return model;
 
-		
 	}
 
-	
+	private void validateBlackListedBene(BenificiaryListView beneficiary) {
+		List<BlackListModel> blist = blistDao.getBlackByName(beneficiary.getBenificaryName());
+		if (blist != null && !blist.isEmpty()) {
+			throw new GlobalException("Beneficiary name found matching with black list ",
+					JaxError.BLACK_LISTED_CUSTOMER.getCode());
+		}
+		blist = blistDao.getBlackByName(beneficiary.getArbenificaryName());
+		if (blist != null && !blist.isEmpty()) {
+			throw new GlobalException("Beneficiary local name found matching with black list ",
+					JaxError.BLACK_LISTED_CUSTOMER.getCode());
+		}
+	}
+
 }
