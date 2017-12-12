e8477b0e402ac2035c47e1f4ed81e201f866fa17
Prashant <prashant.thorat@almullagroup.com>
2017-12-12 21:28:20 +0530

    bug fix

--- ms-user/src/main/java/com/amx/jax/userservice/repository/CustomerIdProofRepository.java
+++ ms-user/src/main/java/com/amx/jax/userservice/repository/CustomerIdProofRepository.java
@@ -11,11 +11,11 @@ import com.amx.jax.dbmodel.CustomerIdProof;
 
 public interface CustomerIdProofRepository extends JpaRepository<CustomerIdProof, Serializable> {
 
-	@Query(value = "select * from FS_CUSTOMER_ID_PROOF where CUSTOMER_ID=?1 and NVL(ISACTIVE,'') <>'D'"
+	@Query(value = "select * from FS_CUSTOMER_ID_PROOF where CUSTOMER_ID=?1 and NVL(ISACTIVE,'') ='Y'"
 			+ " ORDER BY NVL(LAST_UPDATED_DATE,CREATION_DATE) DESC", nativeQuery = true)
 	public List<CustomerIdProof> getCustomerIdProofByCustomerId(BigDecimal customerId);
 
-	@Query(value = "select * from FS_CUSTOMER_ID_PROOF where CUSTOMER_ID=?1 and IDENTITY_TYPE_ID =?2 and NVL(ISACTIVE,'') <>'D' and  TRUNC(IDENTITY_EXPIRY_DATE) >=TRUNC(SYSDATE)"
+	@Query(value = "select * from FS_CUSTOMER_ID_PROOF where CUSTOMER_ID=?1 and IDENTITY_TYPE_ID =?2 and NVL(ISACTIVE,'') ='Y' and  TRUNC(IDENTITY_EXPIRY_DATE) >=TRUNC(SYSDATE)"
 			+ " ORDER BY NVL(LAST_UPDATED_DATE,CREATION_DATE) DESC", nativeQuery = true)
 	public List<CustomerIdProof> getCustomerImageValidation(BigDecimal customerId, BigDecimal identityTypeId);
 
