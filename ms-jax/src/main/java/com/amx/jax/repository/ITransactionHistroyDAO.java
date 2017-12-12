06fe8f5a2d0b4891258a0fde1cff421b2ad1dbdd
mir.rabiluddin <mir.rabiluddin@almullagroup.com>
2017-12-12 09:57:00 +0300

    transaction histroy

--- ms-jax/src/main/java/com/amx/jax/repository/ITransactionHistroyDAO.java
+++ ms-jax/src/main/java/com/amx/jax/repository/ITransactionHistroyDAO.java
@@ -17,7 +17,8 @@ public interface ITransactionHistroyDAO extends JpaRepository<CustomerRemittance
 	
 	//and trunc(document_Date) between  trunc(sysdate-6*30) and  trunc(sysdate);
 	// documentDate  --and th.documentFinanceYear=:docfyr
-	@Query("select th from CustomerRemittanceTransactionView th where th.customerId=:customerId  and trunc(th.documentDate) between  trunc(sysdate-6*30) and  trunc(sysdate)") 
+	@Query("select th from CustomerRemittanceTransactionView th where th.customerId=:customerId  "
+			+ "and trunc(th.documentDate) between  trunc(sysdate-6*30) and  trunc(sysdate) and th.beneficaryCorespondingBankName <>'WU'") 
 	public List<CustomerRemittanceTransactionView> getTransactionHistroy(
 			@Param("customerId") BigDecimal customerId);
 	
@@ -28,7 +29,8 @@ public interface ITransactionHistroyDAO extends JpaRepository<CustomerRemittance
 			@Param("docNumber") BigDecimal docNumber);
 	
 
-	@Query(value=" select * from VW_EX_TRANSACTION_INQUIRY where CUSTOMER_ID=?1 and DOCUMENT_FINANCE_YEAR=?2 and DOCUMENT_DATE between to_date(?3,'dd/mm/yyyy') and to_date(?4,'dd/mm/yyyy')",nativeQuery=true)
+	@Query(value=" select * from VW_EX_TRANSACTION_INQUIRY where CUSTOMER_ID=?1 and DOCUMENT_FINANCE_YEAR=?2 "
+			+ "and DOCUMENT_DATE between to_date(?3,'dd/mm/yyyy') and to_date(?4,'dd/mm/yyyy') and CORRESPONDING_BANK <>'WU'",nativeQuery=true)
 	public List<CustomerRemittanceTransactionView> getTransactionHistroyDateWise(
 			BigDecimal customerId,  BigDecimal docfyr,
 			 String fromDate, String toDate);
