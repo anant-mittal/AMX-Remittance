5394183af331fc59eb71aa18a8a1ecdc4bf121f4
LnT <lalit.tanwar07@gmail.com>
2017-12-14 20:31:43 +0530

    deploy changes

--- deploy.server-jax.sh
+++ deploy.server-jax.sh
@@ -10,14 +10,16 @@ mvn clean
 
 #mvn package -DskipTests
 
-mvn -pl amx-lib clean install -DskipTests
-mvn -pl core clean install -DskipTests
-mvn -pl ms-meta clean install -DskipTests
-mvn -pl ms-user clean install -DskipTests
-mvn -pl ms-exchangerate clean install -DskipTests
-mvn -pl ms-jax clean install -DskipTests
-mvn -pl jax-client clean install -DskipTests
+#mvn -pl amx-lib clean install -DskipTests
+#mvn -pl core clean install -DskipTests
+#mvn -pl ms-meta clean install -DskipTests
+#mvn -pl ms-user clean install -DskipTests
+#mvn -pl ms-exchangerate clean install -DskipTests
+#mvn -pl ms-jax clean install -DskipTests
+#mvn -pl jax-client clean install -DskipTests
 
+mvn -T 4 package -pl ms-jax -am -DskipTests
+mvn -T 4 package -pl jax-client -am -DskipTests
 
 sshpass -p $JAX_PASS scp ms-jax/target/ms-jax-0.0.1-SNAPSHOT.jar $JAX_USER@$JAX_HOST:~/jax
 
