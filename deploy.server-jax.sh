5e3f25a01649a7f66e11dc4d9cd23f6d310731d1
Prashant <prashant.thorat@almullagroup.com>
2017-12-15 11:19:18 +0530

    bug fix

--- deploy.server-jax.sh
+++ deploy.server-jax.sh
@@ -23,7 +23,7 @@ mvn -T 4 package -pl jax-client -am -DskipTests
 
 sshpass -p $JAX_PASS scp ms-jax/target/ms-jax-0.0.1-SNAPSHOT.jar $JAX_USER@$JAX_HOST:~/jax
 
-sshpass -p $JAX_PASS ssh -o StrictHostKeyChecking=no $JAX_USER@$JAX_HOST kill -9 `cat /var/run/jaxapp/jaxapp.pid` &
+sshpass -p $JAX_PASS ssh -o StrictHostKeyChecking=no $JAX_USER@$JAX_HOST kill -9 $(cat /var/run/jaxapp/jaxapp.pid)
 sshpass -p $JAX_PASS ssh -o StrictHostKeyChecking=no $JAX_USER@$JAX_HOST /etc/init.d/jaxapp restart &
 
 # use this command
