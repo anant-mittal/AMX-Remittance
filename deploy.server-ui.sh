d14055cad8e03d7e8eb2bdfbb4ae930a817691cf
Kumar Animesh <kanimesh.s@gmail.com>
2017-12-26 05:22:47 +0000

    Update deploy.server-ui.sh

--- deploy.server-ui.sh
+++ deploy.server-ui.sh
@@ -24,7 +24,7 @@ mvn -T 4 package -pl server-ui -am -DskipTests
 sshpass -p $JAX_PASS scp server-ui/target/server-ui-0.0.1-SNAPSHOT.jar $JAX_USER@$JAX_HOST:~/jax/server-ui
 
 #sshpass -p $JAX_PASS ssh -o StrictHostKeyChecking=no $JAX_USER@$JAX_HOST 'kill -9 $(cat /var/run/server-ui/server-ui.pid)'
-sshpass -p $JAX_PASS ssh -o StrictHostKeyChecking=no $JAX_USER@$JAX_HOST '/etc/init.d/server-ui restart &'
+sshpass -p $JAX_PASS ssh -o StrictHostKeyChecking=no $JAX_USER@$JAX_HOST '/etc/init.d/server-ui start &'
 
 #curl -T "target/app.war" "http://$TOMCAT_USER:$TOMCAT_PASSWORD@$TOMCAT_HOST/manager/text/deploy?path=/&update=true"
 #curl -T "target/app.war" "http://tomcat:tomcat@localhost:8080/manager/text/deploy?path=/app&update=true"
