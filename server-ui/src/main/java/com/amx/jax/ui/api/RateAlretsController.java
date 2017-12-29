bce48b30c7d8a74892cbfaca57e3011aafcdd49b
LnT <lalit.tanwar07@gmail.com>
2017-12-29 16:20:16 +0530

    Rate Controler Fixes

--- server-ui/src/main/java/com/amx/jax/ui/api/RateAlretsController.java
+++ server-ui/src/main/java/com/amx/jax/ui/api/RateAlretsController.java
@@ -1,32 +1,19 @@
 
 package com.amx.jax.ui.api;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
-import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.amx.amxlib.meta.model.CurrencyMasterDTO;
-import com.amx.amxlib.meta.model.RemittancePageDto;
-import com.amx.amxlib.meta.model.SourceOfIncomeDto;
 import com.amx.amxlib.model.RateAlertDTO;
-import com.amx.jax.ui.beans.TenantBean;
-import com.amx.jax.ui.response.ResponseMeta;
 import com.amx.jax.ui.response.ResponseWrapper;
-import com.amx.jax.ui.service.AppEnvironment;
 import com.amx.jax.ui.service.JaxService;
-import com.amx.jax.ui.session.GuestSession;
 
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 
 @RestController
 @Api(value = "Meta APIs")
@@ -34,15 +21,6 @@ public class RateAlretsController {
 
 	@Autowired
 	private JaxService jaxService;
-
-	@Autowired
-	AppEnvironment env;
-
-	@Autowired
-	TenantBean tenantBean;
-
-	@Autowired
-	GuestSession guestSession;
 
 	@RequestMapping(value = "/api/xtrate/alert/list", method = { RequestMethod.POST })
 	public ResponseWrapper<List<RateAlertDTO>> listOfAlerts() {
