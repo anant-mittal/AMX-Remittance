c132147a97c46711c14496ab3734dcb6361e6132
Viki <viki.sangani@almullagroup.com>
2018-01-06 15:10:17 +0530

    PG : cleanup

--- payment-service/src/main/java/com/amx/jax/payment/controller/PaymentController.java
+++ payment-service/src/main/java/com/amx/jax/payment/controller/PaymentController.java
@@ -25,8 +25,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.amx.jax.payment.constant.PGEnum;
 import com.amx.jax.payment.model.url.PaymentResponse;
 import com.amx.jax.payment.service.PaymentService;
-import com.amx.jax.payment.util.JsonUtil;
 import com.amx.jax.payment.util.PaymentUtil;
+import com.bootloaderjs.JsonUtil;
 import com.fasterxml.jackson.core.JsonProcessingException;
 
 import io.swagger.annotations.Api;
@@ -104,6 +104,43 @@ public class PaymentController {
 
 		Map<String, String[]> parameters = request.getParameterMap();
 		log.info("In Payment capture method with params : " + PaymentUtil.getMapKeyValueAsString(parameters));
+		
+		 HashMap<String, String> paramMap = PaymentUtil.generateParameterMapForPaymentCapture(parameters);
+		 PaymentResponse res = paymentService.capturePayment(paramMap);
+
+		String redirectUrl = null;
+		if ("CAPTURED".equalsIgnoreCase(result)) {
+			redirectUrl = String.format("https://applications2.almullagroup.com/payg/callback/success?"
+					+ "PaymentID=%s&result=%s&auth=%s&ref=%s&postdate=%s&trackid=%s&tranid=%s&udf1=%s&udf2=%s&udf3=%s&udf4=%s&udf5=%s",
+					paymentid, result, auth, ref, postdate, trackid, tranid, udf1, udf2, udf3, udf4, udf5);
+		} else if ("CANCELED".equalsIgnoreCase(result)) {
+			redirectUrl = String.format("https://applications2.almullagroup.com/payg/callback/cancelled?"
+					+ "PaymentID=%s&result=%s&auth=%s&ref=%s&postdate=%s&trackid=%s&tranid=%s&udf1=%s&udf2=%s&udf3=%s&udf4=%s&udf5=%s",
+					paymentid, result, auth, ref, postdate, trackid, tranid, udf1, udf2, udf3, udf4, udf5);
+		} else {
+			redirectUrl = String.format("https://applications2.almullagroup.com/payg/callback/error?"
+					+ "PaymentID=%s&result=%s&auth=%s&ref=%s&postdate=%s&trackid=%s&tranid=%s&udf1=%s&udf2=%s&udf3=%s&udf4=%s&udf5=%s",
+					paymentid, result, auth, ref, postdate, trackid, tranid, udf1, udf2, udf3, udf4, udf5);
+		}
+
+		model.addAttribute("REDIRECT", redirectUrl);
+
+		return "thymeleaf/repback";
+
+	}
+
+	@RequestMapping(value = { "/payment_capture/oman/*","/payment_capture/oman" })
+	public @ResponseBody String paymentCaptureOman(HttpServletRequest request, Model model,
+			@RequestParam(required = false) String paymentid, @RequestParam(required = false) String result,
+			@RequestParam(required = false) String auth, @RequestParam(required = false) String ref,
+			@RequestParam(required = false) String postdate, @RequestParam(required = false) String trackid,
+			@RequestParam(required = false) String tranid, @RequestParam(required = false) String responsecode,
+			@RequestParam(required = false) String udf1, @RequestParam(required = false) String udf2,
+			@RequestParam(required = false) String udf3, @RequestParam(required = false) String udf4,
+			@RequestParam(required = false) String udf5) {
+
+		Map<String, String[]> parameters = request.getParameterMap();
+		log.info("In Payment capture method with params : " + PaymentUtil.getMapKeyValueAsString(parameters));
 
 		String redirectUrl = null;
 		if ("CAPTURED".equalsIgnoreCase(result)) {
@@ -126,46 +163,6 @@ public class PaymentController {
 		// PaymentResponse res = paymentService.capturePayment(paramMap);
 		return "thymeleaf/repback";
 
-	}
-
-	@RequestMapping(value = { "/payment_capture/oman/*",
-			"/payment_capture/oman" }, method = RequestMethod.POST, produces = { MediaType.APPLICATION_JSON_VALUE })
-	public @ResponseBody String paymentCaptureOman(HttpServletRequest request) {
-
-		Map<String, String[]> parameters = request.getParameterMap();
-		log.info("In Payment capture method with params : " + PaymentUtil.getMapKeyValueAsString(parameters));
-
-		String tranData = request.getParameter("trandata");
-		PaymentResponse res = paymentService.captureOmanPayment(tranData, request);
-
-		String jsonResponse = null;
-		try {
-			jsonResponse = JsonUtil.toJson(res);
-		} catch (JsonProcessingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return jsonResponse;
-
-	}
-
-	private HashMap<String, String> generateParameterMapForPaymentCapture(Map<String, String[]> parameters) {
-
-		HashMap<String, String> mapPaymentResponeDetails = new HashMap<String, String>();
-		mapPaymentResponeDetails.put("paymentId", parameters.get("paymentid")[0]);
-		mapPaymentResponeDetails.put("result", parameters.get("result")[0]);
-		mapPaymentResponeDetails.put("auth_appNo", parameters.get("auth")[0]);
-		mapPaymentResponeDetails.put("referenceId", parameters.get("ref")[0]);
-		mapPaymentResponeDetails.put("postDate", parameters.get("postdate")[0]);
-		mapPaymentResponeDetails.put("trackId", parameters.get("trackid")[0]);
-		mapPaymentResponeDetails.put("tranId", parameters.get("tranid")[0]);
-		mapPaymentResponeDetails.put("responsecode", parameters.get("responsecode")[0]);
-		mapPaymentResponeDetails.put("udf1", parameters.get("udf1")[0]);
-		mapPaymentResponeDetails.put("udf2", parameters.get("udf2")[0]);
-		mapPaymentResponeDetails.put("udf3", parameters.get("udf3")[0]);
-		mapPaymentResponeDetails.put("udf4", parameters.get("udf4")[0]);
-		mapPaymentResponeDetails.put("udf5", parameters.get("udf5")[0]);
-		return mapPaymentResponeDetails;
 	}
 
 }
--- payment-service/src/main/java/com/amx/jax/payment/util/PaymentUtil.java
+++ payment-service/src/main/java/com/amx/jax/payment/util/PaymentUtil.java
@@ -4,7 +4,10 @@
 package com.amx.jax.payment.util;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.Map;
+
+import com.amx.amxlib.meta.model.PaymentResponseDto;
 
 /**
  * @author Viki Sangani
@@ -41,6 +44,45 @@ public class PaymentUtil {
 			}
 		}
 		return sbuf.toString();
+	}
+	
+	public static PaymentResponseDto  generatePaymentResponseDTO(HashMap<String, String> params){
+		PaymentResponseDto paymentResponseDto = new PaymentResponseDto();
+		
+		//paymentResponseDto.setApplicationCountryId(params.get(""));
+		paymentResponseDto.setAuth_appNo(params.get("auth_appNo"));
+		paymentResponseDto.setTransactionId(params.get("tranId"));
+		paymentResponseDto.setResultCode(params.get("result"));
+		paymentResponseDto.setPostDate(params.get("postDate"));
+		//paymentResponseDto.setCustomerId(params.get(""));
+		paymentResponseDto.setTrackId(params.get("trackId"));
+		paymentResponseDto.setReferenceId(params.get("referenceId"));
+		paymentResponseDto.setUdf1(params.get("udf1"));
+		paymentResponseDto.setUdf2(params.get("udf2"));
+		paymentResponseDto.setUdf3(params.get("udf3"));
+		paymentResponseDto.setUdf4(params.get("udf4"));
+		paymentResponseDto.setUdf5(params.get("udf5"));
+		
+		return paymentResponseDto;
+	}
+	
+	public static HashMap<String, String> generateParameterMapForPaymentCapture(Map<String, String[]> parameters) {
+
+		HashMap<String, String> mapPaymentResponeDetails = new HashMap<String, String>();
+		mapPaymentResponeDetails.put("paymentId", parameters.get("paymentid")[0]);
+		mapPaymentResponeDetails.put("result", parameters.get("result")[0]);
+		mapPaymentResponeDetails.put("auth_appNo", parameters.get("auth")[0]);
+		mapPaymentResponeDetails.put("referenceId", parameters.get("ref")[0]);
+		mapPaymentResponeDetails.put("postDate", parameters.get("postdate")[0]);
+		mapPaymentResponeDetails.put("trackId", parameters.get("trackid")[0]);
+		mapPaymentResponeDetails.put("tranId", parameters.get("tranid")[0]);
+		mapPaymentResponeDetails.put("responsecode", parameters.get("responsecode")[0]);
+		mapPaymentResponeDetails.put("udf1", parameters.get("udf1")[0]);
+		mapPaymentResponeDetails.put("udf2", parameters.get("udf2")[0]);
+		mapPaymentResponeDetails.put("udf3", parameters.get("udf3")[0]);
+		mapPaymentResponeDetails.put("udf4", parameters.get("udf4")[0]);
+		mapPaymentResponeDetails.put("udf5", parameters.get("udf5")[0]);
+		return mapPaymentResponeDetails;
 	}
 
 }
