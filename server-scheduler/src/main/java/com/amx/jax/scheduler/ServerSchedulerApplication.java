e43589e01c755124a680ca983d22e53a10678f81
Prashant <prashant.thorat@almullagroup.com>
2018-01-29 21:56:40 +0530

    scheduler : support multi tenancy

--- server-scheduler/src/main/java/com/amx/jax/scheduler/ServerSchedulerApplication.java
+++ server-scheduler/src/main/java/com/amx/jax/scheduler/ServerSchedulerApplication.java
@@ -14,6 +14,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -132,6 +133,7 @@ public class ServerSchedulerApplication implements SchedulingConfigurer, AsyncCo
 	}
 
 	@Bean
+	@Scope("prototype")
 	public RateAlertTask rateAlertTask() {
 		return new RateAlertTask();
 	}
