1476e4d04e631b9a5dbaef9ce03542e396dd3648
LnT <lalit.tanwar07@gmail.com>
2018-02-15 11:16:32 +0530

    Async Enabled

--- ms-jax/src/main/java/com/amx/jax/JaxServiceApplication.java
+++ ms-jax/src/main/java/com/amx/jax/JaxServiceApplication.java
@@ -1,32 +1,33 @@
 package com.amx.jax;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
-@ComponentScan(basePackages= {"com.amx.jax"})
-public class JaxServiceApplication  {
-	
+@ComponentScan(basePackages = { "com.amx.jax" })
+@EnableAsync
+public class JaxServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JaxServiceApplication.class, args);
 	}
-	
-//	@Autowired
-//	@Qualifier("dataSourcesJax")
-//	Map<String, DataSource> result;
-//	
-//	private void initDBCredentails() {
-//		
-//		
-//		
-//			DataSourceBuilder factory = DataSourceBuilder.create().url(dsProperties.getUrl())
-//					.username(dsProperties.getUsername()).password(dsProperties.getPassword())
-//					.driverClassName("oracle");
-//			result.put(dsProperties.getTenantId(), factory.build());
-//	}
+
+	// @Autowired
+	// @Qualifier("dataSourcesJax")
+	// Map<String, DataSource> result;
+	//
+	// private void initDBCredentails() {
+	//
+	//
+	//
+	// DataSourceBuilder factory =
+	// DataSourceBuilder.create().url(dsProperties.getUrl())
+	// .username(dsProperties.getUsername()).password(dsProperties.getPassword())
+	// .driverClassName("oracle");
+	// result.put(dsProperties.getTenantId(), factory.build());
+	// }
 }
