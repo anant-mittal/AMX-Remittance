e0df016116635f50bf7f51e06fe6c62cafffad85
LnT <lalit.tanwar07@gmail.com>
2018-02-16 10:35:40 +0530

    Servlest Container added

--- ms-jax/src/main/java/com/amx/jax/JaxServiceApplication.java
+++ ms-jax/src/main/java/com/amx/jax/JaxServiceApplication.java
@@ -1,32 +1,46 @@
 package com.amx.jax;
 
+import javax.servlet.ServletContextListener;
+import javax.validation.constraints.NotNull;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import com.amx.jax.postman.client.PostManContextListener;
+
 @SpringBootApplication
 @EnableTransactionManagement
-@ComponentScan(basePackages= {"com.amx.jax"})
-public class JaxServiceApplication  {
-	
+@ComponentScan(basePackages = { "com.amx.jax" })
+public class JaxServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JaxServiceApplication.class, args);
 	}
-	
-//	@Autowired
-//	@Qualifier("dataSourcesJax")
-//	Map<String, DataSource> result;
-//	
-//	private void initDBCredentails() {
-//		
-//		
-//		
-//			DataSourceBuilder factory = DataSourceBuilder.create().url(dsProperties.getUrl())
-//					.username(dsProperties.getUsername()).password(dsProperties.getPassword())
-//					.driverClassName("oracle");
-//			result.put(dsProperties.getTenantId(), factory.build());
-//	}
+
+	@NotNull
+	@Bean
+	ServletListenerRegistrationBean<ServletContextListener> myServletListener() {
+		ServletListenerRegistrationBean<ServletContextListener> srb = new ServletListenerRegistrationBean<>();
+		srb.setListener(new PostManContextListener());
+		return srb;
+	}
+
+	// @Autowired
+	// @Qualifier("dataSourcesJax")
+	// Map<String, DataSource> result;
+	//
+	// private void initDBCredentails() {
+	//
+	//
+	//
+	// DataSourceBuilder factory =
+	// DataSourceBuilder.create().url(dsProperties.getUrl())
+	// .username(dsProperties.getUsername()).password(dsProperties.getPassword())
+	// .driverClassName("oracle");
+	// result.put(dsProperties.getTenantId(), factory.build());
+	// }
 }
