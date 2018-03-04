3edccb7a0e2518daf5d935184d923a3240fe38a5
Mir Rabil uddin <rabil@amgdom.com>
2018-03-04 14:10:25 +0300

    day wise trnx limit check from new parameter table

--- ms-meta/src/main/java/com/amx/jax/repository/AuthenticationLimitCheckDAO.java
+++ ms-meta/src/main/java/com/amx/jax/repository/AuthenticationLimitCheckDAO.java
@@ -23,7 +23,7 @@ public interface AuthenticationLimitCheckDAO extends JpaRepository<Authenticatio
 	public AuthenticationLimitCheckView getTop1OnlineTxnLimit();
 	
 	
-	@Query("select cts from AuthenticationLimitCheckView cts where authorizationType=13")
+	@Query("select cts from AuthenticationLimitCheckView cts where authorizationType='13'")
 	public AuthenticationLimitCheckView getPerBeneTxnLimit();
 	
 	
