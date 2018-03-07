659150e5eed385678f5b94ecb942df5696c88b17
LnT <lalit.tanwar07@gmail.com>
2018-03-07 13:23:26 +0530

    Templaye with jasper done

--- ms-postman/src/main/java/com/amx/jax/postman/api/PostManControllerTest.java
+++ ms-postman/src/main/java/com/amx/jax/postman/api/PostManControllerTest.java
@@ -34,7 +34,7 @@ import com.amx.jax.postman.model.Templates;
 import com.bootloaderjs.IoUtils;
 import com.bootloaderjs.JsonUtil;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.itextpdf.text.DocumentException;
+import com.lowagie.text.DocumentException;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
