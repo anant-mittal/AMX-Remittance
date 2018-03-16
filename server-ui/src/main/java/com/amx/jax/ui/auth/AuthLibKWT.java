f8892a390a4610755311f532645c21ab7fedc9e7
LnT <lalit.tanwar07@gmail.com>
2018-03-16 17:50:49 +0530

    Added suppor for libs

--- server-ui/src/main/java/com/amx/jax/ui/auth/AuthLibKWT.java
+++ server-ui/src/main/java/com/amx/jax/ui/auth/AuthLibKWT.java
@@ -7,7 +7,7 @@ import com.amx.jax.scope.TenantSpecific;
 import com.amx.jax.ui.auth.AuthLibContext.AuthLib;
 
 @Component
-@TenantSpecific(Tenant.KWT)
+@TenantSpecific({ Tenant.KWT, Tenant.BHR, Tenant.KWT2 })
 public class AuthLibKWT implements AuthLib {
 
 	@Override
