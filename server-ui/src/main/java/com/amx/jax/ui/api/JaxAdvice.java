c1771e9dd27d7104d44c63969df8ef21c74a51e5
LnT <lalit.tanwar07@gmail.com>
2018-03-16 18:12:35 +0530

    JaxAdvice Changes

--- server-ui/src/main/java/com/amx/jax/ui/api/JaxAdvice.java
+++ server-ui/src/main/java/com/amx/jax/ui/api/JaxAdvice.java
@@ -5,17 +5,29 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.amx.amxlib.exception.AbstractException;
+import com.amx.jax.logger.AuditService;
+import com.amx.jax.logger.events.AuthEvent;
+import com.amx.jax.ui.auth.AuthState;
+import com.amx.jax.ui.auth.AuthState.AuthFlow;
 import com.amx.jax.ui.response.ResponseStatus;
 import com.amx.jax.ui.response.ResponseWrapper;
+import com.amx.jax.ui.session.GuestSession;
 
 @ControllerAdvice
 public class JaxAdvice {
+
+	@Autowired
+	private AuditService auditService;
+
+	@Autowired
+	private GuestSession guestSession;
 
 	private Logger LOG = LoggerFactory.getLogger(JaxAdvice.class);
 
@@ -25,6 +37,10 @@ public class JaxAdvice {
 		ResponseWrapper<Object> wrapper = new ResponseWrapper<Object>();
 		wrapper.setMessage(ResponseStatus.UNKNOWN_JAX_ERROR, exc);
 		LOG.error(ResponseStatus.UNKNOWN_JAX_ERROR.toString(), exc);
+		AuthState state = guestSession.getState();
+		if (state.getFlow() == AuthFlow.LOGIN) {
+			auditService.log(new AuthEvent(AuthEvent.Type.LOGIN_ATTEMPT, state.getnStep(), false));
+		}
 		return new ResponseEntity<ResponseWrapper<Object>>(wrapper, HttpStatus.OK);
 	}
 
