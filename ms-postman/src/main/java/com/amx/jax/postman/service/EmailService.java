14abe527be9d59c5157d1bf9a8deb212047bbd74
LnT <lalit.tanwar07@gmail.com>
2018-03-26 13:15:22 +0530

    Multimail Config

--- ms-postman/src/main/java/com/amx/jax/postman/service/EmailService.java
+++ ms-postman/src/main/java/com/amx/jax/postman/service/EmailService.java
@@ -1,29 +1,35 @@
 package com.amx.jax.postman.service;
 
 import java.io.IOException;
+import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 
 import com.amx.jax.postman.model.Email;
 import com.amx.jax.postman.model.File;
+import com.amx.jax.scope.TenantScoped;
+import com.amx.jax.scope.TenantValue;
 import com.amx.utils.Constants;
 import com.amx.utils.Utils;
 
+@TenantScoped
 @Component
 public class EmailService {
 
-	private Logger log = Logger.getLogger(getClass());
+	private static final Logger LOGGER = LoggerFactory.getLogger(EmailService.class);
 
 	@Autowired
 	private TemplateService templateService;
@@ -31,8 +37,47 @@ public class EmailService {
 	@Autowired
 	private FileService fileService;
 
-	@Autowired
+	@TenantValue("${tenant}")
+	private String tenant;
+	@TenantValue("${spring.mail.from}")
+	private String mailFrom;
+	@TenantValue("${spring.mail.host}")
+	private String mailHost;
+	@TenantValue("${spring.mail.port}")
+	private int mailPort;
+	@TenantValue("${spring.mail.username}")
+	private String mailUsername;
+	@TenantValue("${spring.mail.password}")
+	private String mailPassword;
+	@TenantValue("${spring.mail.properties.mail.smtp.auth}")
+	private boolean mailSmtpAuth;
+	@TenantValue("${spring.mail.properties.mail.smtp.starttls.enable}")
+	private boolean mailSmtpTls;
+	@TenantValue("${spring.mail.protocol}")
+	private String mailProtocol;
+	@TenantValue("${spring.mail.defaultEncoding}")
+	private String mailDefaultEncoding;
+
 	private JavaMailSender mailSender;
+
+	@Autowired
+	public void setMailSender(JavaMailSender mailSender) {
+		if (tenant != null) {
+			JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+			javaMailSender.setHost(this.mailHost);
+			javaMailSender.setPort(this.mailPort);
+			javaMailSender.setUsername(this.mailUsername);
+			javaMailSender.setPassword(this.mailPassword);
+			Properties mailProp = new Properties();
+			mailProp.put("mail.smtp.auth", mailSmtpAuth);
+			mailProp.put("mail.smtp.starttls.enable", mailSmtpTls);
+			javaMailSender.setProtocol(this.mailProtocol);
+			javaMailSender.setDefaultEncoding(mailDefaultEncoding);
+			this.mailSender = javaMailSender;
+		} else {
+			this.mailSender = mailSender;
+		}
+	}
 
 	@Value("${spring.mail.from}")
 	private String defaultSender;
@@ -43,7 +88,7 @@ public class EmailService {
 			try {
 				sendHtmlMail(eParams);
 			} catch (MessagingException | IOException e) {
-				log.error("Could not send email to : " + Utils.concatenate(eParams.getTo(), ",") + " Error = {}", e);
+				LOGGER.error("Could not send email to : " + Utils.concatenate(eParams.getTo(), ",") + " Error = {}", e);
 			}
 		} else {
 			sendPlainTextMail(eParams);
