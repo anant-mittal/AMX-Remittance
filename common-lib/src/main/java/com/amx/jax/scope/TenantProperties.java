d77838697682723d02a54b97132786251c17ced1
LnT <lalit.tanwar07@gmail.com>
2018-03-26 15:44:14 +0530

    Multimail Conig

--- common-lib/src/main/java/com/amx/jax/scope/TenantProperties.java
+++ common-lib/src/main/java/com/amx/jax/scope/TenantProperties.java
@@ -13,16 +13,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
+import com.amx.utils.ArgUtil;
+
 @Component
 public class TenantProperties {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(TenantProperties.class);
+	private static TenantProperties properties = new TenantProperties();
 
 	private static Environment envProperties;
 
 	@Autowired
 	public TenantProperties(Environment envProperties) {
 		TenantProperties.envProperties = envProperties;
+	}
+
+	public TenantProperties() {
 	}
 
 	public static Environment getEnvProperties() {
@@ -33,7 +39,7 @@ public class TenantProperties {
 		Properties tenantProperties = new Properties();
 		String propertyFile = "application." + tenant + ".properties";
 		File jarPath = new File(
-				object.getClass().getProtectionDomain().getCodeSource().getLocation().getPath().split("!")[0]);
+				properties.getClass().getProtectionDomain().getCodeSource().getLocation().getPath().split("!")[0]);
 		String propertiesPath = jarPath.getParent();
 
 		InputStream inSideInputStream = null;
@@ -41,10 +47,11 @@ public class TenantProperties {
 
 		try {
 
-			URL u = object.getClass().getClassLoader().getResource("classpath:" + propertyFile);
+			URL u = properties.getClass().getClassLoader().getResource("classpath:" + propertyFile);
 			if (u != null) {
 				inSideInputStream = object.getClass().getClassLoader().getResourceAsStream(u.getPath());
 				tenantProperties.load(inSideInputStream);
+				LOGGER.info("reading inside property file : {}", u.getPath());
 			}
 
 			URL u2 = object.getClass().getClassLoader().getResource(propertiesPath + "/" + propertyFile);
@@ -52,15 +59,26 @@ public class TenantProperties {
 				outSideInputStream = object.getClass().getClassLoader()
 						.getResourceAsStream(propertiesPath + "/" + propertyFile);
 				tenantProperties.load(outSideInputStream);
+				LOGGER.info("reading outside property file : {}", u2.getPath());
 			}
 
 			for (Field field : object.getClass().getDeclaredFields()) {
 				if (field.isAnnotationPresent(TenantValue.class)) {
 					TenantValue annotation = field.getAnnotation(TenantValue.class);
 					String propertyName = annotation.value().replace("${", "").replace("}", "");
-					String propertyValue = tenantProperties.getProperty(propertyName);
-					field.setAccessible(true);
-					field.set(object, propertyValue);
+					Object propertyValue = tenantProperties.getProperty(propertyName);
+					if (propertyValue != null) {
+						// ArgUtil.parseAsT(propertyValue, defaultValue, required)
+						String typeName = field.getGenericType().getTypeName();
+						field.setAccessible(true);
+						if ("java.lang.String".equals(typeName)) {
+							field.set(object, propertyValue);
+						} else if ("int".equals(typeName) || "java.lang.Integer".equals(typeName)) {
+							field.set(object, ArgUtil.parseAsInteger(propertyValue));
+						} else if ("boolean".equals(typeName) || "java.lang.Boolean".equals(typeName)) {
+							field.set(object, ArgUtil.parseAsBoolean(propertyValue));
+						}
+					}
 				}
 			}
 		} catch (IllegalArgumentException | IllegalAccessException | IOException e) {
