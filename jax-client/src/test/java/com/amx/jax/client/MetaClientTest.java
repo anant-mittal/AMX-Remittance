9dcdde1b32ad0a6944aad77b08fd189d5dcf594e
Viki Sangani <viki.sangani@almullagroup.com>
2018-03-29 16:25:15 +0530

    added test for country, state, district, currency

--- jax-client/src/test/java/com/amx/jax/client/MetaClientTest.java
+++ jax-client/src/test/java/com/amx/jax/client/MetaClientTest.java
@@ -15,6 +15,10 @@ import com.amx.amxlib.exception.InvalidInputException;
 import com.amx.amxlib.exception.ResourceNotFoundException;
 import com.amx.amxlib.meta.model.BankBranchDto;
 import com.amx.amxlib.meta.model.BankMasterDTO;
+import com.amx.amxlib.meta.model.CountryMasterDTO;
+import com.amx.amxlib.meta.model.CurrencyMasterDTO;
+import com.amx.amxlib.meta.model.ViewDistrictDto;
+import com.amx.amxlib.meta.model.ViewStateDto;
 import com.amx.amxlib.model.OnlineConfigurationDto;
 import com.amx.amxlib.model.request.GetBankBranchRequest;
 import com.amx.amxlib.model.response.ApiResponse;
@@ -47,7 +51,7 @@ public class MetaClientTest extends AbstractTestClient {
 		assertNotNull(response.getResult().getBankId());
 	}
 	
-	@Test
+	//@Test
 	public void testgetBankBranchListByBankId() throws IOException, ResourceNotFoundException, InvalidInputException {
 		setDefaults();
 		ApiResponse<BankBranchDto> response = null;
@@ -59,4 +63,49 @@ public class MetaClientTest extends AbstractTestClient {
 		assertNotNull(response.getResult().getBankId());
 	}
 
+	//@Test
+	public void testGetAllOnlineCurrency() {
+		setDefaults();
+		ApiResponse<CurrencyMasterDTO> response = null;
+		response = metaclient.getAllOnlineCurrency();
+		assertNotNull("Response is null", response);
+		assertNotNull(response.getResult());
+		//assertNotNull(response.getResult().getCurrencyName());
+	}
+	
+	//@Test
+	public void testGetAllCountry() {
+		setDefaults();
+		ApiResponse<CountryMasterDTO> response = null;
+		response = metaclient.getAllCountry();
+		assertNotNull("Response is null", response);
+		assertNotNull(response.getResult());
+		//assertNotNull(response.getResult().getCountryName());
+		//assertNotNull(response.getResult().getNationality());
+	}
+	
+	//@Test
+	public void testGetStateList() {
+		setDefaults();
+		ApiResponse<ViewStateDto> response = null;
+		response = metaclient.getStateList(new BigDecimal(94));
+		assertNotNull("Response is null", response);
+		assertNotNull(response.getResult());
+		//assertNotNull(response.getResult().getStateName());
+	}
+	
+	//@Test
+	public void testGetDistrictList() {
+		
+		// 1 is for english
+		BigDecimal langCode = new BigDecimal(1);
+		BigDecimal stateCode = new BigDecimal(495);
+		
+		setDefaults();
+		ApiResponse<ViewDistrictDto> response = null;
+		response = metaclient.getDistrictList(langCode,stateCode);
+		assertNotNull("Response is null", response);
+		assertNotNull(response.getResult());
+		assertNotNull(response.getResult().getDistrictDesc());
+	}
 }
