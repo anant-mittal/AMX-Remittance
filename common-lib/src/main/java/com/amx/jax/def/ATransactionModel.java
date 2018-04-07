74cd04ac5eac0887c3bce4b1799e52ac16680793
Prashant <prashant.thorat@almullagroup.com>
2018-04-07 15:24:04 +0530

    adding logger

--- common-lib/src/main/java/com/amx/jax/def/ATransactionModel.java
+++ common-lib/src/main/java/com/amx/jax/def/ATransactionModel.java
@@ -37,6 +37,8 @@ public abstract class ATransactionModel<T> {
 			key = ArgUtil.parseAsString(ContextUtil.map().get(ContextUtil.TRACE_ID));
 			ContextUtil.map().put(AppConstants.TRANX_ID_XKEY, key);
 			LOGGER.info("************ Creating New Tranx Id {} *******************", key);
+		}else {
+			LOGGER.info("************ Exisitng Tranx Id {} *******************", key);
 		}
 		return key;
 	}
