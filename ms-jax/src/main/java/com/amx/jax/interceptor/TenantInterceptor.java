c4b2d10771d3f6e13f9dec388f0d821bd42553d4
Prashant <prashant.thorat@almullagroup.com>
2018-04-12 15:01:44 +0530

    swagger bug

--- ms-jax/src/main/java/com/amx/jax/interceptor/TenantInterceptor.java
+++ ms-jax/src/main/java/com/amx/jax/interceptor/TenantInterceptor.java
@@ -1,5 +1,7 @@
 package com.amx.jax.interceptor;
 
+import java.util.HashMap;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -12,20 +14,32 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import com.amx.jax.multitenant.TenantContext;
 import com.amx.jax.scope.TenantContextHolder;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Component
 public class TenantInterceptor extends HandlerInterceptorAdapter {
 
 	private Logger logger = LoggerFactory.getLogger(getClass());
-	
+
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
-		
+
 		String tnt = TenantContextHolder.currentSite().toString();
 		if (StringUtils.isNotBlank(tnt)) {
 			logger.info("current tenant: " + tnt);
 			TenantContext.setCurrentTenant(tnt);
+		}
+		String metaInfo = request.getHeader("meta-info");
+		if (!StringUtils.isEmpty(metaInfo)) {
+			TypeReference<HashMap<String, Object>> typeRef = new TypeReference<HashMap<String, Object>>() {
+			};
+			HashMap<String, Object> metaInfoMap = new ObjectMapper().readValue(metaInfo, typeRef);
+			if (metaInfoMap.get("tenant") != null) {
+				String tenant = (String) metaInfoMap.get("tenant");
+				TenantContext.setCurrentTenant(tenant);
+			}
 		}
 		return true;
 	}
