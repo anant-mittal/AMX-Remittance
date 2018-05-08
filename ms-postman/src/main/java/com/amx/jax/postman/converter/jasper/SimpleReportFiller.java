b4f5b82004873debc5b73a99764adda2ac91430b
LnT <lalit.tanwar07@gmail.com>
2018-05-08 15:34:37 +0530

    Report using Jasper done once

--- ms-postman/src/main/java/com/amx/jax/postman/converter/jasper/SimpleReportFiller.java
+++ ms-postman/src/main/java/com/amx/jax/postman/converter/jasper/SimpleReportFiller.java
@@ -14,6 +14,7 @@ import net.sf.jasperreports.engine.JasperCompileManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.util.JRSaver;
 
 public class SimpleReportFiller {
 
