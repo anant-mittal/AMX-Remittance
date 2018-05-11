81aa4d0031b3f99c83178d7d4d620aa82adeac34
Prashant <prashant.thorat@almullagroup.com>
2018-05-11 13:08:37 +0530

    mobile number validation + user exception

---
+++ ms-meta/src/main/java/com/amx/jax/validation/CountryMetaValidation.java
@@ -0,0 +1,38 @@
+package com.amx.jax.validation;
+
+import java.math.BigDecimal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.amx.amxlib.error.JaxError;
+import com.amx.jax.exception.GlobalException;
+import com.amx.jax.service.CountryService;
+
+/**
+ * @author Prashant
+ *         <p>
+ *         This class is used to validate country meta data
+ *         </p>
+ */
+@Component
+public class CountryMetaValidation {
+
+	@Autowired
+	CountryService countryService;
+
+	/**
+	 * @param countryId
+	 * @param mobile
+	 *            - mobile number to validate
+	 * 
+	 */
+	public void validateMobileNumberLength(BigDecimal countryId, String mobile) {
+		BigDecimal mobileLength = countryService.getCountryMaster(countryId).getCountryMobileLength();
+		int userMobLength = mobile.length();
+
+		if (mobileLength != null && userMobLength != mobileLength.intValue()) {
+			throw new GlobalException("Mobile Number length is not correct.", JaxError.INCORRECT_LENGTH);
+		}
+	}
+}
--- ms-user/src/main/java/com/amx/jax/userservice/validation/CustomerPersonalDetailValidator.java
+++ ms-user/src/main/java/com/amx/jax/userservice/validation/CustomerPersonalDetailValidator.java
@@ -1,35 +1,30 @@
 package com.amx.jax.userservice.validation;
 
-import java.math.BigDecimal;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
-import com.amx.amxlib.error.JaxError;
 import com.amx.amxlib.model.CustomerPersonalDetail;
-import com.amx.jax.exception.GlobalException;
 import com.amx.jax.meta.MetaData;
 import com.amx.jax.repository.IServiceApplicabilityRuleDao;
 import com.amx.jax.scope.TenantContext;
 import com.amx.jax.trnx.CustomerRegistrationTrnxModel;
 import com.amx.jax.userservice.service.CustomerValidationContext.CustomerValidation;
+import com.amx.jax.validation.CountryMetaValidation;
 
 @Component
 public class CustomerPersonalDetailValidator implements Validator {
 
 	@Autowired
 	IServiceApplicabilityRuleDao serviceApplicabilityRuleDao;
-
 	@Autowired
 	MetaData metaData;
-
 	@Autowired
 	TenantContext<CustomerValidation> tenantContext;
-
 	@Autowired
-	ValidationClients validationClients;
+	CountryMetaValidation countryMetaValidation;
 
 	@Override
 	public boolean supports(Class clazz) {
@@ -41,15 +36,8 @@ public class CustomerPersonalDetailValidator implements Validator {
 		CustomerRegistrationTrnxModel beneficiaryTrnxModel = (CustomerRegistrationTrnxModel) target;
 		CustomerPersonalDetail customerPersonalDetail = beneficiaryTrnxModel.getCustomerPersonalDetail();
 		tenantContext.get().validateCivilId(customerPersonalDetail.getIdentityInt());
-		validateMobileNumberLength(customerPersonalDetail.getCountryId(), customerPersonalDetail.getMobile());
-	}
-
-	protected void validateMobileNumberLength(BigDecimal countryId, String mobile) {
-
-		ValidationClient validationClient = validationClients.getValidationClient(countryId.toString());
-		if (validationClient != null && !validationClient.isValidMobileNumber(mobile)) {
-			throw new GlobalException("Mobile Number length is not correct.", JaxError.INCORRECT_LENGTH);
-		}
+		countryMetaValidation.validateMobileNumberLength(customerPersonalDetail.getCountryId(),
+				customerPersonalDetail.getMobile());
 	}
 
 }
