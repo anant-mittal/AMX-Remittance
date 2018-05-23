866e3a9267d217030fde0576b71632fe800bb753
Animesh <kanimesh.s@gmail.com>
2018-05-23 15:25:32 +0530

    undo url change, toast delay increased

--- lib-postman/src/main/java/com/amx/jax/postman/PostManUrls.java
+++ lib-postman/src/main/java/com/amx/jax/postman/PostManUrls.java
@@ -8,7 +8,7 @@ public final class PostManUrls {
 	public static final String SEND_EMAIL_SUPPORT = "/postman/email/support";
 	public static final String NOTIFY_SLACK = "/postman/slack/notify";
 	public static final String NOTIFY_PUSH = "/postman/push/notify";
-	public static final String NOTIFY_PUSH_SUBSCRIBE = "/postman/subscribe{topic}";
+	public static final String NOTIFY_PUSH_SUBSCRIBE = "/postman/subscribe/{topic}";
 	public static final String NOTIFY_SLACK_EXCEP = "/postman/slack/exception";
 	public static final String PROCESS_TEMPLATE = "/postman/template/process";
 	public static final String PROCESS_TEMPLATE_FILE = "/postman/template/file";
