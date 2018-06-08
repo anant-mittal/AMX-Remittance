70455bc1a3f4aa6d5fff19cc4a45fecb72e143a4
LnT <lalit.tanwar07@gmail.com>
2018-06-08 15:48:22 +0530

    Import fixes

--- core/src/main/java/com/amx/jax/dbmodel/meta/ServiceMaster.java
+++ core/src/main/java/com/amx/jax/dbmodel/meta/ServiceMaster.java
@@ -2,11 +2,8 @@ package com.amx.jax.dbmodel.meta;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -15,7 +12,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
