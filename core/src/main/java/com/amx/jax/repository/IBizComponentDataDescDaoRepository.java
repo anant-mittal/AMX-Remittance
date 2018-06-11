4b4675529d707c7edb9ac17f1ebd00188641e99a
Prashant <prashant.thorat@almullagroup.com>
2018-06-11 17:01:21 +0530

    p.r.

--- core/src/main/java/com/amx/jax/repository/IBizComponentDataDescDaoRepository.java
+++ core/src/main/java/com/amx/jax/repository/IBizComponentDataDescDaoRepository.java
@@ -6,11 +6,14 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import com.amx.jax.dbmodel.BizComponentData;
 import com.amx.jax.dbmodel.BizComponentDataDesc;
 import com.amx.jax.dbmodel.LanguageType;
 
-public interface IBizComponentDataDescDaoRepository extends JpaRepository<BizComponentDataDesc, Serializable> 
-{
+public interface IBizComponentDataDescDaoRepository extends JpaRepository<BizComponentDataDesc, Serializable> {
 	@Query("select a from BizComponentDataDesc a where a.dataDesc= ?1 and a.fsLanguageType = ?2")
 	public List<BizComponentDataDesc> getComponentId(String inputString, LanguageType langId);
+
+	public List<BizComponentDataDesc> findByFsBizComponentDataAndFsLanguageType(BizComponentData bizComponentData,
+			LanguageType langId);
 }
