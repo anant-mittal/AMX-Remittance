cc47af913b49295a102dfb059376b0a0cca57196
Prashant <prashant.thorat@almullagroup.com>
2018-06-13 15:01:13 +0530

    adding hbm entitiues

--- ms-jax/src/main/java/com/amx/jax/routing/IfscRoutingLogic.java
+++ ms-jax/src/main/java/com/amx/jax/routing/IfscRoutingLogic.java
@@ -1,5 +1,6 @@
 package com.amx.jax.routing;
 
+import java.math.BigDecimal;
 import java.util.Map;
 
 import org.slf4j.Logger;
@@ -21,7 +22,18 @@ public class IfscRoutingLogic implements IRoutingLogic {
 	@Override
 	public void apply(Map<String, Object> input, Map<String, Object> output) {
 		LOGGER.info("in ifsc routing logic with input, {}", input);
-		
+		BigDecimal rouringBankId = null;
+		BigDecimal routingCountryId = (BigDecimal) input.get("P_ROUTING_COUNTRY_ID");
+		BigDecimal beneBankId = (BigDecimal) input.get("P_BENEFICIARY_BANK_ID");
+		try {
+			rouringBankId = jdbcTemplate.queryForObject("SELECT   ROUTING_BANK_ID"
+					+ "          FROM     EX_ROUTING_HEADER" + "          WHERE    SERVICE_MASTER_ID = 102"
+					+ "          AND      ROUTING_COUNTRY_ID =  94 " + "          AND      NVL(ISACTIVE,'')='Y' ;",
+					BigDecimal.class);
+		} catch (Exception e) {
+			LOGGER.error("Duplicate Routing setup  for  Indian Bank", e.getMessage());
+		}
+
 		LOGGER.info("in ifsc routing logic with output, {}", output);
 	}
 
