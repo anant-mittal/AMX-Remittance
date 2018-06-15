811b3bf4f0eb67fa51cc7f94e1a6e13267e4e3ac
LnT <lalit.tanwar07@gmail.com>
2018-06-15 18:41:06 +0530

    ms Auth Fixes

--- ms-auth/src/main/java/com/amx/jax/auth/AuthServiceConfig.java
+++ ms-auth/src/main/java/com/amx/jax/auth/AuthServiceConfig.java
@@ -1,18 +1,18 @@
 package com.amx.jax.auth;
 
-import java.io.File;
 import java.util.Locale;
 
 import org.springframework.stereotype.Component;
 
 import com.amx.jax.dict.Language;
+import com.amx.jax.postman.model.File;
 import com.amx.jax.scope.TenantScoped;
 import com.amx.jax.scope.TenantValue;
 
 @TenantScoped
 @Component
 public class AuthServiceConfig {
-	
+
 	@TenantValue("${tenant}")
 	private String tenant;
 
--- ms-auth/src/main/java/com/amx/jax/auth/service/AuthServiceImpl.java
+++ ms-auth/src/main/java/com/amx/jax/auth/service/AuthServiceImpl.java
@@ -1,17 +1,16 @@
 package com.amx.jax.auth.service;
 
-import java.util.logging.Logger;
-
-import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import com.amx.jax.logger.LoggerService;
 import com.amx.jax.service.AuthService;
 
 @Component
 public class AuthServiceImpl implements AuthService {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(AuthServiceImpl.class);
+	private static final Logger LOGGER = LoggerService.getLogger(AuthServiceImpl.class);
 
 	public AuthServiceImpl() {
 	}
