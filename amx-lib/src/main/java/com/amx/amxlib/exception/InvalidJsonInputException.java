8253db4dfa28b737b4b3d84e96b412859945a6d7
Chetan Pawar <chetan.pawar@almullaexchange.com>
2018-06-21 17:00:05 +0530

    exxpc fix

--- amx-lib/src/main/java/com/amx/amxlib/exception/InvalidJsonInputException.java
+++ amx-lib/src/main/java/com/amx/amxlib/exception/InvalidJsonInputException.java
@@ -1,9 +1,15 @@
 package com.amx.amxlib.exception;
 
+import com.amx.jax.exception.AmxApiError;
+
 public class InvalidJsonInputException extends AbstractJaxException {
 
 	public InvalidJsonInputException(String errorMessage) {
 		super(errorMessage);
+	}
+	
+	public InvalidJsonInputException(AmxApiError error) {
+		super(error);
 	}
 
 	private static final long serialVersionUID = 1L;
