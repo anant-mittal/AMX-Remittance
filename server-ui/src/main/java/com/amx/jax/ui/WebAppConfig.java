c5b07bc37ecffc14cb142805867453574b09956e
LnT <lalit.tanwar07@gmail.com>
2018-06-22 15:24:19 +0530

    Notification gap Added to Meta

--- server-ui/src/main/java/com/amx/jax/ui/WebAppConfig.java
+++ server-ui/src/main/java/com/amx/jax/ui/WebAppConfig.java
@@ -1,5 +1,6 @@
 package com.amx.jax.ui;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.amx.jax.scope.TenantScoped;
@@ -15,12 +16,40 @@ public class WebAppConfig {
 	@TenantValue("${application.title}")
 	private String appTitle;
 
+	@Value("${ui.features}")
+	private String[] elementToSearch;
+
+	@Value("${notification.sleep.gap}")
+	private String notificationGap;
+
+	@Value("${notification.range.long}")
+	private String notifyRangeLong;
+
+	@Value("${notification.range.short}")
+	private String notifyRangeShort;
+
 	public String[] getFeatures() {
 		return features;
 	}
 
 	public String getAppTitle() {
 		return appTitle;
+	}
+
+	public String getNotificationGap() {
+		return notificationGap;
+	}
+
+	public String[] getElementToSearch() {
+		return elementToSearch;
+	}
+
+	public String getNotifyRangeLong() {
+		return notifyRangeLong;
+	}
+
+	public String getNotifyRangeShort() {
+		return notifyRangeShort;
 	}
 
 }
