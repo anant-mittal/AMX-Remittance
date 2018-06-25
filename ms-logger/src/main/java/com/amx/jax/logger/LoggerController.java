48bc74e2679b97f41eae6782fd70112b10f9adc5
LnT <lalit.tanwar07@gmail.com>
2018-06-25 15:42:31 +0530

    Audit Logger

--- ms-logger/src/main/java/com/amx/jax/logger/LoggerController.java
+++ ms-logger/src/main/java/com/amx/jax/logger/LoggerController.java
@@ -1,5 +1,7 @@
 package com.amx.jax.logger;
 
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -8,9 +10,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.amx.jax.logger.client.AuditServiceClient;
+import com.amx.jax.logger.events.CActivityEvent;
 import com.amx.jax.logger.events.SessionEvent;
 import com.amx.jax.logger.repository.AuditLogRepository;
-import com.amx.jax.tunnel.TunnelClient;
 import com.amx.jax.tunnel.TunnelService;
 
 @RestController
@@ -27,14 +30,27 @@ public class LoggerController {
 	@Autowired
 	TunnelService tunnelService;
 
-	/**
-	 * @task Sync DB perms
-	 */
-	@RequestMapping(value = "/api/event", method = RequestMethod.POST)
+	@Autowired
+	AuditService auditService;
+
+	@RequestMapping(value = "/api/event/map", method = RequestMethod.POST)
+	public void syncPermsMeta(@RequestBody Map<String, Object> map) {
+		AuditServiceClient.publishAbstractEvent(map);
+	}
+
+	@RequestMapping(value = "/api/event/SessionEvent", method = RequestMethod.POST)
 	public void syncPermsMeta(@RequestBody SessionEvent event) {
-		// mongoTemplate.save(event, "Event");
-		// auditLogRepository.save(event);
-		tunnelService.send(TunnelClient.TEST_TOPIC, event);
+		auditService.log(event);
+	}
+
+	@RequestMapping(value = "/api/event/CActivityEvent", method = RequestMethod.POST)
+	public void syncPermsMeta(@RequestBody CActivityEvent event) {
+		auditService.log(event);
+	}
+
+	@RequestMapping(value = "/api/event/AuditEvent", method = RequestMethod.POST)
+	public void syncPermsMeta(@RequestBody AuditEvent event) {
+		auditService.log(event);
 	}
 
 }
