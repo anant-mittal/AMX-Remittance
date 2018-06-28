c7acc7dd898e623dcedae4c6622607c3da2b8fd1
LnT <lalit.tanwar07@gmail.com>
2018-06-28 15:56:49 +0530

    Created Cleint For Auth Service

--- ms-auth/src/main/java/com/amx/jax/auth/api/AuthServiceController.java
+++ ms-auth/src/main/java/com/amx/jax/auth/api/AuthServiceController.java
@@ -9,8 +9,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.amx.jax.auth.perms.PermScope;
-import com.amx.jax.auth.perms.Permission;
+import com.amx.jax.auth.AuthConstants;
+import com.amx.jax.auth.models.PermScope;
+import com.amx.jax.auth.models.Permission;
 import com.amx.jax.auth.service.AuthServiceImpl;
 import com.amx.jax.logger.LoggerService;
 import com.amx.jax.postman.PostManException;
@@ -29,7 +30,7 @@ public class AuthServiceController {
 	 * @task Sync DB perms
 	 */
 	@ApiOperation("Sync Permissions")
-	@RequestMapping(value = "/api/sync/perms", method = RequestMethod.POST)
+	@RequestMapping(value = AuthConstants.ApiEndPoints.SYNC_PERMS, method = RequestMethod.POST)
 	public void syncPermsMeta() {
 
 	}
@@ -47,7 +48,7 @@ public class AuthServiceController {
 	 * @throws IOException
 	 */
 	@ApiOperation("User Auth")
-	@RequestMapping(value = "/api/user/auth", method = RequestMethod.GET)
+	@RequestMapping(value = AuthConstants.ApiEndPoints.USER_AUTH, method = RequestMethod.GET)
 	public void authUser(@RequestParam String empCode, @RequestParam String identity,
 			@RequestParam(required = false) String mOtp) {
 
@@ -59,7 +60,7 @@ public class AuthServiceController {
 	 * @param roleTitle
 	 */
 	@ApiOperation("Create Role")
-	@RequestMapping(value = "/api/role", method = RequestMethod.POST)
+	@RequestMapping(value = AuthConstants.ApiEndPoints.ROLE, method = RequestMethod.POST)
 	public void createRole(@RequestParam String roleTitle) {
 
 	}
@@ -72,7 +73,7 @@ public class AuthServiceController {
 	 * @param permScope
 	 */
 	@ApiOperation("Assign perms to Role")
-	@RequestMapping(value = "/api/role/perm", method = RequestMethod.POST)
+	@RequestMapping(value = AuthConstants.ApiEndPoints.ROLE_PERM, method = RequestMethod.POST)
 	public void assinPerm(@RequestParam String roleId, @RequestParam Permission permission,
 			@RequestParam(required = false) PermScope permScope) {
 
@@ -85,7 +86,7 @@ public class AuthServiceController {
 	 * @param userId
 	 */
 	@ApiOperation("Assign Role to user")
-	@RequestMapping(value = "/api/user/role", method = RequestMethod.POST)
+	@RequestMapping(value = AuthConstants.ApiEndPoints.USER_ROLE, method = RequestMethod.POST)
 	public void assinRole(@RequestParam String roleId, @RequestParam String userId) {
 
 	}
@@ -97,7 +98,7 @@ public class AuthServiceController {
 	 * @param userId
 	 */
 	@ApiOperation("Fetch User permissions")
-	@RequestMapping(value = "/api/user/perms", method = RequestMethod.GET)
+	@RequestMapping(value = AuthConstants.ApiEndPoints.USER_PERMS, method = RequestMethod.GET)
 	public void getUserPerms(@RequestParam String userId) {
 
 	}
--- ms-auth/src/main/java/com/amx/jax/auth/service/AuthServiceImpl.java
+++ ms-auth/src/main/java/com/amx/jax/auth/service/AuthServiceImpl.java
@@ -1,21 +1,14 @@
 package com.amx.jax.auth.service;
 
 import org.slf4j.Logger;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import com.amx.jax.auth.AuthService;
 import com.amx.jax.logger.LoggerService;
-import com.amx.jax.service.AuthService;
 
 @Component
 public class AuthServiceImpl implements AuthService {
 
 	private static final Logger LOGGER = LoggerService.getLogger(AuthServiceImpl.class);
-
-	public AuthServiceImpl() {
-	}
-
-	@Value("${default.tenant}")
-	String defaultTennatId;
 
 }
