96a4cc8b802572095aaae06f5ad15cff6eed1725
Prashant <prashant.thorat@almullagroup.com>
2018-06-28 11:08:33 +0530

    blist changes

--- ms-meta/src/main/java/com/amx/jax/repository/IBlackMasterRepository.java
+++ ms-meta/src/main/java/com/amx/jax/repository/IBlackMasterRepository.java
@@ -10,12 +10,9 @@ import com.amx.jax.dbmodel.BlackListModel;
 
 public interface IBlackMasterRepository extends JpaRepository<BlackListModel, Serializable> {
 
-	// @Query(value="select bm from BlackListModel where cName=?1")
-
-	@Query(value = "select bm  from BlackListModel  bm where cName=?1")
-	public List<BlackListModel> getBlackByName(String name);
-
 	@Query(value = "select bm  from BlackListModel  bm where cAName=?1")
 	public List<BlackListModel> findByarabicName(String localName);
+
+	public List<BlackListModel> findByCANameIgnoreCase(String name);
 
 }
