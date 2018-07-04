5333dd9d383cecb54d5917e141786e622dabcb5f
Prashant <prashant.thorat@almullagroup.com>
2018-07-04 16:20:26 +0530

    branch

--- ms-meta/src/main/java/com/amx/jax/dao/BranchDetailDao.java
+++ ms-meta/src/main/java/com/amx/jax/dao/BranchDetailDao.java
@@ -28,7 +28,7 @@ public class BranchDetailDao {
 		List<BranchDetailModel> branchDetail = jdbcTemplate.query(
 				"SELECT BRANCH_NAME, GOV_NAME, TELEPHONE_NUMBER, ADDRESS, LATITUDE, LONGITUDE from EX_COUNTRY_BRANCH  WHERE ISACTIVE='Y' AND "
 						+ "COUNTRY_ID=?  and CORPORATE_STATUS='N' and branch_name != 'ONLINE' and HEAD_OFFICE_INDICATOR=0  and "
-						+ " ADDRESS NOT NULL AND LATITUDE IS NOT NULL AND LONGITUDE IS NOT NULL ORDER BY BRANCH_NAME ASC",
+						+ " ADDRESS IS NOT NULL AND LATITUDE IS NOT NULL AND LONGITUDE IS NOT NULL ORDER BY BRANCH_NAME ASC",
 				new BranchDetailRowMapper(), countryId);
 
 		return branchDetail;
