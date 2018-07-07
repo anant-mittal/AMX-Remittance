03ff31917eecdbddffc080a021ce7c67e77052f6
Chetan Pawar <chetan.pawar@almullaexchange.com>
2018-07-07 14:12:26 +0530

    Bug Id 2047

--- amx-lib/src/main/java/com/amx/amxlib/model/CustomerHomeAddress.java
+++ amx-lib/src/main/java/com/amx/amxlib/model/CustomerHomeAddress.java
@@ -3,7 +3,9 @@ package com.amx.amxlib.model;
 import java.io.Serializable;
 import java.math.BigDecimal;
 
+import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 import org.springframework.format.annotation.NumberFormat;
@@ -26,13 +28,13 @@ public class CustomerHomeAddress implements Serializable {
 	private BigDecimal districtId;
 
 	@NotNull
-	@NumberFormat
-	@Size(min = 1)
+	@Pattern(regexp = "^[1-9]\\d*$",message="Invalid Mobile No")
+	@Size(min = 1)	
 	private String mobile;
 
 	/** country telephone prefix */
 	@NotNull
-	@NumberFormat
+	@Pattern(regexp = "[\\s]*[0-9]*[1-9]+",message="Invalid Tele Prefix")
 	private String telPrefix;
 
 	public BigDecimal getCountryId() {
