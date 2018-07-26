50aef04928a80589d28dbbbc26d53d0c32684586
abkamble <abkamble@gmail.com>
2018-07-26 08:34:11 +0530

    dependencies cleaned :)

--- ms-auth/src/main/java/com/amx/jax/auth/AuthServiceConfig.java
+++ ms-auth/src/main/java/com/amx/jax/auth/AuthServiceConfig.java
@@ -1,13 +1,8 @@
 package com.amx.jax.auth;
 
-import java.util.Locale;
-
 import org.springframework.stereotype.Component;
 
-import com.amx.jax.dict.Language;
-import com.amx.jax.postman.model.File;
 import com.amx.jax.scope.TenantScoped;
-import com.amx.jax.scope.TenantValue;
 
 @TenantScoped
 @Component
