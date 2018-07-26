363402cbae25534aaf8a9a11201475ea6ac02aa4
Chetan Pawar <chetan.pawar@almullaexchange.com>
2018-07-26 14:41:19 +0530

    COR : Initial Development of Audit Log

---
+++ ms-branch/src/main/java/com/amx/jax/auditlogs/JaxAuditEvent.java
@@ -0,0 +1,60 @@
+package com.amx.jax.auditlogs;
+
+import java.math.BigDecimal;
+
+import com.amx.amxlib.model.request.OffsiteCustomerRegistrationRequest;
+import com.amx.jax.logger.AuditEvent;
+
+public class JaxAuditEvent extends AuditEvent {
+
+	private static final long serialVersionUID = 7451732272992078549L;
+	//BigDecimal customerId;
+	
+	OffsiteCustomerRegistrationRequest model;
+
+	Boolean success;
+
+	public Boolean getSuccess() {
+		return success;
+	}
+
+	public void setSuccess(Boolean success) {
+		this.success = success;
+	}
+	
+	public static enum Type implements EventType {
+		SEND_OTP,VALIDATE_OTP;
+
+		@Override
+		public EventMarker marker() {
+			return EventMarker.AUDIT;
+		}
+	}
+
+
+	public JaxAuditEvent(EventType type) {
+		super(type);
+	}
+
+	/*public BigDecimal getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(BigDecimal customerId) {
+		this.customerId = customerId;
+		if (customerId != null) {
+			this.actorId = customerId.toString();
+		}
+	}*/
+
+	public OffsiteCustomerRegistrationRequest getModel() {
+		return model;
+	}
+
+	public void setModel(OffsiteCustomerRegistrationRequest model) {
+		this.model = model;		
+			
+	}
+	
+	
+}
