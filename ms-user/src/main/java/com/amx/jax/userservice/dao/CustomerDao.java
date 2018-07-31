1ad8f76f0f18cff4c8e6fe8824252ceee4ca9ce4
Subodh Bhoir <subodh.bhoir@almullagroup.com>
2018-07-31 17:42:30 +0530

    merge

--- ms-user/src/main/java/com/amx/jax/userservice/dao/CustomerDao.java
+++ ms-user/src/main/java/com/amx/jax/userservice/dao/CustomerDao.java
@@ -149,12 +149,8 @@ public class CustomerDao {
 				cust.setMobile(model.getMobile());
 			}
 			if(cust.getUpdatedBy() == null)
-<<<<<<<<< Temporary merge branch 1
-				cust.setUpdatedBy(cust.getCreatedBy());
-=========
-				cust.setUpdatedBy(cust.getCreatedBy());		
+				cust.setUpdatedBy(cust.getCreatedBy());			
 
->>>>>>>>> Temporary merge branch 2
 			customerRepo.save(cust);
 		}
 		
