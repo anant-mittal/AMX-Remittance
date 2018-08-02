599362e34fa821d6d2f0f12340211b86b3b42130
abkamble <abkamble@gmail.com>
2018-08-02 12:35:38 +0530

    dependencies cleaned.

--- ms-auth/src/main/java/com/amx/jax/rbaac/api/AuthServiceController.java
+++ ms-auth/src/main/java/com/amx/jax/rbaac/api/AuthServiceController.java
@@ -3,19 +3,14 @@ package com.amx.jax.rbaac.api;
 import java.io.IOException;
 import java.math.BigDecimal;
 
-import javax.validation.Valid;
-import javax.validation.constraints.Pattern;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.amx.jax.AppConstants;
 import com.amx.jax.api.AmxApiResponse;
 import com.amx.jax.api.BoolRespModel;
 import com.amx.jax.auth.AuthService;
