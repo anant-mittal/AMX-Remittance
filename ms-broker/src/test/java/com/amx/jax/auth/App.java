6a08172e4efa7926b4094d3fe6715d9861c73adb
Ahmad Salman <ahmad.salman@almullaexchange.com>
2018-08-11 12:20:09 +0300

    code clean

--- ms-broker/src/test/java/com/amx/jax/auth/App.java
+++ ms-broker/src/test/java/com/amx/jax/auth/App.java
@@ -10,28 +10,8 @@ public class App { // Noncompliant
 	 */
 
 	public static void main(String[] args) {
-		String input = "\u0628\u064A\u0627\u0646\u0627\u062A 177 \u0627\u0644\u063189\u0627\u0633\u0644";
-		System.out.println(input);
-
-		System.out.println(fixBiDi(input));
-
-		char[] temparray = input.toCharArray();
-		int left, right = 0;
-		right = temparray.length - 1;
-		for (left = 0; left < right; left++, right--) {
-			// Swap values of left and right
-			char temp = temparray[left];
-			temparray[left] = temparray[right];
-			temparray[right] = temp;
-		}
-		for (char c : temparray)
-			System.out.print(c);
-		System.out.println();
+	
 	}
 
-	@Async
-	public void myAsyncMethod(){
-		
-	}
 
 }
