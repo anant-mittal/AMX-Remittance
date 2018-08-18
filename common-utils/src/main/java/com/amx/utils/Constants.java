065e38075af4adce5c6676e8bb22d17b6544970a
Chetan Pawar <chetan.pawar@almullaexchange.com>
2018-08-18 16:21:27 +0530

    commit

--- common-utils/src/main/java/com/amx/utils/Constants.java
+++ common-utils/src/main/java/com/amx/utils/Constants.java
@@ -68,4 +68,8 @@ public class Constants {
 
 	/** The Constant IDENTITY_TYPE_ID. */
 	public static final String IDENTITY_TYPE_ID = "198";
+	
+	public static final String COMMON_NATIONALITY = "ALL";
+	
+	public static final String COMPONENT_NAME = "Identity Type";
 }
