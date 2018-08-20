90279348bf45425d2f7a4f8a740f045adc970500
LT <lalit.tanwar07@gmail.com>
2018-08-20 16:05:55 +0530

    XRate Changed

--- ms-worker/src/main/java/com/amx/jax/worker/listner/PlaceOrderListner.java
+++ ms-worker/src/main/java/com/amx/jax/worker/listner/PlaceOrderListner.java
@@ -1,5 +1,20 @@
 package com.amx.jax.worker.listner;
 
-public class PlaceOrderListner {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.amx.jax.event.AmxTunnelEvents;
+import com.amx.jax.tunnel.ITunnelSubscriber;
+import com.amx.jax.tunnel.TunnelEvent;
+
+@TunnelEvent(topic = AmxTunnelEvents.Names.XRATE_BEST_RATE_CHANGE, queued = true)
+public class PlaceOrderListner implements ITunnelSubscriber<Object> {
+
+	private final Logger LOGGER = LoggerFactory.getLogger(getClass());
+
+	@Override
+	public void onMessage(String channel, Object msg) {
+		LOGGER.info("======onMessage1==={} ====  {}", channel, msg);
+	}
 
 }
