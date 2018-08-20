7fee9a869739ab2d25dd69f604d31227c70d1a13
LT <lalit.tanwar07@gmail.com>
2018-08-20 16:23:14 +0530

    Events fixed

--- ms-worker/src/main/java/com/amx/jax/worker/listner/PlaceOrderListner.java
+++ ms-worker/src/main/java/com/amx/jax/worker/listner/PlaceOrderListner.java
@@ -4,17 +4,19 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.amx.jax.event.AmxTunnelEvents;
+import com.amx.jax.event.Event;
 import com.amx.jax.tunnel.ITunnelSubscriber;
 import com.amx.jax.tunnel.TunnelEvent;
+import com.amx.utils.JsonUtil;
 
 @TunnelEvent(topic = AmxTunnelEvents.Names.XRATE_BEST_RATE_CHANGE, queued = true)
-public class PlaceOrderListner implements ITunnelSubscriber<Object> {
+public class PlaceOrderListner implements ITunnelSubscriber<Event> {
 
 	private final Logger LOGGER = LoggerFactory.getLogger(getClass());
 
 	@Override
-	public void onMessage(String channel, Object msg) {
-		LOGGER.info("======onMessage1==={} ====  {}", channel, msg);
+	public void onMessage(String channel, Event event) {
+		LOGGER.info("======onMessage1==={} ====  {}", channel, JsonUtil.toJson(event));
 	}
 
 }
