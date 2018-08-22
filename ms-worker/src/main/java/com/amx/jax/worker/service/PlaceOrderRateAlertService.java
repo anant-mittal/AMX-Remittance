80f520cb551012a003e79ac9a07ab50ac07ce1ef
LT <lalit.tanwar07@gmail.com>
2018-08-22 15:07:24 +0530

    Fixes

--- ms-worker/src/main/java/com/amx/jax/worker/service/PlaceOrderRateAlertService.java
+++ ms-worker/src/main/java/com/amx/jax/worker/service/PlaceOrderRateAlertService.java
@@ -7,42 +7,42 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.amx.amxlib.model.PlaceOrderNotificationDTO;
 import com.amx.amxlib.model.response.ApiResponse;
 import com.amx.amxlib.model.response.ResponseStatus;
 import com.amx.jax.client.PlaceOrderClient;
-import com.amx.amxlib.model.PlaceOrderNotificationDTO;
 
 @Service
-@SuppressWarnings("rawtypes")
-public class PlaceOrderRateAlertService extends AbstractService{
-     
+public class PlaceOrderRateAlertService {
 
 	@Autowired
 	PlaceOrderClient placeOrderClient;
-	
+
 	@Autowired
 	NotificationService notificationService;
-	
-	private static final Logger LOGGER = Logger.getLogger(PlaceOrderRateAlertService.class);
-	
-	public ApiResponse<PlaceOrderNotificationDTO> rateAlertPlaceOrder(BigDecimal fromAmount,BigDecimal toAmount,BigDecimal countryId,BigDecimal currencyId,BigDecimal bankId ,BigDecimal derivedSellRate) {
 
-		ApiResponse<PlaceOrderNotificationDTO> response = getBlackApiResponse();
+	private static final Logger LOGGER = Logger.getLogger(PlaceOrderRateAlertService.class);
+
+	public ApiResponse<PlaceOrderNotificationDTO> rateAlertPlaceOrder(BigDecimal fromAmount, BigDecimal toAmount,
+			BigDecimal countryId, BigDecimal currencyId, BigDecimal bankId, BigDecimal derivedSellRate) {
+
+		ApiResponse<PlaceOrderNotificationDTO> response = ApiResponse.getBlackApiResponse();
 		try {
-			List<PlaceOrderNotificationDTO> placeOrderList =  placeOrderClient.getPlaceOrderOnTrigger(fromAmount, toAmount, countryId, currencyId, bankId,derivedSellRate).getResults();
-			if (placeOrderList != null &&  !placeOrderList.isEmpty()) {
+			List<PlaceOrderNotificationDTO> placeOrderList = placeOrderClient
+					.getPlaceOrderOnTrigger(fromAmount, toAmount, countryId, currencyId, bankId, derivedSellRate)
+					.getResults();
+			if (placeOrderList != null && !placeOrderList.isEmpty()) {
 				placeOrderDetails(placeOrderList);
 			}
 			response.getData().getValues().addAll(placeOrderList);
 			response.setResponseStatus(ResponseStatus.OK);
-		 } catch (Exception e) {
+		} catch (Exception e) {
 			response.setResponseStatus(ResponseStatus.INTERNAL_ERROR);
 			LOGGER.error("Error while fetching Place Order List by Trigger Exchange Rate");
 			e.printStackTrace();
 		}
 		return response;
 	}
-
 
 	private void placeOrderDetails(List<PlaceOrderNotificationDTO> placeOrderList) {
 		int batchSize = 10;
@@ -55,15 +55,4 @@ public class PlaceOrderRateAlertService extends AbstractService{
 		}
 	}
 
-
-	@Override
-	public String getModelType() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-	public String setExchangeRate()
-	{
-		return "******--- Service is up ---*******";
-	}
 }
