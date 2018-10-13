772b72ba6e8e6aa018af16b491d0bc412b42d2f4
LT <lalit.tanwar07@gmail.com>
2018-10-13 11:44:53 +0530

    Device Mapping Done

--- common-lib/src/main/java/com/amx/jax/dict/UserClient.java
+++ common-lib/src/main/java/com/amx/jax/dict/UserClient.java
@@ -2,20 +2,70 @@ package com.amx.jax.dict;
 
 public class UserClient {
 
+	/**
+	 * Business Channel
+	 * 
+	 * @author lalittanwar
+	 *
+	 */
 	public enum Channel {
 		ONLINE, KIOSK, MOBILE, BRANCH, THIRD_PARTY;
 	}
 
-	public enum AppType {
-		WEB, ANDROID, IOS, KIOSK, UNKNOWN;
-	}
-
+	/**
+	 * Physical Device type
+	 * 
+	 * @author lalittanwar
+	 *
+	 */
 	public enum DeviceType {
-		COMPUTER, MOBILE, TABLET, IPAD, UNKNOWN
+		COMPUTER, MOBILE, TABLET(MOBILE), IPAD(MOBILE), UNKNOWN(COMPUTER);
+
+		DeviceType parent;
+
+		DeviceType() {
+			this.parent = this;
+		}
+
+		DeviceType(DeviceType parent) {
+			this.parent = parent.getParent();
+		}
+
+		public DeviceType getParent() {
+			return parent;
+		}
+
+		public boolean isParentOf(DeviceType check) {
+			return (this == check || check.getParent() == this);
+		}
+
+		public boolean hasParent(DeviceType check) {
+			return check.isParentOf(this);
+		}
+
+		public boolean isRelated(DeviceType check) {
+			return check.isParentOf(this.parent);
+		}
 	}
 
+	/**
+	 * More of generic OS
+	 * 
+	 * @author lalittanwar
+	 *
+	 */
 	public enum DevicePlatform {
 		IOS, ANDROID, WINDOWS, MAC, LINUX, UNKNOWN
+	}
+
+	/**
+	 * Client Type, tech or language
+	 * 
+	 * @author lalittanwar
+	 *
+	 */
+	public enum AppType {
+		WEB, ANDROID, IOS, KIOSK, UNKNOWN;
 	}
 
 }
