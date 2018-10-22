ea9102980bb5bebde27f7eb46bdb733c8388ef79
LT <lalit.tanwar07@gmail.com>
2018-10-22 12:18:14 +0530

    UNused Imports removed

--- sw-adapter/src/main/java/com/amx/jax/adapter/SWAdapterLauncher.java
+++ sw-adapter/src/main/java/com/amx/jax/adapter/SWAdapterLauncher.java
@@ -1,33 +1,13 @@
 package com.amx.jax.adapter;
 
 import java.awt.EventQueue;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.security.KeyManagementException;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.UnrecoverableKeyException;
-import java.security.cert.CertificateException;
 
-import javax.net.ssl.SSLContext;
-
-import org.apache.http.client.HttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.ssl.SSLContextBuilder;
-import org.apache.tomcat.util.http.fileupload.FileUtils;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.util.ResourceUtils;
-import org.springframework.web.client.RestTemplate;
-
-import com.amx.utils.FileUtil;
 
 @SpringBootApplication
 @ComponentScan("com.amx.jax")
