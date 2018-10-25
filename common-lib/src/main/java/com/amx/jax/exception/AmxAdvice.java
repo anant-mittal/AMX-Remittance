1e658ffe6eab07bcb768e2722d30b472da3eaf18
LT <lalit.tanwar07@gmail.com>
2018-10-25 12:40:38 +0530

    Exceptions Fixes

--- common-lib/src/main/java/com/amx/jax/exception/AmxAdvice.java
+++ common-lib/src/main/java/com/amx/jax/exception/AmxAdvice.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
+import com.amx.jax.AppConstants;
 import com.amx.jax.api.AmxFieldError;
 import com.amx.jax.http.CommonHttpRequest;
 import com.amx.jax.logger.LoggerService;
@@ -31,12 +32,13 @@ public abstract class AmxAdvice {
 	@ResponseBody
 	public ResponseEntity<AmxApiError> handle(AmxApiException ex, HttpServletRequest request,
 			HttpServletResponse response) {
-		AmxApiError error = ex.createAmxApiError();
-		error.setException(ex.getClass().getName());
-		error.setStatusEnum(ex.getError());
-		error.setMeta(ex.getMeta());
+		AmxApiError apiError = ex.createAmxApiError();
+		apiError.setException(ex.getClass().getName());
+		apiError.setStatusEnum(ex.getError());
+		apiError.setMeta(ex.getMeta());
 		alert(ex);
-		return new ResponseEntity<AmxApiError>(error, getHttpStatus(ex));
+		response.setHeader(AppConstants.EXCEPTION_HEADER_KEY, apiError.getException());
+		return new ResponseEntity<AmxApiError>(apiError, getHttpStatus(ex));
 	}
 
 	public HttpStatus getHttpStatus(AmxApiException exp) {
@@ -59,6 +61,7 @@ public abstract class AmxAdvice {
 		apiError.setHttpStatus(HttpStatus.BAD_REQUEST);
 		apiError.setErrors(errors);
 		apiError.setException(ex.getClass().getName());
+		response.setHeader(AppConstants.EXCEPTION_HEADER_KEY, apiError.getException());
 		return new ResponseEntity<AmxApiError>(apiError, HttpStatus.BAD_REQUEST);
 	}
 
