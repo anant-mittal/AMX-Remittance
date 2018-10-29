5ee40e81792f23d1835a9bd98df6a8c8c06a7b56
Shivaraj <shivaraj.shetty@almullagroup.com>
2018-10-29 10:58:53 +0530

    Beneficiary spelling Change Bug 3581

--- ms-jax/src/main/java/com/amx/jax/services/BeneficiaryCheckService.java
+++ ms-jax/src/main/java/com/amx/jax/services/BeneficiaryCheckService.java
@@ -162,7 +162,7 @@ public class BeneficiaryCheckService extends AbstractService {
 			List<BeneficaryMaster> beneMasterList = beneficiaryMasterDao
 					.getBeneficiaryByBeneMasterId(beneDto.getBeneficaryMasterSeqId());
 			if (beneMasterList.isEmpty()) {
-				errorDesc = "INVALID BENEFICARY MASTER";
+				errorDesc = "INVALID BENEFICIARY MASTER";
 				errorStatusDto = this.setBeneError(JaxError.RECORD_NOT_FOUND.toString(), errorDesc);
 
 				errorListDto.add(errorStatusDto);
@@ -418,14 +418,14 @@ public class BeneficiaryCheckService extends AbstractService {
 
 				if (minLength > 0 && benePhoneLength < minLength) {
 					beneDto.setUpdateNeeded(true);
-					errorDesc = "BENEFICARY TELEPHONE NUMBER - MINIMUM LENGTH MUST BE ||" + minLength
+					errorDesc = "BENEFICIARY TELEPHONE NUMBER - MINIMUM LENGTH MUST BE ||" + minLength
 							+ " || AND MAXIMUM LENGTH MUST BE ||" + maxLength;
 					errorStatusDto = this.setBeneError(JaxError.INVALID_MOB_TELE.toString(), errorDesc);
 					errorListDto.add(errorStatusDto);
 				}
 				if (maxLength > 0 && benePhoneLength > maxLength) {
 					beneDto.setUpdateNeeded(true);
-					errorDesc = "BENEFICARY TELEPHONE NUMBER - MINIMUM LENGTH MUST BE ||" + minLength
+					errorDesc = "BENEFICIARY TELEPHONE NUMBER - MINIMUM LENGTH MUST BE ||" + minLength
 							+ " || AND MAXIMUM LENGTH MUST BE ||" + maxLength;
 					errorStatusDto = this.setBeneError(JaxError.INVALID_MOB_TELE.toString(), errorDesc);
 					errorListDto.add(errorStatusDto);
