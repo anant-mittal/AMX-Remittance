350db2a4712415a744410e4b185e6ba550864f74
LT <lalit.tanwar07@gmail.com>
2018-11-01 21:39:07 +0530

    Updated

--- sw-adapter/src/main/java/com/amx/jax/adapter/SWAdapterController.java
+++ sw-adapter/src/main/java/com/amx/jax/adapter/SWAdapterController.java
@@ -1,14 +1,15 @@
 package com.amx.jax.adapter;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.amx.jax.device.CardReader;
 
-@RestController
+@Controller
 public class SWAdapterController {
 
 	public static final String PUB_AMX_PREFIX = "/pub/amx";
@@ -20,11 +21,13 @@ public class SWAdapterController {
 	@Autowired
 	AdapterServiceClient adapterServiceClient;
 
+	@ResponseBody
 	@RequestMapping(value = "/pub/card/kwt/read", method = RequestMethod.GET)
 	public CardReader readCard() throws InterruptedException {
 		return kwtCardReaderService.read();
 	}
 
+	@ResponseBody
 	@RequestMapping(value = "/pub/script/validation.js", method = RequestMethod.GET)
 	public String makesession(@RequestParam String tranx) throws Exception {
 		adapterServiceClient.pairTerminal(kwtCardReaderService.getAddress(),
