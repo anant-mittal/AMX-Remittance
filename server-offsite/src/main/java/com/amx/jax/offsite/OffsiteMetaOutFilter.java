f547d210c9af433a2a2c376b0cf2e5d4ac79d695
LT <lalit.tanwar07@gmail.com>
2018-11-12 16:48:11 +0530

    meta Out Filter Fixed

---
+++ server-offsite/src/main/java/com/amx/jax/offsite/OffsiteMetaOutFilter.java
@@ -0,0 +1,48 @@
+package com.amx.jax.offsite;
+
+import java.math.BigDecimal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.amx.jax.AppContextUtil;
+import com.amx.jax.client.configs.JaxMetaInfo;
+import com.amx.jax.offsite.service.CustomerSession;
+import com.amx.jax.rest.IMetaRequestOutFilter;
+import com.amx.jax.scope.TenantContextHolder;
+import com.amx.utils.ContextUtil;
+
+@Component
+public class OffsiteMetaOutFilter implements IMetaRequestOutFilter<JaxMetaInfo> {
+
+	@Autowired
+	OffsiteAppConfig offsiteAppConfig;
+
+	@Autowired(required = false)
+	CustomerSession customerSession;
+
+	@Override
+	public JaxMetaInfo exportMeta() {
+		JaxMetaInfo requestMeta = new JaxMetaInfo();
+		outFilter(requestMeta);
+		return requestMeta;
+	}
+
+	@Override
+	public void outFilter(JaxMetaInfo requestMeta) {
+		requestMeta.setTenant(TenantContextHolder.currentSite());
+		requestMeta.setTraceId(ContextUtil.getTraceId());
+		requestMeta.setCountryId(offsiteAppConfig.getCountryId());
+		requestMeta.setCompanyId(offsiteAppConfig.getCompanyId());
+		requestMeta.setLanguageId(offsiteAppConfig.getLanguageId());
+		requestMeta.setCountryBranchId(offsiteAppConfig.getCountrybranchId());
+
+		if (customerSession != null) {
+			requestMeta.setCustomerId(customerSession.getCustomerId());
+			AppContextUtil.setTranxId(customerSession.getTranxId());
+		}
+		// HardCoded
+		requestMeta.setEmployeeId(new BigDecimal(265));
+	}
+
+}
