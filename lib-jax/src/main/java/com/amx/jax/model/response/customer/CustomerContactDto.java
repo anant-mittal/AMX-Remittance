38600b54e972efb1f69fc59285d7ccfc3b4d0493
LT <lalit.tanwar07@gmail.com>
2018-11-20 15:27:47 +0530

    Added Serilase to modeCustomerContactDto

--- lib-jax/src/main/java/com/amx/jax/model/response/customer/CustomerContactDto.java
+++ lib-jax/src/main/java/com/amx/jax/model/response/customer/CustomerContactDto.java
@@ -1,7 +1,11 @@
 package com.amx.jax.model.response.customer;
 
-public class CustomerContactDto {
+import java.io.Serializable;
 
+public class CustomerContactDto implements Serializable {
+
+	private static final long serialVersionUID = -9000337990488461109L;
+	
 	String countryName;
 	String cityName;
 	String block;
@@ -10,52 +14,67 @@ public class CustomerContactDto {
 	String district;
 	String stateName;
 	String buildingNo;
-	
+
 	public String getCountryName() {
 		return countryName;
 	}
+
 	public void setCountryName(String countryName) {
 		this.countryName = countryName;
 	}
+
 	public String getCityName() {
 		return cityName;
 	}
+
 	public void setCityName(String cityName) {
 		this.cityName = cityName;
 	}
+
 	public String getBlock() {
 		return block;
 	}
+
 	public void setBlock(String block) {
 		this.block = block;
 	}
+
 	public String getStreet() {
 		return street;
 	}
+
 	public void setStreet(String street) {
 		this.street = street;
 	}
+
 	public String getFlat() {
 		return flat;
 	}
+
 	public void setFlat(String flat) {
 		this.flat = flat;
 	}
+
 	public String getDistrict() {
 		return district;
 	}
+
 	public void setDistrict(String district) {
 		this.district = district;
 	}
+
 	public String getStateName() {
 		return stateName;
 	}
+
 	public void setStateName(String stateName) {
 		this.stateName = stateName;
 	}
+
 	public String getBuildingNo() {
 		return buildingNo;
 	}
+
 	public void setBuildingNo(String buildingNo) {
 		this.buildingNo = buildingNo;
 	}
