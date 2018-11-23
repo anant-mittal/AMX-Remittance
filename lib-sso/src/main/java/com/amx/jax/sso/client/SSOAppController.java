0632a1467eb53fa74c2b63ffbc20801ddb26d826
LT <lalit.tanwar07@gmail.com>
2018-11-23 14:25:00 +0530

    sotp Check

--- lib-sso/src/main/java/com/amx/jax/sso/client/SSOAppController.java
+++ lib-sso/src/main/java/com/amx/jax/sso/client/SSOAppController.java
@@ -88,7 +88,7 @@ public class SSOAppController {
 			@RequestParam String sotp, HttpServletRequest request,
 			HttpServletResponse response) throws MalformedURLException, URISyntaxException {
 		String tranxId = ssoUser.ssoTranxId();
-		if (ssoUser.isAuthDone()) {
+		if (ssoUser.isAuthDone() && sotp.equalsIgnoreCase(sSOTranx.get().getAppToken())) {
 			return JsonUtil.toJson(AmxApiResponse.buildData(sSOTranx.get().getUserDetails()));
 		}
 		return JsonUtil.toJson(AmxApiResponse.build());
