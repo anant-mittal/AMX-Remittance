46c5c66cebd7ed44a20493e4cd1ff3fcade29035
Animesh <kanimesh.s@gmail.com>
2018-11-30 17:23:53 +0530

    sso instructions changes, branch fc server-ui APIs

---
+++ server-branch/src/main/java/com/amx/jax/branch/controller/FxOrderBranchController.java
@@ -0,0 +1,58 @@
+package com.amx.jax.branch.controller;
+
+import java.math.BigDecimal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.amx.jax.api.AmxApiResponse;
+import com.amx.jax.client.fx.FxOrderBranchClient;
+import com.amx.jax.model.response.fx.FcEmployeeDetailsDto;
+import com.amx.jax.model.response.fx.FcSaleOrderManagementDTO;
+import com.amx.jax.model.response.fx.UserStockDto;
+
+import io.swagger.annotations.Api;
+
+//@PreAuthorize("hasPermission('CUSTOMER_MGMT.FXORDER', 'VIEW')")
+@RestController
+@Api(value = "Order Management APIs")
+public class FxOrderBranchController {
+	@Autowired
+	private FxOrderBranchClient fxOrderBranchClient;
+	
+	@RequestMapping(value = "/api/fxo/order/list", method = { RequestMethod.GET })
+	public AmxApiResponse<FcSaleOrderManagementDTO,Object> getOrderList(){
+		return fxOrderBranchClient.fetchBranchOrderManagement();
+	}
+	
+	@RequestMapping(value = "/api/fxo/order/details",  method = { RequestMethod.POST })
+	public AmxApiResponse<FcSaleOrderManagementDTO,Object> getOrderDetails(@RequestParam(value = "orderNumber", required = true) BigDecimal orderNumber){
+		return fxOrderBranchClient.fetchBranchOrderDetails(orderNumber);
+	}
+	
+	@RequestMapping(value = "/api/fxo/currency/stock",  method = { RequestMethod.POST })
+	public AmxApiResponse<UserStockDto,Object> fetchBranchStockDetailsByCurrency(@RequestParam(value = "currencyId", required = true) BigDecimal foreignCurrencyId){
+		return fxOrderBranchClient.fetchBranchStockDetailsByCurrency(foreignCurrencyId);
+	}
+	
+	@RequestMapping(value = "/api/fxo/currency/stock",  method = { RequestMethod.GET })
+	public AmxApiResponse<UserStockDto,Object> fetchBranchStockDetails(){
+		return fxOrderBranchClient.fetchBranchStockDetails();
+	}
+	
+	@RequestMapping(value = "/api/fxo/drivers",  method = { RequestMethod.GET })
+	public AmxApiResponse<FcEmployeeDetailsDto,Object> fetchDriverList(){
+		return fxOrderBranchClient.fetchBranchEmployee();
+	}
+	
+	@RequestMapping(value = "/api/fxo/driver/assign",  method = { RequestMethod.POST })
+	public AmxApiResponse<Boolean,Object> assignDriver(
+			@RequestParam(value = "orderNumber", required = true) BigDecimal orderNumber,
+			@RequestParam(value = "driverId", required = true) BigDecimal driverId){
+		return fxOrderBranchClient.assignDriver(orderNumber, driverId);
+	}
+	
+}
