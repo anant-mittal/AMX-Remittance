e94ae5a3b393f9663b7f3b8cc3e1ded19304dba8
Prashant <prashant.thorat@almullagroup.com>
2018-12-08 16:09:10 +0530

    default constructor for json parsing

--- lib-jax/src/main/java/com/amx/jax/model/response/OtpPrefixDto.java
+++ lib-jax/src/main/java/com/amx/jax/model/response/OtpPrefixDto.java
@@ -3,6 +3,11 @@ package com.amx.jax.model.response;
 public class OtpPrefixDto {
 	String mOtpPrefix;
 
+	public OtpPrefixDto() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
 	public OtpPrefixDto(String mOtpPrefix) {
 		this.mOtpPrefix = mOtpPrefix;
 	}
