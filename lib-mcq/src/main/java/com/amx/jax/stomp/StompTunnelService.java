3684137d6f9b4b2ea9f1a8cc08d3945ba51d905e
Animesh <kanimesh.s@gmail.com>
2018-12-11 20:35:19 +0530

    stomp changes

--- lib-mcq/src/main/java/com/amx/jax/stomp/StompTunnelService.java
+++ lib-mcq/src/main/java/com/amx/jax/stomp/StompTunnelService.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.amx.jax.stomp.StompSessionCache.StompSession;
 import com.amx.jax.tunnel.TunnelService;
 import com.amx.utils.ArgUtil;
@@ -22,7 +25,9 @@ public class StompTunnelService {
 	public void sendToAll(String topic, Object message) {
 		StompTunnelEvent event = new StompTunnelEvent();
 		event.setTopic(topic);
-		event.setData(JsonUtil.toMap(message));
+		Map<String, Object> messageData = new HashMap<String, Object>();
+		messageData.put("data", message);
+		event.setData(JsonUtil.toMap(messageData));
 		tunnelService.shout(StompTunnelToAllSender.STOMP_TO_ALL, event);
 	}
 
@@ -33,7 +38,9 @@ public class StompTunnelService {
 		StompSession stompSession = stompTunnelSessionManager.getStompSession(stompSessionId);
 		if (!ArgUtil.isEmpty(stompSession)) {
 			event.setHttpSessionId(stompSession.getHttpSessionId());
-			event.setData(JsonUtil.toMap(message));
+			Map<String, Object> messageData = new HashMap<String, Object>();
+			messageData.put("data", message);
+			event.setData(JsonUtil.toMap(messageData));
 			tunnelService.shout(StompTunnelToXSender.getSendTopic(stompSession.getPrefix()), event);
 		}
 
