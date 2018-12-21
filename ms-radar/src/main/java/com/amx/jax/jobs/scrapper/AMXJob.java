0cc17fbcb9ec2142c3820396ba4a61a20fc1b37e
LT <lalit.tanwar07@gmail.com>
2018-12-21 13:39:18 +0530

    Amx rates

--- ms-radar/src/main/java/com/amx/jax/jobs/scrapper/AMXJob.java
+++ ms-radar/src/main/java/com/amx/jax/jobs/scrapper/AMXJob.java
@@ -1,13 +1,9 @@
 package com.amx.jax.jobs.scrapper;
 
-import java.io.IOException;
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.List;
 
-import org.jsoup.Connection;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
@@ -16,6 +12,8 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import com.amx.amxlib.model.MinMaxExRateDTO;
+import com.amx.jax.client.ExchangeRateClient;
 import com.amx.jax.logger.LoggerService;
 import com.amx.jax.rates.AmxCurConstants;
 import com.amx.jax.rates.AmxCurConstants.RCur;
@@ -24,7 +22,6 @@ import com.amx.jax.rates.AmxCurConstants.RType;
 import com.amx.jax.rates.AmxCurRate;
 import com.amx.jax.rates.AmxCurRateRepository;
 import com.amx.utils.ArgUtil;
-import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 
 @Configuration
 @EnableScheduling
@@ -32,89 +29,40 @@ import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 @Service
 public class AMXJob extends AScrapperTasks {
 
-	private static final String UAE_XCHANGE_URL = "https://www.uaeexchange.com.kw/Rates.aspx";
-
-	private static final String PARAM_SCRIPT_MANAGER1 = "ctl00$ScriptManager1";
-	private static final String VALUE_SCRIPT_MANAGER1_AHREF_EXCHANGE = "ctl00$ctl10$updatepnl|ctl00$ctl10$ahrefExchange";
-	private static final String VALUE_SCRIPT_MANAGER1_ANCHOR_FOREX = "ctl00$ctl10$updatepnl|ctl00$ctl10$anchorForex";
-	private static final String PARAM_ANCHOR_FOREX = "ctl00$ctl10$anchorForex";
-	private static final String PARAM_AHREF_EXCHANGE = "ctl00$ctl10$ahrefExchange";
-	private static final String VALUE_EXCHANGE_RATES = "Exchange Rates";
-	private static final String VALUE_FOREX_RATES = "Forex Rates";
-
-	private static final String PARAM_NAME = "name";
-	private static final String TAG_INPUT = "input";
-
 	@Autowired
 	private AmxCurRateRepository curRateRepository;
 
-	XmlMapper xmlMapper = new XmlMapper();
+	@Autowired
+	private ExchangeRateClient xRateClient;
 
 	Logger logger = LoggerService.getLogger(AMXJob.class);
 
 	@Scheduled(fixedDelay = AmxCurConstants.INTERVAL_MIN_30)
 	public void doTask() {
-		try {
-			Document doc0 = Jsoup.connect(UAE_XCHANGE_URL).get();
+		List<MinMaxExRateDTO> rates = xRateClient.getMinMaxExchangeRate().getResults();
 
-			Connection con1 = Jsoup.connect(UAE_XCHANGE_URL)
-					.referrer(UAE_XCHANGE_URL);
+		RType type = RType.SELL_TRNSFR;
 
-			Elements inputs1 = doc0.select(TAG_INPUT);
-			for (Element input : inputs1) {
-				String key = input.attr(PARAM_NAME);
-				if (!PARAM_ANCHOR_FOREX.equalsIgnoreCase(key)) {
-					con1 = con1.data(key, input.val());
-				}
-			}
-			con1 = con1.data(PARAM_SCRIPT_MANAGER1, VALUE_SCRIPT_MANAGER1_AHREF_EXCHANGE)
-					.data(PARAM_AHREF_EXCHANGE, VALUE_EXCHANGE_RATES);
+		for (MinMaxExRateDTO minMaxExRateDTO : rates) {
 
-			Document doc1 = con1.post();
-			fetchrates(doc1, RType.SELL_TRNSFR);
-
-			Connection con2 = Jsoup.connect(UAE_XCHANGE_URL)
-					.referrer(UAE_XCHANGE_URL);
-
-			Elements inputs2 = doc0.select(TAG_INPUT);
-			for (Element input : inputs2) {
-				String key = input.attr(PARAM_NAME);
-				if (!PARAM_AHREF_EXCHANGE.equalsIgnoreCase(key)) {
-					con2 = con2.data(key, input.val());
-				}
-			}
-			con2 = con2.data(PARAM_SCRIPT_MANAGER1, VALUE_SCRIPT_MANAGER1_ANCHOR_FOREX)
-					.data(PARAM_ANCHOR_FOREX, VALUE_FOREX_RATES);
-
-			Document doc2 = con2.post();
-			fetchrates(doc2, RType.SELL_CASH);
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-	}
-
-	public void fetchrates(Document doc, RType type) {
-		Elements trs = doc.select("#ctl10_updatepnl table.table tbody tr");
-		for (Element tr : trs) {
-			Elements tds = tr.select("td");
-			AmxCurConstants.RCur cur = (RCur) ArgUtil.parseAsEnum(tds.get(2).text(),
+			AmxCurConstants.RCur cur = (RCur) ArgUtil.parseAsEnum(minMaxExRateDTO.getToCurrency().getQuoteName(),
 					AmxCurConstants.RCur.UNKNOWN);
-			if (!AmxCurConstants.RCur.UNKNOWN.equals(cur) && tds.size() >= 3) {
-				BigDecimal rate = ArgUtil.parseAsBigDecimal(tds.get(3).text());
+			if (!AmxCurConstants.RCur.UNKNOWN.equals(cur)) {
+				BigDecimal rate = ArgUtil.parseAsBigDecimal(minMaxExRateDTO.getMaxExrate());
 				if (!ArgUtil.isEmpty(rate)) {
 					AmxCurRate trnsfrRate = new AmxCurRate();
-					trnsfrRate.setrSrc(RSource.UAEXCHANGE);
+					trnsfrRate.setrSrc(RSource.AMX);
 					trnsfrRate.setrDomCur(RCur.KWD);
 					trnsfrRate.setrForCur(cur);
 					trnsfrRate.setrType(type);
 					trnsfrRate.setrRate(rate);
+					trnsfrRate.setrRate(BigDecimal.ONE.divide(rate, 12, RoundingMode.CEILING));
 					// System.out.println(JsonUtil.toJson(trnsfrRate));
 					curRateRepository.insertRate(trnsfrRate);
 				}
 			}
 		}
+
 	}
 
 }
