b71301807c615d70ca37427b7d7d3aabd88149b6
LT <lalit.tanwar07@gmail.com>
2019-01-03 13:43:54 +0530

    Server info Added

--- lib-postman/src/main/java/com/amx/jax/postman/client/PostManContextListener.java
+++ lib-postman/src/main/java/com/amx/jax/postman/client/PostManContextListener.java
@@ -11,14 +11,26 @@ import com.amx.jax.AppConfig;
 import com.amx.jax.postman.PostManService;
 import com.amx.jax.postman.model.Notipy;
 import com.amx.jax.postman.model.Notipy.Channel;
+import com.amx.utils.ArgUtil;
+import com.amx.utils.JsonUtil;
+import com.amx.utils.NetworkAdapter;
+import com.amx.utils.NetworkAdapter.NetAddress;
 
 public class PostManContextListener implements ServletContextListener {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(PostManContextListener.class);
+	private static NetAddress NET_ADDRESS = null;
 
 	public PostManService getService(ServletContextEvent sce) {
 		return WebApplicationContextUtils.getRequiredWebApplicationContext(sce.getServletContext())
 				.getBean(PostManService.class);
+	}
+
+	public static NetAddress getNetAddress() {
+		if (NET_ADDRESS == null) {
+			NET_ADDRESS = NetworkAdapter.getAddress();
+		}
+		return NET_ADDRESS;
 	}
 
 	@Override
@@ -28,12 +40,16 @@ public class PostManContextListener implements ServletContextListener {
 					.getBean(AppConfig.class);
 
 			if (!appConfig.isDebug()) {
+				NetAddress netAddress = getNetAddress();
 				PostManService postManService = getService(sce);
 				Notipy msg = new Notipy();
 				msg.setChannel(Channel.DEPLOYER);
 				msg.setColor("#36a64f");
 				msg.setMessage("App : " + appConfig.getAppName());
 				msg.addLine("Status : server is started...");
+				if (!ArgUtil.isEmpty(netAddress)) {
+					msg.addLine("SERVER:  " + JsonUtil.toStringMap(netAddress).toString());
+				}
 				LOGGER.info("{} : Status : server is started...", appConfig.getAppName());
 
 				postManService.notifySlack(msg);
