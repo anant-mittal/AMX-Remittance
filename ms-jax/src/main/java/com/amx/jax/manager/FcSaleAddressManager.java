ab75d251796b8cda348ff007a80fa97442bc16e6
Subodh Bhoir <subodh.bhoir@almullagroup.com>
2019-01-16 11:26:34 +0530

    resolved conflicts

--- ms-jax/src/main/java/com/amx/jax/manager/FcSaleAddressManager.java
+++ ms-jax/src/main/java/com/amx/jax/manager/FcSaleAddressManager.java
@@ -523,22 +523,7 @@ public class FcSaleAddressManager extends AbstractModel {
 					.append(shippingAddressDto.getBuildingNo() == null ? "" : concat + "House no. " + shippingAddressDto.getBuildingNo())
 					.append(shippingAddressDto.getFlat() == null ? "" : concat + "Flat "+  shippingAddressDto.getHouse());
 					
-			
-			
 			if (shippingAddressDto.getLocalContactCity() != null) {
-<<<<<<< Updated upstream
-				sb.append("City ").append(shippingAddressDto.getLocalContactCity() == null ? ""
-						: concat + shippingAddressDto.getLocalContactCity());
-			}
-			sb.append(
-					shippingAddressDto.getGovernoatesDto() == null ? "" : concat + shippingAddressDto.getGovernoatesDto().getResourceName());
-			sb.append(
-					shippingAddressDto.getGovtAreaDesc() == null ? "" : concat + shippingAddressDto.getGovtAreaDesc());
-			sb.append(shippingAddressDto.getLocalContactDistrict() == null ? ""
-					: concat + shippingAddressDto.getLocalContactDistrict());
-			sb.append(shippingAddressDto.getLocalContactState() == null ? ""
-					: concat + shippingAddressDto.getLocalContactState());
-=======
 				sb.append(concat).append("City ").append(shippingAddressDto.getLocalContactCity() == null ? "": shippingAddressDto.getLocalContactCity());
 			}
 			if(shippingAddressDto.getGovernoatesDto()!=null && !shippingAddressDto.getGovernoatesDto().equals("")) {
@@ -553,16 +538,14 @@ public class FcSaleAddressManager extends AbstractModel {
 			
 			if(shippingAddressDto.getLocalContactState()!=null && !shippingAddressDto.getLocalContactState().equals("")) {
 				sb.append(concat).append(shippingAddressDto.getLocalContactState());
-			}
->>>>>>> Stashed changes
-			
+
 		}
 		if(sb!=null){
 			address = sb.toString();
 		}
-		return address;
+		
 	}
-	
-	
+		return address;
+}
 	
 }
