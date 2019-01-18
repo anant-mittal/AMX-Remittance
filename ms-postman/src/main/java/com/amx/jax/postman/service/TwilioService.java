fab5a14e642b444ca9a923fef39a33957ccaef73
LT <lalit.tanwar07@gmail.com>
2019-01-18 15:01:34 +0530

    Twilio Added

---
+++ ms-postman/src/main/java/com/amx/jax/postman/service/TwilioService.java
@@ -0,0 +1,16 @@
+package com.amx.jax.postman.service;
+
+//@Component
+//@TenantScoped
+public class TwilioService {
+
+	/**
+	 * @TenantValue("${twilio.api.account.sid}") String accountSid = "ACXXXXXX"; //
+	 * Your Account SID from www.twilio.com/user/account
+	 * 
+	 * @TenantValue("${twilio.api.auth.token}") String authToken = "XXXXXXXX"; //
+	 * Your Auth Token from www.twilio.com/user/account
+	 * 
+	 **/
+
+}
