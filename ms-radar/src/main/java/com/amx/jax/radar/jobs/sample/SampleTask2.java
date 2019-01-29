633cd8df6d1e239244df6774aa0268c20cb70e01
LT <lalit.tanwar07@gmail.com>
2019-01-29 12:54:48 +0530

    Locking

--- ms-radar/src/main/java/com/amx/jax/radar/jobs/sample/SampleTask2.java
+++ ms-radar/src/main/java/com/amx/jax/radar/jobs/sample/SampleTask2.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 
 import com.amx.jax.AppConfig;
 import com.amx.jax.logger.LoggerService;
+import com.amx.jax.mcq.Candidate;
 import com.amx.jax.mcq.MCQ;
 import com.amx.jax.radar.TestSizeApp;
 import com.amx.jax.rates.AmxCurConstants;
@@ -23,6 +24,8 @@ import com.amx.jax.rates.AmxCurConstants;
 public class SampleTask2 {
 
 	private static final Logger LOGGER = LoggerService.getLogger(SampleTask2.class);
+	private static final Candidate lock = new Candidate().fixedDelay(AmxCurConstants.INTERVAL_SEC * 5)
+			.maxAge(AmxCurConstants.INTERVAL_MIN).queue(SampleTask2.class);
 
 	@Autowired
 	private MCQ mcq;
@@ -32,13 +35,10 @@ public class SampleTask2 {
 
 	@Scheduled(fixedDelay = AmxCurConstants.INTERVAL_SEC * 5)
 	public void doTask() throws InterruptedException {
-		boolean x;
-		if (x = mcq.claimLeaderShip(SampleTask2.class, AmxCurConstants.INTERVAL_SEC * 10)) {
+		if (mcq.lead(lock)) {
 			Thread.sleep(AmxCurConstants.INTERVAL_SEC * 3);
 			LOGGER.info("======= I am doing my Task @ {}", appConfig.getSpringAppName());
-			mcq.resignLeaderShip(SampleTask2.class);
-		} else {
-			LOGGER.info("======= I NOT doing my Task @ {}", appConfig.getSpringAppName());
+			mcq.resign(lock);
 		}
 	}
 
