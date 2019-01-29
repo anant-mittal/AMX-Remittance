15662105a430741276189d6ae3494d6dffd82d55
LT <lalit.tanwar07@gmail.com>
2019-01-29 13:38:37 +0530

    Sample task Worker

--- ms-radar/src/main/java/com/amx/jax/radar/jobs/sample/SampleTaskWorker.java
+++ ms-radar/src/main/java/com/amx/jax/radar/jobs/sample/SampleTaskWorker.java
@@ -7,7 +7,7 @@ import com.amx.jax.tunnel.ITunnelSubscriber;
 import com.amx.jax.tunnel.TunnelEventMapping;
 import com.amx.jax.tunnel.TunnelEventXchange;
 
-@TunnelEventMapping(byEvent = SampleTaskEvent.class, scheme = TunnelEventXchange.SEND_LISTNER)
+@TunnelEventMapping(byEvent = SampleTaskEvent.class, scheme = TunnelEventXchange.SHOUT_LISTNER)
 public class SampleTaskWorker implements ITunnelSubscriber<SampleTaskEvent> {
 
 	private final Logger LOGGER = LoggerFactory.getLogger(getClass());
