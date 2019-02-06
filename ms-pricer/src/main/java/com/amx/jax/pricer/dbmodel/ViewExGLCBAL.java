4a476296a2465b03a58d71fa523bdfa408cacfbf
abkamble <abkamble@gmail.com>
2019-02-06 15:36:06 +0530

    GLCBAL View Updated.

--- ms-pricer/src/main/java/com/amx/jax/pricer/dbmodel/ViewExGLCBAL.java
+++ ms-pricer/src/main/java/com/amx/jax/pricer/dbmodel/ViewExGLCBAL.java
@@ -12,7 +12,7 @@ import javax.persistence.Table;
  * The Class ViewExGLCBAL.
  */
 @Entity
-@Table(name = "VW_EX_GLCBAL_NEW")
+@Table(name = "VW_EX_GLCBAL")
 public class ViewExGLCBAL implements Serializable {
 
 	/** The Constant serialVersionUID. */
