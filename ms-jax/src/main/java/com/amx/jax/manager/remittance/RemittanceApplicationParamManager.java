1081330c8dd415b51249e58aeccd2400a4c46277
Prashant <prashant.thorat@almullagroup.com>
2019-02-11 17:38:36 +0530

    local changes

--- ms-jax/src/main/java/com/amx/jax/manager/remittance/RemittanceApplicationParamManager.java
+++ ms-jax/src/main/java/com/amx/jax/manager/remittance/RemittanceApplicationParamManager.java
@@ -1,14 +1,18 @@
 package com.amx.jax.manager.remittance;
 
+import java.math.BigDecimal;
 import java.util.Map;
 
 import javax.annotation.Resource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.amx.amxlib.constant.ApplicationProcedureParam;
+import com.amx.jax.dbmodel.BankMasterModel;
 import com.amx.jax.dbmodel.BenificiaryListView;
 import com.amx.jax.model.request.remittance.IRemittanceApplicationParams;
+import com.amx.jax.services.BankService;
 
 /**
  * @author Prashant
@@ -19,10 +23,13 @@ public class RemittanceApplicationParamManager {
 
 	@Resource
 	Map<String, Object> remitApplParametersMap;
+	@Autowired
+	BankService bankService;
 
 	public void populateRemittanceApplicationParamMap(IRemittanceApplicationParams remittanceApplicationParams,
 			BenificiaryListView beneficiaryView) {
-		//remitApplParametersMap.put(ApplicationProcedureParam.P_ROUTING_COUNTRY_ID, remittanceApplicationParams.getLocalAmount());
+		 BankMasterModel routintBankMaster = bankService.getBankById(new BigDecimal( remittanceApplicationParams.getCorrespondanceBankId()));
+		//P_ROUTING_COUNTRY_ID.putValue(remitApplParametersMap, routintBankMaster.getBankCountryId());
 
 	}
 }
