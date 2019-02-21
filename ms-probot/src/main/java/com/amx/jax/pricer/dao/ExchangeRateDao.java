ff38badbb4dd56329a18543619dc3f055e86adee
abkamble <abkamble@gmail.com>
2019-02-21 17:44:21 +0530

    Exchange Rate - CorrBank Country Bug is Fixed.

--- ms-probot/src/main/java/com/amx/jax/pricer/dao/ExchangeRateDao.java
+++ ms-probot/src/main/java/com/amx/jax/pricer/dao/ExchangeRateDao.java
@@ -68,8 +68,7 @@ public class ExchangeRateDao {
 	public List<ExchangeRateAPRDET> getUniqueSellRatesForRoutingBanks(BigDecimal currencyId, BigDecimal countryId,
 			BigDecimal applicationCountryId, List<BigDecimal> routingBankIds, List<BigDecimal> serviceIds) {
 
-		return repo.getSellRatesForRoutingBanks(currencyId, countryId, applicationCountryId, routingBankIds, serviceIds);
+		return repo.getSellRatesForRoutingBanks(currencyId, applicationCountryId, routingBankIds, serviceIds);
 	}
-	
 
 }
