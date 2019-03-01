2029ab4a2f0309df55d90735e4ad819b8424d84e
LT <lalit.tanwar07@gmail.com>
2019-03-01 18:12:57 +0530

    Merge Conflicts in JaxDynamicPriceService

--- ms-exchangerate/src/main/java/com/amx/jax/exrateservice/service/JaxDynamicPriceService.java
+++ ms-exchangerate/src/main/java/com/amx/jax/exrateservice/service/JaxDynamicPriceService.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 
 import com.amx.amxlib.exception.jax.GlobalException;
 import com.amx.amxlib.meta.model.BankMasterDTO;
+import com.amx.amxlib.model.response.ApiResponse;
 import com.amx.amxlib.model.response.ExchangeRateResponseModel;
 import com.amx.jax.api.AmxApiResponse;
 import com.amx.jax.error.JaxError;
@@ -23,6 +24,7 @@ import com.amx.jax.pricer.dto.PricingRequestDTO;
 import com.amx.jax.pricer.dto.PricingResponseDTO;
 import com.amx.jax.pricer.var.PricerServiceConstants.PRICE_BY;
 import com.amx.jax.service.BankMetaService;
+import com.amx.utils.JsonUtil;
 
 @Service
 public class JaxDynamicPriceService {
@@ -38,24 +40,41 @@ public class JaxDynamicPriceService {
 	@Autowired
 	ExchangeRateService exchangeRateService;
 
-	public ExchangeRateResponseModel getExchangeRatesWithDiscount(BigDecimal fromCurrency, BigDecimal toCurrency,
-			BigDecimal lcAmount, BigDecimal foreignAmount, BigDecimal countryId, BigDecimal routingBankId) {
-		PricingRequestDTO pricingRequestDTO = createPricingRequest(fromCurrency, toCurrency, lcAmount, foreignAmount,
-				countryId, routingBankId);
+	public ExchangeRateResponseModel getExchangeRates(BigDecimal fromCurrency, BigDecimal toCurrency,
+			BigDecimal lcAmount, BigDecimal foreignAmount, BigDecimal beneBankCountryId, BigDecimal routingBankId,
+			BigDecimal serviceIndicatorId) {
+		PricingRequestDTO pricingRequestDTO = new PricingRequestDTO();
+		pricingRequestDTO.setCustomerId(metaData.getCustomerId());
+		pricingRequestDTO.setChannel(metaData.getChannel().getClientChannel());
+		pricingRequestDTO.setCountryBranchId(metaData.getCountryBranchId());
+		pricingRequestDTO.setForeignCurrencyId(toCurrency);
+		pricingRequestDTO.setLocalAmount(lcAmount);
+		pricingRequestDTO.setForeignAmount(foreignAmount);
+		pricingRequestDTO.setLocalCountryId(metaData.getCountryId());
+		pricingRequestDTO.setLocalCurrencyId(fromCurrency);
+		if (routingBankId != null) {
+			pricingRequestDTO.setRoutingBankIds(Arrays.asList(routingBankId));
+			pricingRequestDTO.setPricingLevel(PRICE_BY.ROUTING_BANK);
+		} else {
+			pricingRequestDTO.setPricingLevel(PRICE_BY.COUNTRY);
+		}
+		pricingRequestDTO.setForeignCountryId(beneBankCountryId);
 		AmxApiResponse<PricingResponseDTO, Object> apiResponse = null;
 		try {
+			LOGGER.debug("Pricing request json : {}", JsonUtil.toJson(pricingRequestDTO));
 			apiResponse = pricerServiceClient.fetchPriceForCustomer(pricingRequestDTO);
 		} catch (Exception e) {
 			LOGGER.debug("No exchange data found from pricer, error is: ", e);
 			throw new GlobalException(JaxError.EXCHANGE_RATE_NOT_FOUND, "No exchange data found");
 		}
 		ExchangeRateResponseModel exchangeRateResponseModel = createExchangeRateResponseModel(apiResponse, lcAmount,
-				foreignAmount);
+				foreignAmount, serviceIndicatorId);
 		return exchangeRateResponseModel;
 	}
 
 	public ExchangeRateResponseModel getBaseExchangeRates(BigDecimal fromCurrency, BigDecimal toCurrency,
-			BigDecimal lcAmount, BigDecimal foreignAmount, BigDecimal countryId, BigDecimal routingBankId) {
+			BigDecimal lcAmount, BigDecimal foreignAmount, BigDecimal countryId, BigDecimal routingBankId,
+			BigDecimal serviceIndicatorId) {
 		PricingRequestDTO pricingRequestDTO = createPricingRequest(fromCurrency, toCurrency, lcAmount, foreignAmount,
 				countryId, routingBankId);
 		AmxApiResponse<PricingResponseDTO, Object> apiResponse = null;
@@ -66,16 +85,20 @@ public class JaxDynamicPriceService {
 			throw new GlobalException(JaxError.EXCHANGE_RATE_NOT_FOUND, "No exchange data found");
 		}
 		ExchangeRateResponseModel exchangeRateResponseModel = createExchangeRateResponseModel(apiResponse, lcAmount,
-				foreignAmount);
+				foreignAmount, serviceIndicatorId);
 		return exchangeRateResponseModel;
 	}
 
 	private ExchangeRateResponseModel createExchangeRateResponseModel(
-			AmxApiResponse<PricingResponseDTO, Object> apiResponse, BigDecimal lcAmount, BigDecimal foreignAmount) {
+			AmxApiResponse<PricingResponseDTO, Object> apiResponse, BigDecimal lcAmount, BigDecimal foreignAmount,
+			BigDecimal serviceIndicatorId) {
 		ExchangeRateResponseModel exchangeRateResponseModel = new ExchangeRateResponseModel();
 		List<BankMasterDTO> bankWiseRates = new ArrayList<>();
 		List<ExchangeRateDetails> sellRateDetails = apiResponse.getResult().getSellRateDetails();
 		for (ExchangeRateDetails sellRateDetail : sellRateDetails) {
+			if (serviceIndicatorId != null && !serviceIndicatorId.equals(sellRateDetail.getServiceIndicatorId())) {
+				continue;
+			}
 			BankMasterDTO dto = bankMetaService.convert(bankMetaService.getBankMasterbyId(sellRateDetail.getBankId()));
 			if (foreignAmount != null) {
 				dto.setExRateBreakup(exchangeRateService.createBreakUpFromForeignCurrency(
@@ -112,6 +135,12 @@ public class JaxDynamicPriceService {
 		}
 		pricingRequestDTO.setForeignCountryId(beneBankCountryId);
 		return pricingRequestDTO;
+	}
+
+	public ExchangeRateResponseModel getExchangeRates(BigDecimal fromCurrency, BigDecimal toCurrency,
+			BigDecimal lcAmount, BigDecimal foreignAmount, BigDecimal beneBankCountryId, BigDecimal routingBankId) {
+		return getExchangeRates(fromCurrency, toCurrency, lcAmount, foreignAmount, beneBankCountryId, routingBankId,
+				null);
 	}
 
 }
