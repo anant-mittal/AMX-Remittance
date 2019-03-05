5711fc7906384edcb3648e1a88f0a85bd4a8bf29
Animesh <kanimesh.s@gmail.com>
2019-03-05 19:34:32 +0530

    send email api placeholder

--- server-branch/src/main/java/com/amx/jax/branch/controller/RemitBranchController.java
+++ server-branch/src/main/java/com/amx/jax/branch/controller/RemitBranchController.java
@@ -256,4 +256,31 @@ public class RemitBranchController {
 		// return JsonUtil.toJson(wrapper);
 		// }
 	}
+
+	@RequestMapping(value = "/api/remitt/tranx/email", method = { RequestMethod.GET })
+	public AmxApiResponse<BoolRespModel, Object> sendEmail(
+			@RequestParam(required = false) BigDecimal collectionDocumentNo,
+			@RequestParam(required = false) BigDecimal documentNumber,
+			@RequestParam(required = false) BigDecimal documentFinanceYear,
+			@RequestParam(required = false) BigDecimal collectionDocumentFinYear,
+			@RequestParam(required = false) BigDecimal collectionDocumentCode,
+			@RequestParam(required = false) BigDecimal customerReference, @RequestParam("ext") File.Type ext,
+			@RequestParam(required = false) Boolean duplicate) throws PostManException, IOException {
+
+		TransactionHistroyDTO tranxDTO = new TransactionHistroyDTO();
+		tranxDTO.setCollectionDocumentNo(collectionDocumentNo);
+		tranxDTO.setDocumentNumber(documentNumber);
+		tranxDTO.setDocumentFinanceYear(documentFinanceYear);
+		tranxDTO.setCollectionDocumentFinYear(collectionDocumentFinYear);
+		tranxDTO.setCollectionDocumentCode(collectionDocumentCode);
+		tranxDTO.setCustomerReference(customerReference);
+
+		RemittanceReceiptSubreport rspt = remitClient
+				.report(tranxDTO, !duplicate.booleanValue(), branchMetaOutFilter.exportMeta()).getResult();
+		AmxApiResponse<RemittanceReceiptSubreport, Object> wrapper = AmxApiResponse.buildData(rspt);
+
+		AmxApiResponse<BoolRespModel, Object> resp = new AmxApiResponse<BoolRespModel, Object>();
+		return resp;
+
+	}
 }
