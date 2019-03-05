dfca1dd56fec4ecfdce0d45f9fa8d01f4daa967a
LT <lalit.tanwar07@gmail.com>
2019-03-05 11:09:37 +0530

    COmpile Error after merge

--- lib-probot/src/main/java/com/amx/jax/pricer/AbstractProbotInterface.java
+++ lib-probot/src/main/java/com/amx/jax/pricer/AbstractProbotInterface.java
@@ -19,18 +19,18 @@ public interface AbstractProbotInterface {
 		public static final String FETCH_DISCOUNTED_PRICE = SERVICE_PREFIX + API_VERSION_V1 + "/fetch-discounted-price";
 
 		public static final String FETCH_DISCOUNTED_RATES = SERVICE_PREFIX + API_VERSION_V1 + "/fetch-discounted-rates";
-		
+
 		// Service Controllers for Exchange Rate Related Data
 
 		public static final String GET_CB_AND_PRODUCTS = SERVICE_PREFIX + API_VERSION_V1 + "/get-cb-and-products";
 
 		public static final String PRICE_TEST = SERVICE_PREFIX + API_VERSION_V1 + "/price-test";
-		
-		public static final String SERVICE_TEST = SERVICE_PREFIX + API_VERSION_V1 + "/service-test";
+
 		// Service Controllers for Discount Management Data
 		public static final String GET_DISCOUNT_DETAILS = SERVICE_PREFIX + API_VERSION_V1 + "/get-discount-details";
-		
-		public static final String GET_ROUTBANKS_AND_SEVICES = SERVICE_PREFIX + API_VERSION_V1 + "/get-routbanks-and-services";
+
+		public static final String GET_ROUTBANKS_AND_SEVICES = SERVICE_PREFIX + API_VERSION_V1
+				+ "/get-routbanks-and-services";
 		public static final String SERVICE_TEST = SERVICE_PREFIX + API_VERSION_V1 + "/service-test";
 
 	}
