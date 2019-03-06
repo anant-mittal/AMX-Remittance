19dd01ecb0fb336a65538a72e8cb9cc3017a910f
Radhika.Ala <radhika.ala@almullaexchange.com>
2019-03-06 13:38:40 +0530

    5362 changes

--- ms-user/src/main/java/com/amx/jax/userservice/repository/CustomerRepository.java
+++ ms-user/src/main/java/com/amx/jax/userservice/repository/CustomerRepository.java
@@ -34,8 +34,6 @@ public interface CustomerRepository extends CrudRepository<Customer, BigDecimal>
 
 	@Query("select c from Customer c where identityInt =?1 and countryId=?2 and identityTypeId = ?3 and   isActive in ('Y','N')")
 	public Customer getCustomerByCivilIdAndIsActive(String identityInt, BigDecimal countryId,BigDecimal identityTypeId);
-	
-	public Customer getCustomerByCustomerIdAndIsActive(BigDecimal customerId, String isActive);
 
 	@Query("select c from Customer c where mobile=?1 ")
 	public List<Customer> getCustomerByMobileCheck(String mobile);
