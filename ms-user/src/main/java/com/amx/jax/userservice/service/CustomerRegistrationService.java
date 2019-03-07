c707de3a2a8b90aea008c702c4d55dfb3b124142
Radhika.Ala <radhika.ala@almullaexchange.com>
2019-03-07 13:40:39 +0530

    for validation

--- ms-user/src/main/java/com/amx/jax/userservice/service/CustomerRegistrationService.java
+++ ms-user/src/main/java/com/amx/jax/userservice/service/CustomerRegistrationService.java
@@ -143,7 +143,7 @@ public class CustomerRegistrationService extends AbstractService {
 	 */
 	public ApiResponse saveLoginDetail(CustomerCredential customerCredential) {
 		customerRegistrationManager.saveLoginDetail(customerCredential);
-		//customerCredentialValidator.validate(customerRegistrationManager.get(),  null);
+		customerCredentialValidator.validate(customerRegistrationManager.get(),  null);
 		customerRegistrationManager.commit();
 		
 		CustomerOnlineRegistration custIdd = custDao.getCustomerIDByuserId(customerCredential.getLoginId());
