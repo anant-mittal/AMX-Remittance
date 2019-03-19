3b709fc9a8a62971546a509583b5802628a58031
mir.rabiluddin <mir.rabiluddin@almullaexchange.com>
2019-03-19 09:44:11 +0300

    test

--- ms-jax/src/test/java/com/amx/jax/remittance/RemittanceApplicationTest.java
+++ ms-jax/src/test/java/com/amx/jax/remittance/RemittanceApplicationTest.java
@@ -2,8 +2,6 @@ package com.amx.jax.remittance;
 
 import java.math.BigDecimal;
 
-import javax.transaction.Transactional;
-
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -11,7 +9,6 @@ import com.amx.jax.AbstractTest;
 import com.amx.jax.branchremittance.dao.BranchRemittanceDao;
 import com.amx.jax.constant.ConstantDocument;
 import com.amx.jax.dbmodel.remittance.RemittanceApplication;
-import com.amx.jax.dbmodel.remittance.RemittanceTransaction;
 import com.amx.jax.repository.RemittanceApplicationRepository;
 import com.amx.jax.repository.RemittanceTransactionRepository;
 
@@ -27,6 +24,8 @@ public class RemittanceApplicationTest extends AbstractTest {
 
 	@Test
 	public void testSaveAplication() {
-		brRemittanceDao.deleteFromCart(new BigDecimal(2946240), ConstantDocument.Deleted);
+		RemittanceApplication appl = new RemittanceApplication();
+		appl.setRemittanceApplicationId(new BigDecimal(2946240));
+		brRemittanceDao.deleteFromCart(appl, ConstantDocument.Deleted);
 	}
 }
