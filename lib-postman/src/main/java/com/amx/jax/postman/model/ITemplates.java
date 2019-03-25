f99af0177f7fa958860411218a0459522782a61c
LT <lalit.tanwar07@gmail.com>
2019-03-25 15:39:59 +0530

    Template Exception for Not found  added

--- lib-postman/src/main/java/com/amx/jax/postman/model/ITemplates.java
+++ lib-postman/src/main/java/com/amx/jax/postman/model/ITemplates.java
@@ -4,6 +4,7 @@ import com.amx.jax.ProjectConfig;
 import com.amx.jax.dict.Project;
 import com.amx.jax.postman.model.File.PDFConverter;
 import com.amx.jax.postman.model.Notipy.Channel;
+import com.amx.utils.ArgUtil;
 
 public class ITemplates {
 
@@ -29,9 +30,9 @@ public class ITemplates {
 
 	public static ITemplate getTemplate(String templateStr) {
 		if (ProjectConfig.PROJECT == Project.IB) {
-			return TemplatesIB.valueOf(templateStr);
+			return (ITemplate) ArgUtil.parseAsEnum(templateStr, TemplatesIB.class);
 		}
-		return TemplatesMX.valueOf(templateStr);
+		return (ITemplate) ArgUtil.parseAsEnum(templateStr, TemplatesMX.class);
 	}
 
 }
