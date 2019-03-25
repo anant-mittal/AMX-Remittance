b44381124773b6c6a7cb98e604bb2ab16f9ad495
Anant Mittal <anant.mittal@almullaexchange.com>
2019-03-25 13:37:49 +0530

    Fingerprint linked flag created.

--- lib-jax/src/main/java/com/amx/jax/model/response/customer/CustomerFlags.java
+++ lib-jax/src/main/java/com/amx/jax/model/response/customer/CustomerFlags.java
@@ -3,6 +3,15 @@ package com.amx.jax.model.response.customer;
 public class CustomerFlags {
 
 	public String idProofStatus;
+	public Boolean fingerprintlinked;
+
+	public Boolean getFingerprintlinked() {
+		return fingerprintlinked;
+	}
+
+	public void setFingerprintlinked(Boolean fingerprintlinked) {
+		this.fingerprintlinked = fingerprintlinked;
+	}
 
 	public String getIdProofStatus() {
 		return idProofStatus;
@@ -11,6 +20,6 @@ public class CustomerFlags {
 	public void setIdProofStatus(String idProofStatus) {
 		this.idProofStatus = idProofStatus;
 	}
-
+	
 
 }
