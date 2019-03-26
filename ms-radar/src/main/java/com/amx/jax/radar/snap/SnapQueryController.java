84bd812c607d479692384391decc34804848065a
LT <lalit.tanwar07@gmail.com>
2019-03-26 10:39:50 +0530

    SnapContoller Table Added

--- ms-radar/src/main/java/com/amx/jax/radar/snap/SnapQueryController.java
+++ ms-radar/src/main/java/com/amx/jax/radar/snap/SnapQueryController.java
@@ -9,10 +9,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.amx.jax.client.snap.SnapConstants.SnapQueryTemplate;
 import com.amx.jax.client.snap.SnapModels.SnapModelWrapper;
+import com.amx.jax.radar.jobs.customer.OracleVarsCache;
+import com.amx.jax.radar.jobs.customer.OracleVarsCache.DBSyncJobs;
 import com.amx.jax.rest.RestService;
 
 @Controller
@@ -23,6 +26,9 @@ public class SnapQueryController {
 
 	@Autowired
 	RestService restService;
+
+	@Autowired
+	OracleVarsCache oracleVarsCache;
 
 	@ResponseBody
 	@RequestMapping(value = "/snap/query/{snapView}", method = RequestMethod.POST)
@@ -36,6 +42,29 @@ public class SnapQueryController {
 	public SnapModelWrapper snapView(@PathVariable(value = "snapView") SnapQueryTemplate snapView,
 			@RequestBody Map<String, Object> params) throws IOException {
 		return snapQueryTemplateService.execute(snapView, params);
+	}
+
+	@ResponseBody
+	@RequestMapping(value = "/snap/reset/start/{dbSyncJobs}", method = RequestMethod.GET)
+	public String snapResetStart(@PathVariable(value = "dbSyncJobs") DBSyncJobs dbSyncJobs,
+			@RequestBody Map<String, Object> params) throws IOException {
+		oracleVarsCache.clearStampStart(dbSyncJobs);
+		return "CLEARED";
+	}
+
+	@ResponseBody
+	@RequestMapping(value = "/snap/reset/end/{dbSyncJobs}", method = RequestMethod.GET)
+	public String snapResetEnd(@PathVariable(value = "dbSyncJobs") DBSyncJobs dbSyncJobs,
+			@RequestBody Map<String, Object> params) throws IOException {
+		oracleVarsCache.clearStampEnd(dbSyncJobs);
+		return "CLEARED";
+	}
+
+	@RequestMapping(value = "/snap/table/{snapView}", method = RequestMethod.GET)
+	public String table(@PathVariable(value = "snapView") SnapQueryTemplate snapView,
+			@RequestParam String gte, @RequestParam String lte) throws IOException {
+		
+		return "table";
 	}
 
 }
