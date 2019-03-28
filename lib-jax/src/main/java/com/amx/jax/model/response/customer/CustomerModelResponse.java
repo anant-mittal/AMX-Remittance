a269fc6c4a2cb8a2a65739a4f31848c39e417dbc
Prashant Thorat <prashantnitdgp@gmail.com>
2019-03-28 18:02:17 +0530

    customer model resp

--- lib-jax/src/main/java/com/amx/jax/model/response/customer/CustomerModelResponse.java
+++ lib-jax/src/main/java/com/amx/jax/model/response/customer/CustomerModelResponse.java
@@ -1,9 +1,22 @@
 package com.amx.jax.model.response.customer;
 
+import java.math.BigDecimal;
+
 public class CustomerModelResponse {
 
+	BigDecimal customerId;
 	PersonInfo personInfo;
 	CustomerFlags customerFlags;
+
+	public CustomerModelResponse() {
+		super();
+	}
+
+	public CustomerModelResponse(PersonInfo personInfo, CustomerFlags customerFlags) {
+		super();
+		this.personInfo = personInfo;
+		this.customerFlags = customerFlags;
+	}
 
 	public PersonInfo getPersonInfo() {
 		return personInfo;
@@ -19,5 +32,13 @@ public class CustomerModelResponse {
 
 	public void setCustomerFlags(CustomerFlags customerFlags) {
 		this.customerFlags = customerFlags;
+	}
+
+	public BigDecimal getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(BigDecimal customerId) {
+		this.customerId = customerId;
 	}
 }
--- ms-user/src/main/java/com/amx/jax/userservice/service/CustomerModelService.java
+++ ms-user/src/main/java/com/amx/jax/userservice/service/CustomerModelService.java
@@ -1,19 +1,44 @@
 package com.amx.jax.userservice.service;
 
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.WebApplicationContext;
 
+import com.amx.jax.constant.ConstantDocument;
+import com.amx.jax.constant.JaxApiFlow;
+import com.amx.jax.dbmodel.Customer;
+import com.amx.jax.model.response.customer.CustomerFlags;
 import com.amx.jax.model.response.customer.CustomerModelResponse;
+import com.amx.jax.model.response.customer.PersonInfo;
+import com.amx.jax.userservice.manager.CustomerFlagManager;
 
 @Service
 @Scope(value = WebApplicationContext.SCOPE_REQUEST, proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class CustomerModelService {
 
+	@Autowired
+	UserValidationService userValidationService;
+	@Autowired
+	UserService userService;
+	@Autowired
+	CustomerFlagManager customerFlagManager;
+
 	public CustomerModelResponse getCustomerModelResponse(String identityInt) {
-		// TODO Auto-generated method stub
-		return null;
+		userValidationService.validateIdentityInt(identityInt, ConstantDocument.BIZ_COMPONENT_ID_CIVIL_ID);
+		List<Customer> customers = userValidationService.validateNonActiveOrNonRegisteredCustomerStatus(identityInt,
+				JaxApiFlow.SIGNUP_ONLINE);
+		Customer customer = customers.get(0);
+		BigDecimal customerId = customer.getCustomerId();
+		PersonInfo personInfo = userService.getPersonInfo(customerId);
+		CustomerFlags customerFlags = customerFlagManager.getCustomerFlags(customerId);
+		CustomerModelResponse response = new CustomerModelResponse(personInfo, customerFlags);
+		response.setCustomerId(customer.getCustomerId());
+		return response;
 	}
 
 }
