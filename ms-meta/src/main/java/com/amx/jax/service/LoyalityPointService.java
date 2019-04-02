d28d830ea1de20e3110bb3f4faf1e55317187af0
Prashant Thorat <prashantnitdgp@gmail.com>
2019-04-02 13:56:25 +0530

    loyality points method for corporate

--- ms-meta/src/main/java/com/amx/jax/service/LoyalityPointService.java
+++ ms-meta/src/main/java/com/amx/jax/service/LoyalityPointService.java
@@ -11,6 +11,7 @@ import org.springframework.web.context.WebApplicationContext;
 import com.amx.jax.dbmodel.remittance.VwLoyalityEncash;
 import com.amx.jax.meta.MetaData;
 import com.amx.jax.repository.VwLoyalityEncashRepository;
+import com.amx.jax.util.JaxUtil;
 
 @Service
 @Scope(value = WebApplicationContext.SCOPE_REQUEST, proxyMode = ScopedProxyMode.TARGET_CLASS)
@@ -19,7 +20,6 @@ public class LoyalityPointService {
 
 	@Autowired
 	VwLoyalityEncashRepository repo;
-
 	@Autowired
 	MetaData meta;
 
@@ -31,5 +31,21 @@ public class LoyalityPointService {
 
 	public BigDecimal getTodaysLoyalityPointsEncashed() {
 		return repo.getTodaysLoyalityPointsEncashed(meta.getCustomerId());
+	}
+
+	public BigDecimal getloyaltyAmountEncashed(BigDecimal commission, BigDecimal corporateDiscount) {
+		BigDecimal loyalityPoints = getVwLoyalityEncash().getLoyalityPoint();
+		BigDecimal loyalityPointsEncashed = getVwLoyalityEncash().getEquivalentAmount();
+		if (JaxUtil.isNullZeroBigDecimalCheck(commission) && JaxUtil.isNullZeroBigDecimalCheck(loyalityPoints)
+				&& loyalityPointsEncashed.compareTo(corporateDiscount) > 0) {
+			if (commission.compareTo(loyalityPointsEncashed) >= 0) {
+				loyalityPointsEncashed = loyalityPointsEncashed.subtract(BigDecimal.ZERO);
+			} else {
+				loyalityPointsEncashed = loyalityPointsEncashed.subtract(corporateDiscount);
+			}
+		}
+
+		return loyalityPointsEncashed;
+
 	}
 }
