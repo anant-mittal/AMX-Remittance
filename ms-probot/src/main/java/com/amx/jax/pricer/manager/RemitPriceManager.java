f73a3aed439b1d9faaa6fbddcedf8f979c3980dc
abkamble <abkamble@gmail.com>
2019-04-02 16:54:41 +0530

    Exchange Rates for Branch Corrected.

--- ms-probot/src/main/java/com/amx/jax/pricer/manager/RemitPriceManager.java
+++ ms-probot/src/main/java/com/amx/jax/pricer/manager/RemitPriceManager.java
@@ -193,12 +193,12 @@ public class RemitPriceManager {
 
 				bankExchangeRates = exchangeRateDao.getBranchExchangeRatesForRoutingBanksAndServiceIds(
 						requestDto.getForeignCurrencyId(), requestDto.getCountryBranchId(),
-						requestDto.getForeignCountryId(), requestDto.getLocalCountryId(), validBankIds, serviceIdsList);
+						 requestDto.getLocalCountryId(), validBankIds, serviceIdsList);
 			} else {
 
 				bankExchangeRates = exchangeRateDao.getBranchExchangeRatesForRoutingBanks(
 						requestDto.getForeignCurrencyId(), requestDto.getCountryBranchId(),
-						requestDto.getForeignCountryId(), requestDto.getLocalCountryId(), validBankIds);
+						requestDto.getLocalCountryId(), validBankIds);
 
 			}
 
