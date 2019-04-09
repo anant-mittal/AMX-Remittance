27db2b5d7692763f120d09ccb94ac2417710ab7c
Prashant Thorat <prashantnitdgp@gmail.com>
2019-04-09 11:54:05 +0530

    maskutil added

--- common-lib/src/main/java/com/amx/utils/MaskUtil.java
+++ common-lib/src/main/java/com/amx/utils/MaskUtil.java
@@ -3,16 +3,35 @@ package com.amx.utils;
 public final class MaskUtil {
 
 	/**
-	 * Masks input string as per lenght and maskstring
+	 * Masks input string as per lenght and maskstring from left
 	 * 
 	 * @param input
-	 * @param maskLength
+	 * @param unmaskLength
+	 *            - length of chars which need not be masked
 	 * @param maskString
 	 * @return
 	 */
-	public static final String maskString(String input, final int maskLength, final String maskString) {
+	public static final String leftMask(String input, final int unmaskLength, final String maskString) {
 
-		String regex = ".(?=.{" + maskLength + "})";
+		String regex = ".(?=.{" + unmaskLength + "})";
 		return input.replaceAll(regex, maskString);
+	}
+
+	/**
+	 * Mask email id from left side
+	 * 
+	 * @param email
+	 * @param unmaskLength
+	 * @param maskString
+	 * @return
+	 */
+	public static final String maskEmail(String emailId, final int unmaskLength, final String maskString) {
+		String email = emailId.split("@")[0];
+		String domain = emailId.split("@")[1];
+		StringBuilder maskedEmail = new StringBuilder();
+		maskedEmail.append(leftMask(email, unmaskLength, maskString));
+		maskedEmail.append("@");
+		maskedEmail.append(domain);
+		return maskedEmail.toString();
 	}
 }
--- common-lib/src/test/java/com/jax/util/MaskUtilTest.java
+++ common-lib/src/test/java/com/jax/util/MaskUtilTest.java
@@ -10,8 +10,15 @@ public class MaskUtilTest {
 
 	@Test
 	public void testMask() {
-		String mobile = "8123944923";
-		String output = MaskUtil.maskString(mobile, 4, "*");
+		String mobile = "8123";
+		String output = MaskUtil.leftMask(mobile, 4, "*");
+		assertEquals("******4923", output);
+	}
+	
+	@Test
+	public void testMaskEmail() {
+		String email = "pras@almullagroup.com";
+		String output = MaskUtil.maskEmail(email, 4, "*");
 		assertEquals("******4923", output);
 	}
 }
