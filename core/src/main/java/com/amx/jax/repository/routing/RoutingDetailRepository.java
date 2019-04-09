50ef1bd9810d98798e2474aa07ce7fbfc2325569
Prashant Thorat <prashantnitdgp@gmail.com>
2019-04-09 13:10:41 +0530

    bug id 5722

--- core/src/main/java/com/amx/jax/repository/routing/RoutingDetailRepository.java
+++ core/src/main/java/com/amx/jax/repository/routing/RoutingDetailRepository.java
@@ -7,11 +7,13 @@ import java.util.List;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import com.amx.jax.dbmodel.CountryMaster;
 import com.amx.jax.dbmodel.CurrencyMasterModel;
 import com.amx.jax.dbmodel.routing.RoutingDetails;
 
 public interface RoutingDetailRepository extends CrudRepository<RoutingDetails, Serializable> {
 
-	@Query("select distinct rd.agentBankId from RoutingDetails rd where rd.agentBankId is not null and  rd.isActive='Y' and rd.exCurrenyId= ?1")
-	List<BigDecimal> getCashRoutingBanks(CurrencyMasterModel currencyId);
+	@Query("select distinct rd.agentBankId from RoutingDetails rd where rd.agentBankId is not null and  rd.isActive='Y' and rd.exCurrenyId= ?1"
+			+ " and exCountryId= ?2")
+	List<BigDecimal> getCashRoutingBanks(CurrencyMasterModel currencyId, CountryMaster countryMaster);
 }
