ec07a5f8b9899775756d0b39d619418b8d6e8bd0
Radhika.Ala <radhika.ala@almullaexchange.com>
2019-04-13 10:58:00 +0530

    6095 changes

--- ms-jax/src/main/java/com/amx/jax/services/DeviceStateService.java
+++ ms-jax/src/main/java/com/amx/jax/services/DeviceStateService.java
@@ -148,8 +148,6 @@ public class DeviceStateService extends AbstractService {
 
 	public BoolRespModel updateSignatureStateData(Integer deviceRegId, String imageUrlStr) {
 		//
-		if(!(deviceRegId==null))
-		{
 		validateDeviceRegId(deviceRegId);
 		devicestateValidation.validateDeviceRegIdndImageURL(deviceRegId, imageUrlStr);
 		DeviceStateInfo deviceStateInfo = deviceDao.getDeviceStateInfo(new BigDecimal(deviceRegId));
@@ -157,9 +155,6 @@ public class DeviceStateService extends AbstractService {
 		deviceDao.saveDeviceInfo(deviceStateInfo);
 
 		return new BoolRespModel(Boolean.TRUE);
-		}else
-			throw new GlobalException("Device Registartion Id is Required");
-			
 	}
 
 	public void validateDeviceRegId(Integer deviceRegId) {
