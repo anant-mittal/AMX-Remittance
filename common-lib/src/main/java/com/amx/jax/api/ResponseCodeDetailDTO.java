794726bb83eb1fae1ddbdcad3d1ce93cc406ee2f
Subodh Bhoir <subodh.bhoir@almullagroup.com>
2019-04-24 12:58:08 +0530

    6088 - type and status added

--- common-lib/src/main/java/com/amx/jax/api/ResponseCodeDetailDTO.java
+++ common-lib/src/main/java/com/amx/jax/api/ResponseCodeDetailDTO.java
@@ -8,11 +8,19 @@ public class ResponseCodeDetailDTO implements Serializable {
 
 	private static final long serialVersionUID = -5279804951579408228L;
 
-	private String responseCode;
-	private String responseDesc;
+	private String responseCode; // client
+	private String responseDesc; // client
 	private String almullaErrorCode;
 	private CodeCategory category;
+	private String type;
+	private String status;
+	
+	// Type ?
+	// Status
 
+	// Map <String, String> clientResp
+
+	
 	public String getResponseCode() {
 		return responseCode;
 	}
@@ -43,6 +51,22 @@ public class ResponseCodeDetailDTO implements Serializable {
 
 	public void setCategory(CodeCategory category) {
 		this.category = category;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
 	}
 
 }
--- common-lib/src/main/java/com/amx/jax/dict/PayGRespCodeJSONConverter.java
+++ common-lib/src/main/java/com/amx/jax/dict/PayGRespCodeJSONConverter.java
@@ -20,6 +20,8 @@ public class PayGRespCodeJSONConverter {
 					responseCodeDetail.setResponseDesc(enumBHR.getResponseDesc());
 					responseCodeDetail.setAlmullaErrorCode(enumBHR.getAlmullaErrorCode());
 					responseCodeDetail.setCategory(enumBHR.getCategory());
+					responseCodeDetail.setType(enumBHR.getCategory().getType().toString());
+					responseCodeDetail.setStatus(enumBHR.getCategory().getStatus().toString());
 					
 					LOGGER.info("Response Code Details BHR JSON : " +responseCodeDetail.toString());
 				}
