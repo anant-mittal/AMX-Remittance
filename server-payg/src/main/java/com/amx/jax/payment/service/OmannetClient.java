d2e3f4b49e6f9d644c25e13fa78ebadd74fca24d
Subodh Bhoir <subodh.bhoir@almullagroup.com>
2019-05-02 15:47:38 +0530

    removing trim

--- server-payg/src/main/java/com/amx/jax/payment/service/OmannetClient.java
+++ server-payg/src/main/java/com/amx/jax/payment/service/OmannetClient.java
@@ -190,18 +190,19 @@ public class OmannetClient implements PayGClient {
 			/*OmanNetCodes statusCode = (OmanNetCodes) PayGCodes.getPayGCode(resultResponse, OmanNetCodes.UNKNOWN);
 			gatewayResponse.setErrorCategory(statusCode.getCategory().name());*/
 			
-			String trimError = pipe.getResult();
+			/*String trimError = pipe.getResult();
 			if(trimError.contains(" ")){
 				trimError = trimError.substring(0, trimError.indexOf(" "));
 				LOGGER.info("TrimError Value ---> " + trimError);
-			}
+			}*/
 			
-			ResponseCodeOMN responseCodeEnum = ResponseCodeOMN.getResponseCodeEnumByCode(trimError);
+			ResponseCodeOMN responseCodeEnum = ResponseCodeOMN.getResponseCodeEnumByCode(pipe.getResult());
 			if(responseCodeEnum != null) {
 				gatewayResponse.setErrorCategory(responseCodeEnum.name());
-				LOGGER.info("Result from response Values ---> " + gatewayResponse.getErrorCategory());
+				LOGGER.info("Result from response Values IF ---> " + gatewayResponse.getErrorCategory());
 			}else {
 				gatewayResponse.setErrorCategory(ResponseCodeOMN.UNKNOWN.name());
+				LOGGER.info("Result from response Values ELSE ---> " + gatewayResponse.getErrorCategory());
 			}
 			gatewayResponse.setError(resultResponse);
 		}
