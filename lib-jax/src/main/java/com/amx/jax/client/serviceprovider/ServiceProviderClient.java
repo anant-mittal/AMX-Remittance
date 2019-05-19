69ddaf5514d68b07dd4fde9d13c05c8968cb2279
Ahmad Salman <ahmad.salman@almullaexchange.com>
2019-05-19 14:40:54 +0300

    Change in the client

--- lib-jax/src/main/java/com/amx/jax/client/serviceprovider/ServiceProviderClient.java
+++ lib-jax/src/main/java/com/amx/jax/client/serviceprovider/ServiceProviderClient.java
@@ -7,7 +7,10 @@ import org.springframework.stereotype.Component;
 
 import com.amx.jax.AppConfig;
 import com.amx.jax.api.AmxApiResponse;
+import com.amx.jax.client.configs.JaxMetaInfo;
+import com.amx.jax.client.fx.IFxOrderService.Path;
 import com.amx.jax.exception.JaxSystemError;
+import com.amx.jax.model.request.CustomerShippingAddressRequestModel;
 import com.amx.jax.model.request.serviceprovider.ServiceProviderCallRequestDto;
 import com.amx.jax.model.response.serviceprovider.ServiceProviderResponse;
 import com.amx.jax.rest.RestService;
@@ -29,8 +32,9 @@ public class ServiceProviderClient implements IServiceProviderService
 	{
 		try
 		{
+		
 			LOGGER.debug("in get quataion :");
-			return restService.ajax(appConfig.getJaxURL() + Path.GET_QUATATION).post()
+			return restService.ajax(appConfig.getJaxURL() + Path.GET_QUATATION).meta(new JaxMetaInfo()).post(quatationRequestDto)
 					.as(new ParameterizedTypeReference<AmxApiResponse<ServiceProviderResponse, Object>>()
 					{
 					});
@@ -48,7 +52,7 @@ public class ServiceProviderClient implements IServiceProviderService
 		try
 		{
 			LOGGER.debug("in send remittance :");
-			return restService.ajax(appConfig.getJaxURL() + Path.SEND_REMITTANCE).get()
+			return restService.ajax(appConfig.getJaxURL() + Path.SEND_REMITTANCE).meta(new JaxMetaInfo()).post(sendRemittanceRequestDto)
 					.as(new ParameterizedTypeReference<AmxApiResponse<ServiceProviderResponse, Object>>()
 					{
 					});
