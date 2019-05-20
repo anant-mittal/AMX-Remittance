140cf37ede4c05df3c5aaf015d15eca7357c5cab
dipali <dipali.pingale@almullaexchange.com>
2019-05-20 15:34:56 +0530

    space removed

--- lib-postman/src/main/java/com/amx/jax/postman/model/TemplatesMX.java
+++ lib-postman/src/main/java/com/amx/jax/postman/model/TemplatesMX.java
@@ -22,8 +22,6 @@ public enum TemplatesMX implements ITemplate {
 	REMIT_RECEIPT_JASPER_NO_HEADER("TransactionReceiptNoHeader_jasper", PDFConverter.JASPER, "TransactionReceipt.json"),
 	REMIT_APPLICATION_RECEIPT_JASPER("ApplicationReceipt_jasper", PDFConverter.JASPER, "ApplicationReceipt.json"),
 
-
-
 	REMIT_RECEIPT_COPY("TransactionReceiptCopy"), REMIT_STATMENT("TransactionList"),
 	REMIT_STATMENT_EMAIL("RemittanceStatmentEmail"),
 	REMIT_STATMENT_EMAIL_FILE("TransactionStatementHistory"),
