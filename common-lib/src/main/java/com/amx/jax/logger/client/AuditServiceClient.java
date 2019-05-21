0bb1ece99a4c3888231175af29267155eb09d775
LT <lalit.tanwar07@gmail.com>
2019-05-21 11:39:25 +0530

    Added OverRidaable Method getTypeMarker

--- common-lib/src/main/java/com/amx/jax/logger/client/AuditServiceClient.java
+++ common-lib/src/main/java/com/amx/jax/logger/client/AuditServiceClient.java
@@ -203,7 +203,7 @@ public class AuditServiceClient implements AuditService {
 			LOGGER2.error("Exception while logStatic {}", JsonUtil.toJson(event));
 			return null;
 		}
-		EventMarker eventMarker = eventType.marker();
+		EventMarker eventMarker = event.getTypeMarker();
 		if (eventMarker == null) {
 			eventMarker = EventMarker.AUDIT;
 		}
