9e874b873079873180afd7189de0183afa99524a
Prashant Thorat <prashantnitdgp@gmail.com>
2019-05-27 13:29:03 +0530

    get document

---
+++ lib-jax/src/main/java/com/amx/jax/model/customer/DocumentImageRenderType.java
@@ -0,0 +1,6 @@
+package com.amx.jax.model.customer;
+
+public enum DocumentImageRenderType {
+
+	URL, STRING;
+}
---
+++ ms-jax-modules/ms-jax-customer/src/main/java/com/amx/jax/customer/document/manager/CustomerDocumentManager.java
@@ -0,0 +1,9 @@
+package com.amx.jax.customer.document.manager;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomerDocumentManager {
+
+	
+}
