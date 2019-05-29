f55fed6a49812370d93f538dbfadcf51667fd935
Subodh Bhoir <subodh.bhoir@almullagroup.com>
2019-05-29 11:44:52 +0530

    mail configured

--- ms-jax/src/main/java/com/amx/jax/manager/DailyPromotionManager.java
+++ ms-jax/src/main/java/com/amx/jax/manager/DailyPromotionManager.java
@@ -129,7 +129,8 @@ public class DailyPromotionManager {
 				logger.info("Sending WantIT BuyIT voucher Email to Marketing New : ");
 				Email wantITbuyITEmailM = new Email();
 				wantITbuyITEmailM.setSubject("Congratulations! You have got a coupon from Al Mulla Exchange.");
-				wantITbuyITEmailM.addTo("bhoirsubodh24@gmail.com");
+				wantITbuyITEmailM.addTo("preeti.pawar@almullaexchange.com");
+				wantITbuyITEmailM.addTo("almullatest@gmail.com");
 				wantITbuyITEmailM.setITemplate(TemplatesMX.WANTIT_BUYIT_PROMOTION);
 				wantITbuyITEmailM.setHtml(true);
 				wantITbuyITEmailM.getModel().put(NotificationConstants.RESP_DATA_KEY, dailyPromotionDTO);
