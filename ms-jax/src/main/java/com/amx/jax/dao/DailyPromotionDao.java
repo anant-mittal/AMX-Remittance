6eb19ace445da9f88135f83c07b1d777b79229a6
Subodh Bhoir <subodh.bhoir@almullagroup.com>
2019-06-03 16:36:52 +0530

    6888 - Marketing mail removed

--- ms-jax/src/main/java/com/amx/jax/dao/DailyPromotionDao.java
+++ ms-jax/src/main/java/com/amx/jax/dao/DailyPromotionDao.java
@@ -1,13 +1,19 @@
 package com.amx.jax.dao;
 
+import java.math.BigDecimal;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.amx.jax.dbmodel.promotion.DailyPromotion;
 import com.amx.jax.repository.promotion.DailyPromotionRepository;
 
 public class DailyPromotionDao {
 
 	@Autowired
 	DailyPromotionRepository dailyPromotionRepository;
-	
-	
+
+	public DailyPromotion getWantitByTrnxId(BigDecimal remittanceTransactionId) {
+
+		return dailyPromotionRepository.getWantitByTrnxId(remittanceTransactionId);
+	}
 }
