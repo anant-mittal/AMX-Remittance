d522f98dfefa278416b196927d1a5a952fa80718
Subodh Bhoir <subodh.bhoir@almullagroup.com>
2019-06-03 16:50:50 +0530

    6888 - promo code text added

--- ms-jax/src/main/java/com/amx/jax/dao/DailyPromotionDao.java
+++ ms-jax/src/main/java/com/amx/jax/dao/DailyPromotionDao.java
@@ -3,10 +3,12 @@ package com.amx.jax.dao;
 import java.math.BigDecimal;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import com.amx.jax.dbmodel.promotion.DailyPromotion;
 import com.amx.jax.repository.promotion.DailyPromotionRepository;
 
+@Component
 public class DailyPromotionDao {
 
 	@Autowired
