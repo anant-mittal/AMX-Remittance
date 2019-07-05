f8d631432bb7fe8ac254ae5d52b3b1d12fd3edf3
Radhika.Ala <radhika.ala@almullaexchange.com>
2019-07-05 17:48:06 +0530

    Local changes for country list

--- core/src/main/java/com/amx/jax/dbmodel/CountryMasterView.java
+++ core/src/main/java/com/amx/jax/dbmodel/CountryMasterView.java
@@ -55,6 +55,7 @@ public class CountryMasterView implements java.io.Serializable, IResourceEntity 
 	private String countryMobileLength;
 	private Integer beneCountryRisk;
 	private Integer remitterCountryRisk;
+	private String localName;
 
 	public CountryMasterView() {
 		super();
@@ -292,5 +293,16 @@ public class CountryMasterView implements java.io.Serializable, IResourceEntity 
 	public void setRemitterCountryRisk(Integer remitterCountryRisk) {
 		this.remitterCountryRisk = remitterCountryRisk;
 	}
+
+	@Column(name = "LOCAL_NAME")
+	public String getLocalName() {
+		return localName;
+	}
+
+	public void setLocalName(String localName) {
+		this.localName = localName;
+	}
+	
+	
 
 }
