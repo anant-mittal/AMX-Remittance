762c3284a011ba1743d24bdfc12cbbda0f82ac80
LT <lalit.tanwar07@gmail.com>
2019-07-09 17:11:08 +0530

    Ignore

--- lib-jax/src/main/java/com/amx/jax/model/request/remittance/RemittanceTransactionRequestModel.java
+++ lib-jax/src/main/java/com/amx/jax/model/request/remittance/RemittanceTransactionRequestModel.java
@@ -8,9 +8,11 @@ import java.math.BigDecimal;
 import javax.validation.constraints.NotNull;
 
 import com.amx.jax.model.response.ExchangeRateBreakup;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
 @JsonDeserialize(as = RemittanceTransactionDrRequestModel.class)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class RemittanceTransactionRequestModel extends RemittanceAdditionalBeneFieldModel implements IRemitTransReqPurpose {
 
 	/**
