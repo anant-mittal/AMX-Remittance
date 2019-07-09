86d71aa52638391df90ad6b6772e502e847669fa
Radhika.Ala <radhika.ala@almullaexchange.com>
2019-07-09 15:58:23 +0530

    Email verification changes

--- core/src/main/java/com/amx/jax/services/JaxNotificationService.java
+++ core/src/main/java/com/amx/jax/services/JaxNotificationService.java
@@ -73,7 +73,7 @@ public class JaxNotificationService {
 		} else if (TenantContextHolder.currentSite().equals(Tenant.OMN)) {
 			email.setSubject("Your transaction on Modern Exchange - Oman is successful");
 		}
-		if (!(pinfo.getEmailVerified().equalsIgnoreCase("N")) && !pinfo.getEmailVerified().isEmpty()) {
+
 
 			email.addTo(pinfo.getEmail());
 			email.setITemplate(TemplatesMX.TXN_CRT_SUCC);
@@ -89,16 +89,7 @@ public class JaxNotificationService {
 			email.addFile(file);
 			logger.debug("Email to - " + pinfo.getEmail() + " first name : " + pinfo.getFirstName());
 			sendEmail(email);
-		} else {
-
-			email.addTo(pinfo.getEmail());
-			email.setITemplate(TemplatesMX.CONTACT_VERIFICATION_EMAIL);
-			email.setHtml(true);
-			email.getModel().put(RESP_DATA_KEY, pinfo);
-			logger.info("Email to - " + pinfo.getEmail() + " first name : " + pinfo.getFirstName());
-			sendEmail(email);
-
-		}
+		
 	}
 
 	public void sendTransactionNotification(FxOrderReportResponseDto remittanceReceiptSubreport,
