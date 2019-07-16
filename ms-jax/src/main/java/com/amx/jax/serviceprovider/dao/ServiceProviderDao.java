7594904cdc306fa18db29fd3a18fb4b16f61435e
Anant Mittal <anant.mittal@almullaexchange.com>
2019-07-16 11:25:20 +0530

    Service provider temporary upload done.

--- ms-jax/src/main/java/com/amx/jax/serviceprovider/dao/ServiceProviderDao.java
+++ ms-jax/src/main/java/com/amx/jax/serviceprovider/dao/ServiceProviderDao.java
@@ -9,8 +9,10 @@ import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.WebApplicationContext;
 
+import com.amx.jax.dbmodel.FileUploadTempModel;
 import com.amx.jax.dbmodel.ServiceProviderPartner;
 import com.amx.jax.repository.ServiceProviderPartnerRepository;
+import com.amx.jax.repository.ServiceProviderTempUploadRepository;
 
 @Component
 @Scope(value = WebApplicationContext.SCOPE_REQUEST, proxyMode = ScopedProxyMode.TARGET_CLASS)
@@ -18,11 +20,17 @@ public class ServiceProviderDao {
 	private Logger logger = Logger.getLogger(ServiceProviderDao.class);
 	@Autowired
 	ServiceProviderPartnerRepository serviceProviderPartnerRepository;
+	@Autowired
+	ServiceProviderTempUploadRepository serviceProviderTempUploadRepository;
 	
 	public List<ServiceProviderPartner> getServiceProviderPartner() {
 		List<ServiceProviderPartner> serviceProviderPartner =  serviceProviderPartnerRepository.getServiceProviderPartner();
 		return serviceProviderPartner;
 		
+	}
+	public void saveFileUploadTemp(FileUploadTempModel fileUploadTempModel) {
+		
+		serviceProviderTempUploadRepository.save(fileUploadTempModel);
 	}
 	
 }
--- ms-jax/src/main/java/com/amx/jax/serviceprovider/service/ServiceProviderService.java
+++ ms-jax/src/main/java/com/amx/jax/serviceprovider/service/ServiceProviderService.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Date;
@@ -26,6 +27,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.amx.amxlib.exception.jax.GlobalException;
 import com.amx.jax.api.BoolRespModel;
+import com.amx.jax.dbmodel.FileUploadTempModel;
 import com.amx.jax.dbmodel.ServiceProviderPartner;
 import com.amx.jax.response.branchuser.ServiceProviderPartnerResponse;
 import com.amx.jax.serviceprovider.dao.ServiceProviderDao;
@@ -97,14 +99,49 @@ public class ServiceProviderService extends AbstractService {
 	    		if(!(cellValue1.equals(String.valueOf(year))&&cellValue2.equals(String.valueOf(month))&&cellValue3.equals(String.valueOf(day)))) {
 	    			throw new GlobalException("File Date is not matching with the date input");
 	    		}
+	    		j=4;
 	    	}
 	    }
-	    for (Row row: sheet) {
-            for(Cell cell: row) {
-                String cellValue = dataFormatter.formatCellValue(cell);
-                
-                logger.info(cellValue + "\t");
-            }
+	    FileUploadTempModel fileUploadTempModel = new FileUploadTempModel();
+	    for(i=1;i<sheet.getLastRowNum();i++) {
+	    	Row row = sheet.getRow(i);
+	    	Cell cell0 = row.getCell(0);
+	    	String cellValue0 = dataFormatter.formatCellValue(cell0);
+	    	fileUploadTempModel.setLocalYear(new BigDecimal(cellValue0));
+	    	Cell cell1 = row.getCell(1);
+	    	String cellValue1 = dataFormatter.formatCellValue(cell1);
+	    	fileUploadTempModel.setReportingYear(new BigDecimal(cellValue1));
+	    	Cell cell2 = row.getCell(2);
+	    	String cellValue2 = dataFormatter.formatCellValue(cell2);
+	    	fileUploadTempModel.setReportingMonth(new BigDecimal(cellValue2));
+	    	Cell cell3 = row.getCell(3);
+	    	String cellValue3 = dataFormatter.formatCellValue(cell3);
+	    	fileUploadTempModel.setReportingDate(new BigDecimal(cellValue3));
+	    	Cell cell4 = row.getCell(4);
+	    	String cellValue4 = dataFormatter.formatCellValue(cell4);
+	    	fileUploadTempModel.setDirection(cellValue4);
+	    	Cell cell5 = row.getCell(5);
+	    	String cellValue5 = dataFormatter.formatCellValue(cell5);
+	    	fileUploadTempModel.setAccountNo(cellValue5);
+	    	Cell cell6 = row.getCell(6);
+	    	String cellValue6 = dataFormatter.formatCellValue(cell6);
+	    	fileUploadTempModel.setLocationName(cellValue6);
+	    	Cell cell7 = row.getCell(7);
+	    	String cellValue7 = dataFormatter.formatCellValue(cell7);
+	    	fileUploadTempModel.setLocalCurrencyCode(cellValue7);
+	    	Cell cell8 = row.getCell(8);
+	    	String cellValue8 = dataFormatter.formatCellValue(cell8);
+	    	fileUploadTempModel.setCompanyShareLocal(new BigDecimal(cellValue8));
+	    	Cell cell9 = row.getCell(9);
+	    	String cellValue9 = dataFormatter.formatCellValue(cell9);
+	    	fileUploadTempModel.setExchangeGainLocal(new BigDecimal(cellValue9));
+	    	Cell cell10 = row.getCell(10);
+	    	String cellValue10 = dataFormatter.formatCellValue(cell10);
+	    	fileUploadTempModel.setSendPayIndicator(cellValue10);
+            Cell cell11 = row.getCell(11);
+            String cellValue11 = dataFormatter.formatCellValue(cell11);
+            fileUploadTempModel.setMtcnNo(cellValue11);
+            serviceProviderDao.saveFileUploadTemp(fileUploadTempModel);
             logger.info("\n");
         }
 	    
