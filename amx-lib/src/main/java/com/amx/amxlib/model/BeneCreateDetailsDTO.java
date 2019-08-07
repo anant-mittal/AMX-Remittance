3d98880978dd4c04b8765854ef92351b39d98b63
dipali <dipali.pingale@almullaexchange.com>
2019-08-07 18:37:02 +0530

    BeneCreationDTO Added

---
+++ amx-lib/src/main/java/com/amx/amxlib/model/BeneCreateDetailsDTO.java
@@ -0,0 +1,38 @@
+package com.amx.amxlib.model;
+
+public class BeneCreateDetailsDTO {
+	
+	private String customerName;
+
+	private String beneBankName;
+	
+	private String beneCountry;
+
+	public String getCustomerName() {
+		return customerName;
+	}
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
+	public String getBeneBankName() {
+		return beneBankName;
+	}
+
+	public void setBeneBankName(String beneBankName) {
+		this.beneBankName = beneBankName;
+	}
+
+	public String getBeneCountry() {
+		return beneCountry;
+	}
+
+	public void setBeneCountry(String beneCountry) {
+		this.beneCountry = beneCountry;
+	}
+
+	
+	
+
+}
