6d8153f220ab632e026e9db85d98a8b1988d479e
AMGDOM\chiranjeevi <chiranjeevi.veeravalli@almullagroup.com>
2019-08-12 11:20:56 +0300

    Home Send related changes done 1. timer is kept only five minutes max 2. bank service rule and bank charges changes done

--- ms-probot/src/main/java/com/amx/jax/pricer/repository/IBankChargesRepository.java
+++ ms-probot/src/main/java/com/amx/jax/pricer/repository/IBankChargesRepository.java
@@ -12,7 +12,7 @@ import com.amx.jax.pricer.dbmodel.BankCharges;
 
 public interface IBankChargesRepository extends CrudRepository<BankCharges, Serializable> {
 	
-	@Query(value = "SELECT * FROM EX_BANK_CHARGES WHERE BANK_SERVICE_RULE_ID=?1 AND FROM_AMOUNT<=?2 AND TO_AMOUNT>=?2 AND CHARGES_FOR=?3 AND CHARGES_TYPE=?4 AND ISACTIVE = 'Y'", nativeQuery = true)
-	public List<BankCharges> fetchBankCharges(BigDecimal bankServiceRuleId,BigDecimal fcAmount,BigDecimal chargesFor,String chargesType);
+	@Query(value = "SELECT * FROM EX_BANK_CHARGES WHERE BANK_SERVICE_RULE_ID=?1 AND FROM_AMOUNT<=?2 AND TO_AMOUNT>=?2 AND CHARGES_FOR=?3 AND CHARGES_TYPE=?4 AND BENE_COUNTRY_ID =?5 AND ISACTIVE = 'Y'", nativeQuery = true)
+	public List<BankCharges> fetchBankCharges(BigDecimal bankServiceRuleId,BigDecimal fcAmount,BigDecimal chargesFor,String chargesType,BigDecimal beneCountryId);
 
 }
