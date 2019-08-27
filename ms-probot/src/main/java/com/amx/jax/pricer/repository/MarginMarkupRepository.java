778ba9b35658676e432aa2e458b280d407274997
abhijeet <abkamble@gmail.com>
2019-08-27 18:23:09 +0530

    Only Active records consideration added in Online Margin Markup.

--- ms-probot/src/main/java/com/amx/jax/pricer/repository/MarginMarkupRepository.java
+++ ms-probot/src/main/java/com/amx/jax/pricer/repository/MarginMarkupRepository.java
@@ -15,11 +15,11 @@ public interface MarginMarkupRepository extends CrudRepository<OnlineMarginMarku
 	List<OnlineMarginMarkup> findByApplicationCountryIdAndCountryIdAndCurrencyId(BigDecimal aplCountryId,
 			BigDecimal countryId, BigDecimal currencyId);
 
-	List<OnlineMarginMarkup> findByApplicationCountryIdAndCountryIdAndCurrencyIdAndBankId(BigDecimal aplCountryId,
-			BigDecimal countryId, BigDecimal currencyId, BigDecimal bankId);
+	List<OnlineMarginMarkup> findByApplicationCountryIdAndCountryIdAndCurrencyIdAndBankIdAndIsActive(BigDecimal aplCountryId,
+			BigDecimal countryId, BigDecimal currencyId, BigDecimal bankId, String isActive);
 
-	List<OnlineMarginMarkup> findByApplicationCountryIdAndCurrencyIdAndBankIdIn(BigDecimal aplCountryId,
-			BigDecimal currencyId, List<BigDecimal> bankIds);
+	List<OnlineMarginMarkup> findByApplicationCountryIdAndCurrencyIdAndIsActiveAndBankIdIn(BigDecimal aplCountryId,
+			BigDecimal currencyId, String isActive, List<BigDecimal> bankIds);
 	
 	@Query(value = "select * from EX_ONLINE_MARGIN_MARKUP where APPLICATION_COUNTRY_ID=?1 and COUNTRY_ID=?2 and CURRENCY_ID=?3 and BANK_ID=?4 and "
 			+ "ISACTIVE='Y'", nativeQuery = true)
