2243313604ecc264730f8c270c936e3e6e62b999
LT <lalit.tanwar07@gmail.com>
2019-09-11 02:56:24 +0530

    Radar Fixes

--- lib-postman/src/main/java/com/amx/jax/postman/model/File.java
+++ lib-postman/src/main/java/com/amx/jax/postman/model/File.java
@@ -23,11 +23,13 @@ public class File implements Serializable {
 
 	private static final long serialVersionUID = -3165262414318034816L;
 	private static Logger LOGGER = LoggerService.getLogger(File.class);
+	private static Map<String, Type> TYPEMAP = new HashMap<String, Type>();
 
 	public enum Type implements EnumType {
 		PDF("application/pdf"), CSV("text/csv"),
 
-		PNG("image/png"), JPEG("image/jpeg"), JPG("image/jpg"),
+		PNG("image/png"), JPEG("image/jpeg"), JPG("image/jpg"), BMP("image/bmp"), GIF("image/gif"),
+		TIFF("image/tiff"), TIF("image/tif"),
 
 		JSON("application/json"), HTML("text/html"), TEXT(
 				"text/plain");
@@ -40,7 +42,13 @@ public class File implements Serializable {
 
 		Type(String contentType) {
 			this.contentType = contentType;
+			TYPEMAP.put(contentType, this);
 		}
+
+		public static Type from(String contentType) {
+			return TYPEMAP.get(contentType);
+		}
+
 	}
 
 	public enum PDFConverter {
--- ms-radar/src/main/java/com/amx/jax/radar/snap/SnapUtilityController.java
+++ ms-radar/src/main/java/com/amx/jax/radar/snap/SnapUtilityController.java
@@ -1,29 +1,36 @@
 package com.amx.jax.radar.snap;
 
 import java.io.IOException;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.amx.jax.radar.service.CivilIdValidationService;
 import com.amx.jax.radar.service.CivilIdValidationService.CivilIdValidationResponse;
 import com.google.i18n.phonenumbers.NumberParseException;
 
-@Controller
+@RestController
 public class SnapUtilityController {
 
 	@Autowired
 	CivilIdValidationService civilIdValidationService;
 
-	@ResponseBody
-	@RequestMapping(value = "snap/api/utility/civil/check", method = RequestMethod.GET)
+	@RequestMapping(value = "snap/api/utility/civil/check/id", method = RequestMethod.GET)
 	public CivilIdValidationResponse charResponse(@RequestParam String identity)
 			throws NumberParseException, IOException {
 		return civilIdValidationService.validate(identity);
+	}
+
+	@SuppressWarnings("static-access")
+	@RequestMapping(value = "/snap/api/utility/civil/scan/card", method = { RequestMethod.POST })
+	public Map<String, Object> uploadServiceProviderFile(@RequestParam MultipartFile file) throws Exception {
+
+		return civilIdValidationService.scan(file);
 	}
 
 }
