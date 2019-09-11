da7b7ef1963adc34174a88062c103081ea87ed28
Subodh Bhoir <subodh.bhoir@almullaexchange.com>
2019-09-11 12:32:15 +0530

    8864 - loyalty point check

--- core/src/main/java/com/amx/jax/config/JaxTenantProperties.java
+++ core/src/main/java/com/amx/jax/config/JaxTenantProperties.java
@@ -1,8 +1,7 @@
 package com.amx.jax.config;
 
-import java.util.Date;
+import java.math.BigDecimal;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.amx.jax.scope.TenantScoped;
@@ -76,6 +75,9 @@ public class JaxTenantProperties {
 	
 	@TenantValue("${signature.algorithem}")
 	private String sigAlgorithem;
+	
+	@TenantValue("${jax.max.loyalty.count}")
+	private BigDecimal loyaltyCount;
 
 	public Boolean getCashDisable() {
 		return cashDisable;
@@ -206,6 +208,14 @@ public class JaxTenantProperties {
 
 	}
 
+	public BigDecimal getLoyaltyCount() {
+		return loyaltyCount;
+	}
 
+	public void setLoyaltyCount(BigDecimal loyaltyCount) {
+		this.loyaltyCount = loyaltyCount;
+	}
+
+	
 
 }
