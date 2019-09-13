8c7a7d4d5fcef4626b0b46a3479f6671417394d2
Radhika.Ala <radhika.ala@almullaexchange.com>
2019-09-13 17:59:24 +0530

    local id issue

--- ms-jax/src/main/java/com/amx/jax/services/BeneficiaryService.java
+++ ms-jax/src/main/java/com/amx/jax/services/BeneficiaryService.java
@@ -509,9 +509,11 @@ public class BeneficiaryService extends AbstractService {
 		LanguageType languageType = new LanguageType();
 		languageType = languageTypeRepository.findBylanguageId(metaData.getLanguageId());
 		
-		if(languageType.getLanguageName().equals(LanguageCodeType.English.toString())){
+		if(languageType.getLanguageName().equals(LanguageCodeType.Arabic.toString())){
+			beneModel.setBankLocalName(beneModel.getBankLocalName());
+		}	else {
 			beneModel.setBankLocalName(null);
-		}		
+		}
 		try {
 			BeanUtils.copyProperties(dto, beneModel);
 		} catch (IllegalAccessException | InvocationTargetException e) {
