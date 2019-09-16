e6db625244bae81d815cf882d52ebfe1ab5cdc9e
Anant Mittal <anant.mittal@almullaexchange.com>
2019-09-16 11:40:58 +0530

    Query changed for pb list.

--- lib-db/src/main/java/com/amx/jax/repository/PayAtBranchTrnxListRepository.java
+++ lib-db/src/main/java/com/amx/jax/repository/PayAtBranchTrnxListRepository.java
@@ -10,9 +10,9 @@ import org.springframework.data.jpa.repository.Query;
 import com.amx.jax.dbmodel.PayAtBranchTrnxModel;
 
 public interface PayAtBranchTrnxListRepository extends JpaRepository<PayAtBranchTrnxModel, Serializable>{
-	@Query("select w from PayAtBranchTrnxModel w where w.customerId=?1 and (w.applIsActive='Y'and wireTransferStatus='NEW') and wireTransferStatus is not 'PAID'")
+	@Query("select w from PayAtBranchTrnxModel w where w.customerId=?1 and (w.applIsActive='Y'and wireTransferStatus='NEW') and wireTransferStatus is not 'PAID' and trunc(w.documentDate)=trunc(sysDate)")
 	public List<PayAtBranchTrnxModel> getPbTrnxList(BigDecimal customerId);
 	
-	@Query("select w from PayAtBranchTrnxModel w where (w.applIsActive='Y'and wireTransferStatus='NEW') order by documentDate desc")
+	@Query("select w from PayAtBranchTrnxModel w where (w.applIsActive='Y'and wireTransferStatus='NEW') and trunc(w.documentDate)=trunc(sysDate) order by documentDate desc")
 	public List<PayAtBranchTrnxModel> getPbTrnxListBranch();
 }
