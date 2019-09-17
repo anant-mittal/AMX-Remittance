ca24dc9ef793da1321ca24e6d7405c1c15d6d435
Anant Mittal <anant.mittal@almullaexchange.com>
2019-09-17 13:41:06 +0530

    Compile error removed.

--- amx-lib/src/main/java/com/amx/amxlib/exception/jax/OtpRequiredException.java
+++ amx-lib/src/main/java/com/amx/amxlib/exception/jax/OtpRequiredException.java
@@ -1,5 +1,7 @@
 package com.amx.amxlib.exception.jax;
 import com.amx.amxlib.exception.AbstractJaxException;
+import com.amx.jax.error.JaxError;
+import com.amx.jax.exception.AmxApiError;
 
 public class OtpRequiredException extends AbstractJaxException{
 
@@ -7,5 +9,17 @@ public class OtpRequiredException extends AbstractJaxException{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	public OtpRequiredException() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public OtpRequiredException(String errorMessage) {
+		super(JaxError.JAX_FIELD_VALIDATION_FAILURE.getStatusKey(), errorMessage);
+	}
+	
+	public OtpRequiredException(AmxApiError error) {
+		super(error);
+	}
 
 }
