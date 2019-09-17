5f3fee84576bcfb9c727f5279887b21a343f7625
Prashant Thorat <prashantnitdgp@gmail.com>
2019-09-17 16:09:17 +0530

    stackoverflow issue resolved

--- ms-jax/src/main/java/com/amx/jax/branchremittance/manager/BranchRemittanceSaveManager.java
+++ ms-jax/src/main/java/com/amx/jax/branchremittance/manager/BranchRemittanceSaveManager.java
@@ -252,7 +252,7 @@ public class BranchRemittanceSaveManager {
 	
 
 	public RemittanceResponseDto saveRemittanceTrnx(BranchRemittanceRequestModel remittanceRequestModel) {
-		
+		logger.debug("saveRemittanceTrnx request model : {}", JsonUtil.toJson(remittanceRequestModel));
 		List<BranchApplicationDto> shoppingCartList = new ArrayList<>();
 		shoppingCartList = remittanceRequestModel.getRemittanceApplicationId();
 		//updateApplicationStatus(shoppingCartList);
@@ -1175,7 +1175,6 @@ public BigDecimal generateDocumentNumber(BigDecimal appCountryId,BigDecimal comp
 
  public TransferDto getTrasnferModeByBankServiceRule(RemittanceTransaction remitTrnx){
 	 Map<String,Object> mapBankServiceRule= routingProDao.checkBankServiceRule(remitTrnx);
-	 logger.debug("getTrasnferModeByBankServiceRule request json : {}", JsonUtil.toJson(remitTrnx));
 	 TransferDto dto = new TransferDto();
 	 String transferMode=null;
 	 BigDecimal transferModeId=BigDecimal.ZERO;
