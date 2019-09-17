03eea7cf36edfb086fa7deab19070f5ced3a6aeb
Prashant Thorat <prashantnitdgp@gmail.com>
2019-09-17 11:52:03 +0530

    compile error fixed

--- lib-jax/src/main/java/com/amx/jax/customer/ICustomerProfileService.java
+++ lib-jax/src/main/java/com/amx/jax/customer/ICustomerProfileService.java
@@ -15,9 +15,11 @@ public interface ICustomerProfileService {
 	public static class ApiPath {
 		public static final String PREFIX = "/customer-profile";
 		public static final String CONTACT_LINK_CREATE = PREFIX + "/contact-link-create";
+		public static final String CONTACT_LINK_RESEND = PREFIX + "/contact-link-resend";
 		public static final String CONTACT_LINK_VALIDATE = PREFIX + "/contact-link-validate";
 		public static final String CONTACT_LINK_VERIFY_BY_CODE = PREFIX + "/contact-link-verify-by-code";
 		public static final String CONTACT_LINK_VERIFY_BY_CONTACT = PREFIX + "/contact-link-verify-by-contact";
+
 	}
 
 	public static class ApiParams {
@@ -33,25 +35,28 @@ public interface ICustomerProfileService {
 	 * 
 	 * 
 	 * @param customerId
-	 * @param type       - contact type
+	 * @param type
+	 *            - contact type
 	 * @param identity
-	 * @param send       - to send link or not
+	 * @param send
+	 *            - to send link or not
 	 * @return
 	 */
 	@ApiJaxStatus({ JaxError.CUSTOMER_NOT_FOUND, JaxError.MISSING_CONTACT })
 	@ApiStatus({ ApiStatusCodes.PARAM_MISSING })
-	AmxApiResponse<CustomerContactVerificationDto, Object> createVerificationLink(BigDecimal customerId,
-			ContactType type, String identity);
+	AmxApiResponse<CustomerContactVerificationDto, Object> createVerificationLink(BigDecimal customerId, ContactType type, String identity);
 
 	@ApiJaxStatus({ JaxError.ENTITY_INVALID, JaxError.ENTITY_EXPIRED })
 	AmxApiResponse<CustomerContactVerificationDto, Object> validateVerificationLink(BigDecimal linkId);
 
 	@ApiJaxStatus({ JaxError.ENTITY_INVALID, JaxError.ENTITY_EXPIRED, JaxError.INVALID_CIVIL_ID })
-	AmxApiResponse<CustomerContactVerificationDto, Object> verifyLinkByCode(String identity, BigDecimal linkId,
-			String code);
+	AmxApiResponse<CustomerContactVerificationDto, Object> verifyLinkByCode(String identity, BigDecimal linkId, String code);
 
 	@ApiJaxStatus({ JaxError.ENTITY_INVALID, JaxError.ENTITY_EXPIRED, JaxError.INVALID_CIVIL_ID })
-	AmxApiResponse<CustomerContactVerificationDto, Object> verifyLinkByContact(String identity, ContactType type,
-			String contact);
+	AmxApiResponse<CustomerContactVerificationDto, Object> verifyLinkByContact(String identity, ContactType type, String contact);
+
+	@ApiJaxStatus({ JaxError.CUSTOMER_NOT_FOUND, JaxError.MISSING_CONTACT })
+	@ApiStatus({ ApiStatusCodes.PARAM_MISSING })
+	AmxApiResponse<CustomerContactVerificationDto, Object> resendLink(String identity, BigDecimal linkId, String code);
 
 }
