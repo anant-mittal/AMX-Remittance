4196516c2e97dd503e76131866423fb9f4279c03
Subodh Bhoir <subodh.bhoir@almullaexchange.com>
2019-09-19 12:08:34 +0530

    merge error fixed

--- ms-jax/src/main/java/com/amx/jax/services/FcSaleDeliveryService.java
+++ ms-jax/src/main/java/com/amx/jax/services/FcSaleDeliveryService.java
@@ -93,7 +93,8 @@ public class FcSaleDeliveryService {
 	JaxConfigService jaxConfigService; 
 	@Autowired
 	CustomerDao custDao;
-	
+	@Autowired
+	PushNotifyClient pushNotifyClient;
 	@Autowired
 	UserContactVerificationManager userContactVerificationManager;
 
