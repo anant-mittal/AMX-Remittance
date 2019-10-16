ea5dc0feabd368b5c91b2213a9c68c6281988588
Subodh Bhoir <subodh.bhoir@almullaexchange.com>
2019-10-16 10:24:59 +0530

    9229-add new model

---
+++ ms-meta/src/main/java/com/amx/jax/repository/IViewBankChannelRepository.java
@@ -0,0 +1,13 @@
+package com.amx.jax.repository;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.amx.jax.dbmodel.ViewBankChannelModel;
+
+public interface IViewBankChannelRepository extends CrudRepository<ViewBankChannelModel, BigDecimal> {
+
+	public List<ViewBankChannelModel> findBybankCountryIdOrderByBankShortNameAsc(BigDecimal bankCountryId);
+}
