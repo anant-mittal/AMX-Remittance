b7380b20ef0ca6a024390fddcc35bcf6584e629e
dipali <dipali.pingale@almullaexchange.com>
2019-10-17 17:09:03 +0530

    parameter spelling corrected

--- ms-user/src/main/java/com/amx/jax/userservice/manager/CustomerRegistrationManager.java
+++ ms-user/src/main/java/com/amx/jax/userservice/manager/CustomerRegistrationManager.java
@@ -366,7 +366,7 @@ public class CustomerRegistrationManager extends TransactionModel<CustomerRegist
 				throw new GlobalException(JaxError.ID_TYPE_LENGTH_NOT_DEFINED,"The minimum length should be:"+minimumLength +" and maximum should be :"+maxLength);
 			}
 		}else {
-			throw new GlobalException(JaxError.ID_TYPE_LENGTH_NOT_DEFINED,"Id length setup is missing  in paramter :"+identityInt +" identityTypeId :"+identityTypeId);
+			throw new GlobalException(JaxError.ID_TYPE_LENGTH_NOT_DEFINED,"Id length setup is missing  in parameter :"+identityInt +" identityTypeId :"+identityTypeId);
 		}
 		
 		List<Customer> customerList = customerRepository.getCustomerDetails(identityInt,jaxMetaInfo.getCountryId());
