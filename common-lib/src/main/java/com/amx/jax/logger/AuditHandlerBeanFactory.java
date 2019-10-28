9712933f7bd5faa4df21c7eb583605f3c2fe3fce
LT <lalit.tanwar07@gmail.com>
2019-10-28 22:59:23 +0530

    null Check Added for beans

--- common-lib/src/main/java/com/amx/jax/logger/AuditHandlerBeanFactory.java
+++ common-lib/src/main/java/com/amx/jax/logger/AuditHandlerBeanFactory.java
@@ -2,6 +2,7 @@ package com.amx.jax.logger;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.amx.common.HandlerBeanFactory;
@@ -11,7 +12,7 @@ public class AuditHandlerBeanFactory extends HandlerBeanFactory<AuditHandler> {
 
 	private static final long serialVersionUID = 2959267620648739583L;
 
-	public AuditHandlerBeanFactory(List<AuditHandler> libs) {
+	public AuditHandlerBeanFactory(@Autowired(required = false) List<AuditHandler> libs) {
 		super(libs);
 	}
 
