94b3ce837ff90b198dbafdb62d0dfe40b6730bc6
Animesh <kanimesh.s@gmail.com>
2019-11-12 10:38:38 +0530

    group APIs

--- jax-client/src/main/java/com/amx/jax/client/ExchRateMgmtClient.java
+++ jax-client/src/main/java/com/amx/jax/client/ExchRateMgmtClient.java
@@ -1,18 +1,24 @@
 package com.amx.jax.client;
 
 import com.amx.jax.api.AmxApiResponse;
+import com.amx.jax.client.configs.JaxMetaInfo;
+import com.amx.jax.pricer.AbstractProbotInterface.ApiEndPoints;
 import com.amx.jax.pricer.PricerServiceClient;
 import com.amx.jax.pricer.dto.ExchRateEnquiryReqDto;
 import com.amx.jax.pricer.dto.ExchangeRateEnquiryRespDto;
+import com.amx.jax.pricer.dto.GroupDetails;
+import com.amx.jax.pricer.var.PricerServiceConstants.GROUP_TYPE;
 import com.amx.jax.rest.RestService;
+import com.amx.utils.ArgUtil;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ExchRateMgmtClient extends AbstractJaxServiceClient {
-	private static final Logger LOGGER = Logger.getLogger(DiscountMgmtClient.class);
+	private static final Logger LOGGER = Logger.getLogger(ExchRateMgmtClient.class);
 
 	@Autowired
 	RestService restService;
@@ -20,8 +26,28 @@ public class ExchRateMgmtClient extends AbstractJaxServiceClient {
 	@Autowired
 	PricerServiceClient pricerServiceClient;
 
-	public AmxApiResponse<ExchangeRateEnquiryRespDto, Object> enquireExchangeRates(ExchRateEnquiryReqDto rateEnquiryReqDto) {
+	public AmxApiResponse<ExchangeRateEnquiryRespDto, Object> enquireExchangeRates(
+			ExchRateEnquiryReqDto rateEnquiryReqDto) {
 		return pricerServiceClient.enquireExchangeRates(rateEnquiryReqDto);
 	}
+
+	public AmxApiResponse<GroupDetails, Object> getGroupsOfType(GROUP_TYPE groupType) {
+		return pricerServiceClient.getGroupsOfType(groupType.toString());
+	}
+
+	public AmxApiResponse<GroupDetails, Object> saveGroup(GroupDetails group) {
+		return pricerServiceClient.saveGroups(group);
+	}
+
+	// public AmxApiResponse<GroupDetails, Object> saveGroup(GroupDetails group) {
+	// 	LOGGER.info("In save Group client");
+	// 	if(ArgUtil.isEmpty(group.getApplCountryId())){
+	// 		JaxMetaInfo jaxMetaInfo = new JaxMetaInfo();
+	// 		group.setApplCountryId(jaxMetaInfo.getCountryBranchId());
+	// 	}
+	// 	return restService.ajax(appConfig.getPricerURL()).path(ApiEndPoints.SAVE_GROUPS).post(group)
+	// 			.as(new ParameterizedTypeReference<AmxApiResponse<GroupDetails, Object>>() {
+	// 			});
+	// }
 
 }
--- server-branch/src/main/java/com/amx/jax/branch/controller/ExchRateMgmtController.java
+++ server-branch/src/main/java/com/amx/jax/branch/controller/ExchRateMgmtController.java
@@ -8,14 +8,18 @@ import com.amx.jax.client.MetaClient;
 import com.amx.jax.logger.LoggerService;
 import com.amx.jax.pricer.dto.ExchRateEnquiryReqDto;
 import com.amx.jax.pricer.dto.ExchangeRateEnquiryRespDto;
+import com.amx.jax.pricer.dto.GroupDetails;
+import com.amx.jax.pricer.var.PricerServiceConstants.GROUP_TYPE;
 import com.amx.jax.sso.SSOUser;
 import com.amx.jax.swagger.IStatusCodeListPlugin.ApiStatusService;
+import com.amx.utils.ArgUtil;
 
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.Api;
@@ -43,6 +47,18 @@ public class ExchRateMgmtController {
 			@RequestBody ExchRateEnquiryReqDto rateEnquiryReqDto) {
 		return exchRateMgmtClient.enquireExchangeRates(rateEnquiryReqDto);
 	}
-	
-	
+
+	@RequestMapping(value = "/api/exch/group/get", method = { RequestMethod.POST })
+	public AmxApiResponse<GroupDetails, Object> getGroupsOfType(@RequestParam GROUP_TYPE groupType) {
+		return exchRateMgmtClient.getGroupsOfType(groupType);
+	}
+
+	@RequestMapping(value = "/api/exch/group/create", method = { RequestMethod.POST })
+	public AmxApiResponse<GroupDetails, Object> saveGroup(@RequestBody GroupDetails group) {
+		if (ArgUtil.isEmpty(group.getApplCountryId())) {
+			group.setApplCountryId(ssoUser.getUserDetails().getCountryId());
+		}
+		return exchRateMgmtClient.saveGroup(group);
+	}
+
 }
