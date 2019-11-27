c834087a66844d6726125fd6a1efb940174bfc79
itsanant <anant.mittal@almullaexchange.com>
2019-11-27 17:03:58 +0530

    Compile errors removed.

--- common-amx/src/main/java/com/amx/jax/dict/AmxEnums.java
+++ common-amx/src/main/java/com/amx/jax/dict/AmxEnums.java
@@ -1,5 +1,8 @@
 package com.amx.jax.dict;
+import com.amx.jax.def.Communication.CommunicationEvent;
+import com.amx.jax.def.Communication.CommunicationEventModel;
 
+import com.amx.utils.ArgUtil;
 import com.amx.utils.EnumType;
 
 public class AmxEnums {
@@ -27,9 +30,17 @@ public class AmxEnums {
 		ORD, ACP, PCK, DVD, OFD_ACK, OFD_CNF, OFD, CND_ACK, CND, RTD_ACK, RTD
 	}
 
-	public static enum CommunicationEvents implements EnumType {
+	public static enum CommunicationEvents implements CommunicationEvent {
 		CASH_PICKUP_BANK, CASH_PICKUP_WU, CASH_PICKUP_TF, BIRTHDAY_WISHES, NEW_DEVICE_LOGIN, BPI_JOLLIBEE,
-		ADD_BENEFICIARY, TRNX_BENE_CREDIT, REMITTANCE
+		ADD_BENEFICIARY, TRNX_BENE_CREDIT, REMITTANCE;
+	
+	public static CommunicationEvent fromString(String eventStr) {
+		CommunicationEvent x = (CommunicationEvent) ArgUtil.parseAsEnum(eventStr, CommunicationEvents.class);
+		if (ArgUtil.isEmpty(x)) {
+			return new CommunicationEventModel(eventStr);
+		}
+		return x;
+	}
 	}
 
 }
