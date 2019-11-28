9b0b3865ae4b869e22cd4f5c35a4033dba85a1a5
Animesh <kanimesh.s@gmail.com>
2019-11-28 15:37:51 +0530

    place order server-branch integration

---
+++ server-branch/src/main/java/com/amx/jax/branch/controller/POBranchController.java
@@ -0,0 +1,56 @@
+package com.amx.jax.branch.controller;
+
+import java.math.BigDecimal;
+
+import com.amx.jax.api.AmxApiResponse;
+import com.amx.jax.api.BoolRespModel;
+import com.amx.jax.client.remittance.RemittanceClient;
+import com.amx.jax.model.request.remittance.PlaceOrderRequestModel;
+import com.amx.jax.model.request.remittance.PlaceOrderUpdateStatusDto;
+import com.amx.jax.model.response.remittance.GsmPlaceOrderListDto;
+import com.amx.jax.model.response.remittance.GsmSearchRequestParameter;
+import com.amx.jax.model.response.remittance.RatePlaceOrderInquiryDto;
+import com.amx.jax.rbaac.IRbaacService;
+import com.amx.jax.swagger.IStatusCodeListPlugin.ApiStatusService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.Api;
+
+@RestController
+@Api(value = "Place Order  APIs")
+@ApiStatusService(IRbaacService.class)
+public class POBranchController {
+
+	@Autowired
+	private RemittanceClient branchRemittanceClient;
+
+	@RequestMapping(value = "/api/placeorder/create", method = { RequestMethod.POST })
+	public AmxApiResponse<BoolRespModel, Object> createPlaceOrder(@RequestBody PlaceOrderRequestModel placeOrderRequestModel) {
+		return branchRemittanceClient.savePlaceOrderApplication(placeOrderRequestModel);
+	}
+
+	@RequestMapping(value = "/api/placeorder/provider/list", method = { RequestMethod.POST })
+	public AmxApiResponse<GsmPlaceOrderListDto, Object> getCountryWisePlaceOrderProviderList(
+		@RequestBody GsmSearchRequestParameter requestParameter) {
+		return branchRemittanceClient.getCountryWisePlaceOrderCount(requestParameter);
+	}
+
+	@RequestMapping(value = "/api/placeorder/provider/action", method = { RequestMethod.POST })
+	public AmxApiResponse<BoolRespModel, Object> updateRatePlaceOrder(
+			@RequestBody PlaceOrderUpdateStatusDto placeOrderRequestUpdatDto) {
+		return branchRemittanceClient.updateRatePlaceOrder(placeOrderRequestUpdatDto);
+	}
+
+	@RequestMapping(value = "/api/placeorder/consumer/list", method = { RequestMethod.POST })
+	public AmxApiResponse<RatePlaceOrderInquiryDto, Object> getCountryWisePlaceOrderConsumerList(
+			@RequestParam BigDecimal countryBranchId) {
+		return branchRemittanceClient.fetchPlaceOrderInquiry(countryBranchId);
+	}
+
+}
