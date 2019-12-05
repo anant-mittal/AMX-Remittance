53ebdde67d4c9ee54aad78ef239b0d5947588f6b
dipali <dipali.pingale@almullaexchange.com>
2019-12-05 19:51:25 +0530

    integration added

--- lib-jax/src/main/java/com/amx/jax/client/snap/SnapServiceClient.java
+++ lib-jax/src/main/java/com/amx/jax/client/snap/SnapServiceClient.java
@@ -8,10 +8,12 @@ import com.amx.jax.client.snap.SnapConstants.SnapQueryTemplate;
 import com.amx.jax.client.snap.SnapModels.SnapModelWrapper;
 import com.amx.jax.client.snap.SnapModels.SnapQueryParams;
 import com.amx.jax.dict.Currency;
+import com.amx.jax.logger.LoggerService;
 import com.amx.jax.rest.RestService;
 
 @Component
 public class SnapServiceClient implements ISnapService {
+	private static final org.slf4j.Logger logger = LoggerService.getLogger(SnapServiceClient.class);
 
 	@Autowired
 	RestService restService;
@@ -27,26 +29,22 @@ public class SnapServiceClient implements ISnapService {
 
 	@Override
 	public SnapModelWrapper getTranxStats() {
-		return new SnapModelWrapper(
-				restService.ajax(appConfig.getRadarURL() + Path.SNAP_API_TRANX_DONE).get().asMap());
+		return new SnapModelWrapper(restService.ajax(appConfig.getRadarURL() + Path.SNAP_API_TRANX_DONE).get().asMap());
 	}
 
 	@Override
 	public SnapModelWrapper getXRateStats(StatsSpan graph, Currency forCur, Currency domCur) {
-		return new SnapModelWrapper(
-				restService.ajax(appConfig.getRadarURL() + Path.SNAP_API_XRATE_SELL_TRANSFER)
-						.queryParam("graph", graph).queryParam("forCur", forCur).queryParam("domCur", domCur)
-						.get().asMap());
+		return new SnapModelWrapper(restService.ajax(appConfig.getRadarURL() + Path.SNAP_API_XRATE_SELL_TRANSFER)
+				.queryParam("graph", graph).queryParam("forCur", forCur).queryParam("domCur", domCur).get().asMap());
 	}
 
-	public SnapModelWrapper snapView(SnapQueryTemplate snapView, SnapQueryParams params, String gte, String lte,
-			Integer level, Integer minCount) {
+	public SnapModelWrapper snapView(SnapQueryTemplate snapView, SnapQueryParams params) {
+		
+		logger.info("params gte",params.get("gte"));
 		return new SnapModelWrapper(
-				restService.ajax(appConfig.getRadarURL() + Path.SNAP_API_VIEW)
-						.pathParam(Params.SNAP_VIEW, snapView)
-						.queryParam("gte", gte).queryParam("lte", lte).queryParam("level", level)
-						.queryParam("level", level).queryParam("minCount", minCount)
-						.post(params).asMap());
+				restService.ajax(appConfig.getRadarURL() + Path.SNAP_API_VIEW).pathParam(Params.SNAP_VIEW, snapView)
+						.queryParam("gte", params.get("gte")).queryParam("lte", params.get("lte")).queryParam("level", params.getLevel())
+						.queryParam("minCount", params.getMinCount()).post(params).asMap());
 	}
 
 }
---
+++ server-branch/src/main/java/com/amx/jax/branch/controller/DashboardAnalyticsController.java
@@ -0,0 +1,75 @@
+package com.amx.jax.branch.controller;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.amx.jax.client.snap.SnapConstants.SnapQueryTemplate;
+import com.amx.jax.client.snap.SnapModels.SnapModelWrapper;
+import com.amx.jax.client.snap.SnapModels.SnapQueryParams;
+import com.amx.jax.client.snap.SnapServiceClient;
+import com.amx.jax.logger.LoggerService;
+import com.amx.jax.sso.SSOUser;
+
+import io.swagger.annotations.Api;
+
+@RestController
+@Api(value = "Snap Api")
+public class DashboardAnalyticsController {
+
+	@Autowired
+	private SnapServiceClient snapServiceClient;
+
+	@Autowired
+	private SSOUser ssoUser;
+
+	private static final org.slf4j.Logger logger = LoggerService.getLogger(DashboardAnalyticsController.class);
+
+	@RequestMapping(value = "/pub/snap/view/RPT", method = RequestMethod.POST)
+	public SnapModelWrapper snapView(@RequestBody SnapQueryParams params) throws IOException {
+
+		Map<String, Map<String, String>> permissionMap = ssoUser.getUserDetails().getUserRole().getPermissionMap();
+		SnapQueryParams snapQueryParams = new SnapQueryParams();
+		String userName = ssoUser.getUserDetails().getUserName();
+		BigDecimal areacode = ssoUser.getUserDetails().getAreaCode();
+		BigDecimal branchId = ssoUser.getUserDetails().getBranchId();
+		Map<String, String> testMap = new HashMap<String, String>();
+
+		for (String outerMapKey : permissionMap.keySet()) {
+			if (outerMapKey == "CUSTOMER_MGMT.REMITTANCE") {
+				Map<String, String> innerMap = permissionMap.get(outerMapKey);
+				innerMap.forEach((key, value) -> {
+					if (key.contains("VIEW") && value.contains("COUNTRY")) {
+						logger.debug("Key = " + key + ", Value = " + value);
+						snapQueryParams.addFilter("branch.areaId", areacode.toString());
+						testMap.put("branch.areaId", areacode.toString());
+					}
+					if (key.contains("VIEW") && value.contains("BRANCH")) {
+						logger.debug("Key = " + key + ", Value = " + value);
+						snapQueryParams.addFilter("branch.id", branchId.toString());
+						testMap.put("branch.id", branchId.toString());
+
+					}
+
+				});
+
+			}
+			testMap.forEach((key, value) -> {
+				logger.debug("Key = " + key + ", Value = " + value);
+
+			});
+
+		}
+		// snapQueryParams.addFilter("user.name", userName);
+		return snapServiceClient.snapView(SnapQueryTemplate.RPTPG2, snapQueryParams);
+
+	}
+
+}
