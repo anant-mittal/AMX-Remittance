5e4795ad7877fc5566359206a487a9b2872044a9
Prashant Thorat <prashantnitdgp@gmail.com>
2019-12-07 19:25:26 +0530

    swagger config for ms-sp

--- ms-service-provider/src/main/java/com/amx/service_provider/MsServiceProviderConfig.java
+++ ms-service-provider/src/main/java/com/amx/service_provider/MsServiceProviderConfig.java
@@ -1,10 +1,12 @@
 package com.amx.service_provider;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -14,9 +16,14 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @EnableAutoConfiguration
 @Scope(value = WebApplicationContext.SCOPE_REQUEST, proxyMode = ScopedProxyMode.TARGET_CLASS)
-@ComponentScan(basePackages = { "com.amx.service_provider.repository.webservice" })
+@ComponentScan(basePackages = { "com.amx.service_provider.repository.webservice" ,"com.amx.jax","com.amx.service_provider"})
 @EnableAsync(proxyTargetClass = true)
-public class MsServiceProviderConfig
-{
+public class MsServiceProviderConfig {
 
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+		PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
+		propertySourcesPlaceholderConfigurer.setIgnoreUnresolvablePlaceholders(true);
+		return propertySourcesPlaceholderConfigurer;
+	}
 }
