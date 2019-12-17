dec0799ab427afa0731c844d132d8f014fadfc95
dipali <dipali.pingale@almullaexchange.com>
2019-12-17 15:23:16 +0530

    transaction life cycle api added

--- ms-radar/src/main/java/com/amx/jax/radar/snap/SnapQueryProcessorActiveDevice.java
+++ ms-radar/src/main/java/com/amx/jax/radar/snap/SnapQueryProcessorActiveDevice.java
@@ -5,17 +5,17 @@ import java.util.List;
 import java.util.Map.Entry;
 import java.util.Set;
 
-import org.slf4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
 import com.amx.jax.client.snap.SnapConstants.SnapQueryTemplate;
+import com.amx.jax.client.snap.SnapModels.SnapQueryParams;
 import com.amx.jax.def.AbstractQueryFactory.QueryProcessor;
 import com.amx.jax.device.DeviceBox;
 import com.amx.jax.device.DeviceData;
 import com.amx.jax.logger.LoggerService;
 import com.amx.jax.radar.service.SnapQueryFactory.SnapQuery;
-import com.amx.jax.radar.service.SnapQueryFactory.SnapQueryParams;
+
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 @Component
 @SnapQuery(SnapQueryTemplate.ACTIVE_DEVICE_REPORT)
---
+++ server-offsite/src/main/java/com/amx/jax/offsite/controller/DashboardAnalyticsController.java
@@ -0,0 +1,39 @@
+package com.amx.jax.offsite.controller;
+
+import java.util.Map;
+
+import com.amx.jax.client.snap.SnapConstants.SnapQueryTemplate;
+import com.amx.jax.client.snap.SnapModels.SnapModelWrapper;
+import com.amx.jax.client.snap.SnapModels.SnapQueryParams;
+import com.amx.jax.client.snap.SnapServiceClient;
+import com.amx.jax.logger.LoggerService;
+
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.Api;
+
+/**
+ * The Class MetaController.
+ */
+@RestController
+@Api(value = "DashBoard  APIs")
+public class DashboardAnalyticsController {
+
+	private static final Logger LOGGER = LoggerService.getLogger(DashboardAnalyticsController.class);
+
+	@Autowired
+	private SnapServiceClient snapServiceClient;
+
+	@RequestMapping(value = "/api/reports/trnx_status", method = RequestMethod.POST)
+	public SnapModelWrapper snapView(@RequestBody Map<String, Object> params) {
+		return snapServiceClient.snapView(SnapQueryTemplate.TRNX_LIFECYCLE, new SnapQueryParams(params));
+	}
+
+
+	
+}
