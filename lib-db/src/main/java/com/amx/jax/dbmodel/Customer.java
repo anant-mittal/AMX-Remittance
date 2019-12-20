317004ac32165d8cf2edf21e70e1f13b515fa725
itsanant <anant.mittal@almullaexchange.com>
2019-12-20 19:08:17 +0530

    JsonIgnore property for lazy type fetch

--- lib-db/src/main/java/com/amx/jax/dbmodel/Customer.java
+++ lib-db/src/main/java/com/amx/jax/dbmodel/Customer.java
@@ -31,6 +31,7 @@ import com.amx.jax.dict.Communicatable;
 import com.amx.jax.dict.ContactType;
 import com.amx.jax.util.AmxDBConstants.Status;
 import com.amx.utils.ArgUtil;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "FS_CUSTOMER")
@@ -986,6 +987,7 @@ public class Customer implements java.io.Serializable, Communicatable {
 
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "ARTICLE_DETAIL_ID")
+	@JsonIgnore
 	public ArticleDetails getFsArticleDetails() {
 		return fsArticleDetails;
 	}
@@ -996,6 +998,7 @@ public class Customer implements java.io.Serializable, Communicatable {
 
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "INCOME_RANGE_ID")
+	@JsonIgnore
 	public IncomeRangeMaster getFsIncomeRangeMaster() {
 		return fsIncomeRangeMaster;
 	}
@@ -1157,6 +1160,7 @@ public class Customer implements java.io.Serializable, Communicatable {
 	@ManyToMany(cascade = CascadeType.ALL, fetch=FetchType.LAZY)
 	@JoinTable(  name = "JAX_COMPLIANCE_BLOCKED_DOC_MAP", joinColumns = @JoinColumn(name = "CUSTOMER_ID", referencedColumnName="CUSTOMER_ID"), inverseJoinColumns = @JoinColumn(name = "COMP_BLOCKED_CUST_DOC_MAP_ID",
 			referencedColumnName="ID"))
+	@JsonIgnore
 	public List<ComplianceBlockedCustomerDocMap> getComplianceBlockedDocuments() {
 		return complianceBlockedDocuments;
 	}
