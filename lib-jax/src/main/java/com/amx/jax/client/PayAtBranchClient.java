f20b0a3505408f8853b3fc782595b09c7d3899d9
Radhika Ala <radhika.ala@almullaexchange.com>
2020-01-09 12:33:17 +0530

    Removing try catch block to resolve error count issue

--- lib-jax/src/main/java/com/amx/jax/client/PayAtBranchClient.java
+++ lib-jax/src/main/java/com/amx/jax/client/PayAtBranchClient.java
@@ -26,45 +26,32 @@ public class PayAtBranchClient implements IWireTransferService{
 	@Override
 	public AmxApiResponse<ResourceDTO, Object> getPaymentModes() {
 
-		try {
+		
 			return restService.ajax(appConfig.getJaxURL())
 					.path(Path.PREFIX + Path.PAYMENT_MODES)
 					.meta(new JaxMetaInfo()).post()
 					.as(new ParameterizedTypeReference<AmxApiResponse<ResourceDTO, Object>>() {
 					});
-		} catch (Exception ae) {
-			logger.error("exception in Annual Income details : ", ae);
-			return JaxSystemError.evaluate(ae);
-		}
+		
 	}
 	
 	@Override
 	public AmxApiResponse<PayAtBranchTrnxListDTO, Object> getPbTrnxList() {
 
-		try {
 			return restService.ajax(appConfig.getJaxURL())
 					.path(Path.PREFIX + Path.PB_TRNX_LIST)
 					.meta(new JaxMetaInfo()).post()
 					.as(new ParameterizedTypeReference<AmxApiResponse<PayAtBranchTrnxListDTO, Object>>() {
 					});
-		} catch (Exception ae) {
-			logger.error("exception in get wire transaction list : ", ae);
-			return JaxSystemError.evaluate(ae);
-		}
 	}
 
 	@Override
 	public AmxApiResponse<PayAtBranchTrnxListDTO, Object> getPbTrnxListBranch() {
 
-		try {
 			return restService.ajax(appConfig.getJaxURL())
 					.path(Path.PREFIX + Path.PB_TRNX_LIST_BRANCH)
 					.meta(new JaxMetaInfo()).post()
 					.as(new ParameterizedTypeReference<AmxApiResponse<PayAtBranchTrnxListDTO, Object>>() {
 					});
-		} catch (Exception ae) {
-			logger.error("exception in get wire transaction list for branch : ", ae);
-			return JaxSystemError.evaluate(ae);
-		}
 	}
 }
