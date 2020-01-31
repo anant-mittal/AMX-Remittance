271e1f91f10b3dd9f4b259c85494a289de1c4982
LT <lalit.tanwar07@gmail.com>
2020-01-31 18:12:44 +0530

    Moved Files to common Server

--- server-common/server-common-uis/src/main/java/com/amx/jax/ui/WebCommonApplication.java
+++ server-common/server-common-uis/src/main/java/com/amx/jax/ui/WebCommonApplication.java
@@ -1,51 +1,27 @@
 package com.amx.jax.ui;
 
-import java.util.ArrayList;
-
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.DelegatingFilterProxyRegistrationBean;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.context.WebApplicationContext;
 
 import com.amx.jax.client.configs.JaxMetaInfo;
-import com.amx.jax.ui.config.WebTenantFilter;
 
-/**
- * The Class WebApplication.
- */
-@ServletComponentScan
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
-@ComponentScan("com.amx.jax")
-@EnableAsync
-@EnableCaching
-public class WebApplication extends SpringBootServletInitializer {
-
-	/** The Constant USE_HAZELCAST. */
-	public static final String USE_HAZELCAST = "false";
-
-	/** The Constant USE_REDIS. */
-	public static final String USE_REDIS = "true";
+public class WebCommonApplication extends SpringBootServletInitializer {
 
 	/**
 	 * The main method.
 	 *
 	 * @param args the arguments
 	 */
-	public static void main(String[] args) {
-		SpringApplication.run(WebApplication.class, args);
+	public static void run(Class<? extends WebCommonApplication> applicationClass, String[] args) {
+		SpringApplication.run(applicationClass, args);
 	}
 
 	/*
@@ -57,23 +33,7 @@ public class WebApplication extends SpringBootServletInitializer {
 	 */
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder) {
-		return applicationBuilder.sources(WebApplication.class);
-	}
-
-	/**
-	 * Filter registration bean.
-	 *
-	 * @return the filter registration bean
-	 */
-	@Bean(name = "checkSession")
-	public FilterRegistrationBean filterRegistrationBean() {
-		WebTenantFilter f = new WebTenantFilter();
-		FilterRegistrationBean registrationBean = new FilterRegistrationBean();
-		registrationBean.setFilter(f);
-		ArrayList<String> match = new ArrayList<>();
-		match.add("/*");
-		registrationBean.setUrlPatterns(match);
-		return registrationBean;
+		return applicationBuilder.sources(WebCommonApplication.class);
 	}
 
 	/**
